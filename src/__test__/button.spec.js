c9336bb714a6468c025d66e838143a7e2c8f4df3
fomenyesu <fomenyesu@gmail.com>
2020-04-28 13:20:02 +0800

    test：增加E2E测试环境和示例用例

---
+++ src/__test__/button.spec.js
@@ -0,0 +1,13 @@
+describe('测试按钮组件', () => {
+  beforeEach(() => {
+    cy.visit('/#/components/button');
+  });
+  it('测试三种按钮类型，内容，渲染正确的类型跟内容', function() {
+  });
+  it('测试按钮尺寸，渲染正确的大小', function() {
+  });
+  it('测试带图标按钮，按钮内容里的图标位置', function() {
+  });
+  it('测试loading状态的按钮', function() {
+  });
+});
