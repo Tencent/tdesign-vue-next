141e476d6cf6015b6418eec6efde4714bdcf99e4
fomenyesu <fomenyesu@gmail.com>
2020-05-25 20:03:39 +0800

    test: update test lint config

--- test/e2e/button/button.spec.js
+++ test/e2e/button/button.spec.js
@@ -6,14 +6,8 @@ describe('测试按钮组件', () => {
     cy.get('.tdesign-demo-item--button')
       .find('button')
       .first()
-      .should(ele => {
+      .should((ele) => {
         expect(ele).to.have.class('t-button--primary');
-      })
-  });
-  it('测试按钮尺寸，渲染正确的大小', () => {
-  });
-  it('测试带图标按钮，按钮内容里的图标位置', () => {
-  });
-  it('测试loading状态的按钮', () => {
+      });
   });
 });
