d39d12ba69c34f34b644c136b32b6651396066f4
fomenyesu <fomenyesu@gmail.com>
2020-05-25 19:47:57 +0800

    chore: update gitignore , eslintignore

--- test/e2e/button/button.spec.js
+++ test/e2e/button/button.spec.js
@@ -2,12 +2,18 @@ describe('测试按钮组件', () => {
   beforeEach(() => {
     cy.visit('/#/components/button');
   });
-  it('测试三种按钮类型，内容，渲染正确的类型跟内容', function() {
+  it('测试三种按钮类型，内容，渲染正确的类型跟内容', () => {
+    cy.get('.tdesign-demo-item--button')
+      .find('button')
+      .first()
+      .should(ele => {
+        expect(ele).to.have.class('t-button--primary');
+      })
   });
-  it('测试按钮尺寸，渲染正确的大小', function() {
+  it('测试按钮尺寸，渲染正确的大小', () => {
   });
-  it('测试带图标按钮，按钮内容里的图标位置', function() {
+  it('测试带图标按钮，按钮内容里的图标位置', () => {
   });
-  it('测试loading状态的按钮', function() {
+  it('测试loading状态的按钮', () => {
   });
 });
