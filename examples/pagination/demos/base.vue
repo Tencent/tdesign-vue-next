b4936712bc7c89a9511f286d242b4576c8c5f0f5
lovechuck <lovechuck>
2020-06-02 11:33:24 +0800

    feat(pagination): init pagination props

--- examples/pagination/demos/base.vue
+++ examples/pagination/demos/base.vue
@@ -1,3 +1,27 @@
 <template>
-  <t-pagination></t-pagination>
-</template>;
+  <div>
+    <t-pagination
+      v-model="current"
+      :total="100"
+      @change="onChange"
+      :page-size="5"
+    />
+    {{current}}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      current: 12,
+    };
+  },
+  methods: {
+    onChange(index, event) {
+      console.log(`转到第${index}页`);
+      console.log(event);
+    },
+  },
+};
+</script>
