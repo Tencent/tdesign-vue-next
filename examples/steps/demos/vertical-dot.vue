4a7dd816529d1334fe4c73d4a7adf448efe54a1a
JrainLau <jrainlau@163.com>
2020-06-11 19:14:20 +0800

    feat: support reverse

--- examples/steps/demos/vertical-dot.vue
+++ examples/steps/demos/vertical-dot.vue
@@ -1,8 +1,17 @@
 <template>
-  <t-steps direction="vertical" type="dot" :current="1" status="process">
-    <t-step title="已完成的步骤" content="这里是提示文字"></t-step>
-    <t-step title="进行中的步骤" content="这里是提示文字"></t-step>
-    <t-step title="未进行的步骤" content="这里是提示文字"></t-step>
-    <t-step title="未进行的步骤" content="这里是提示文字"></t-step>
-  </t-steps>
+  <div>
+    <t-steps direction="vertical" type="dot" :current="1" status="process">
+      <t-step title="已完成的步骤" content="这里是提示文字"></t-step>
+      <t-step title="进行中的步骤" content="这里是提示文字"></t-step>
+      <t-step title="未进行的步骤" content="这里是提示文字"></t-step>
+      <t-step title="未进行的步骤" content="这里是提示文字"></t-step>
+    </t-steps>
+    <div style="margin: 16px 0;"></div>
+    <t-steps direction="vertical" sequence="reverse" type="dot" :current="1" status="process">
+      <t-step title="已完成的步骤" content="这里是提示文字"></t-step>
+      <t-step title="进行中的步骤" content="这里是提示文字"></t-step>
+      <t-step title="未进行的步骤" content="这里是提示文字"></t-step>
+      <t-step title="未进行的步骤" content="这里是提示文字"></t-step>
+    </t-steps>
+  </div>
 </template>
