aeeb3d109859d65789700375b02fa0ac0baf9fb8
chaishi <974383157@qq.com>
2020-06-16 18:29:55 +0800

    docs: add all icons demo to document

---
+++ examples/icon/demos/all.vue
@@ -0,0 +1,89 @@
+<template>
+  <!--
+    该文件有脚本 tdesign/tdesign-web-vue/src/icon/generate/index.js 自动生成。
+    若需要编辑，请编辑源文件：tdesign/tdesign-web-vue/src/icon/generate/demo.tpl 。
+  -->
+  <div class="t-demo-iconfont">
+    <p>目前设计师给到的全部图标</p><br>
+    <div class="t-demo-icon">
+      <t-icon name="browse-20" style="color: #0052d9" />
+      <div class="t-demo-icon-name">browse-20</div>
+    </div>
+    <div class="t-demo-icon">
+      <t-icon name="close-fill" style="color: #50cc9c" />
+      <div class="t-demo-icon-name">close-fill</div>
+    </div>
+    <div class="t-demo-icon">
+      <t-icon name="close" style="color: #51cbdb" />
+      <div class="t-demo-icon-name">close</div>
+    </div>
+    <div class="t-demo-icon">
+      <t-icon name="discount" style="color: #a599ff" />
+      <div class="t-demo-icon-name">discount</div>
+    </div>
+    <div class="t-demo-icon">
+      <t-icon name="help-fill" style="color: #a970cc" />
+      <div class="t-demo-icon-name">help-fill</div>
+    </div>
+    <div class="t-demo-icon">
+      <t-icon name="more" style="color: #cc47bd" />
+      <div class="t-demo-icon-name">more</div>
+    </div>
+    <div class="t-demo-icon">
+      <t-icon name="not-visible-20" style="color: #80c9f2" />
+      <div class="t-demo-icon-name">not-visible-20</div>
+    </div>
+    <div class="t-demo-icon">
+      <t-icon name="loading" style="color: #34a3e3" />
+      <div class="t-demo-icon-name">loading</div>
+    </div>
+    <div class="t-demo-icon">
+      <t-icon name="search" style="color: #0e86cc" />
+      <div class="t-demo-icon-name">search</div>
+    </div>
+    <div class="t-demo-icon">
+      <t-icon name="prompt-fill" style="color: #091c99" />
+      <div class="t-demo-icon-name">prompt-fill</div>
+    </div>
+    <div class="t-demo-icon">
+      <t-icon name="success-fill" style="color: #0052d9" />
+      <div class="t-demo-icon-name">success-fill</div>
+    </div>
+    <div class="t-demo-icon">
+      <t-icon name="warning-fill" style="color: #50cc9c" />
+      <div class="t-demo-icon-name">warning-fill</div>
+    </div>
+    <div class="t-demo-icon">
+      <t-icon name="arrow-down" style="color: #51cbdb" />
+      <div class="t-demo-icon-name">arrow-down</div>
+    </div>
+    <div class="t-demo-icon">
+      <t-icon name="arrow-left" style="color: #a599ff" />
+      <div class="t-demo-icon-name">arrow-left</div>
+    </div>
+    <div class="t-demo-icon">
+      <t-icon name="arrow-right" style="color: #a970cc" />
+      <div class="t-demo-icon-name">arrow-right</div>
+    </div>
+  </div>
+</template>
+
+<style lang="less">
+  .t-demo-iconfont {
+    .t-demo-icon {
+      width: 100px;
+      margin-right: 24px;
+      display: inline-block;
+      text-align: center;
+      margin-bottom: 24px;
+      .t-icon {
+        font-size: 25px;
+        margin-right: 0;
+      }
+      .t-demo-icon-name {
+        text-align: center;
+        margin-top: 8px;
+      }
+    }
+  }
+</style>
