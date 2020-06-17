22ef42230c00deea9879f5e4b8e29dedfacb9ff1
realyuyanan <1491198339@qq.com>
2020-06-17 15:27:21 +0800

    feat(table): support multi-level header

--- src/table/util/props-util.ts
+++ src/table/util/props-util.ts
@@ -1,31 +1,51 @@
 import { Columns, Column, ColumnsMap } from '../types/table';
 
 export const columnClassify =  (columns: Columns, verticalAlign: string): any => {
-  const leftFixedColumns: ColumnsMap = {};
-  const rightFixedColumns: ColumnsMap = {};
-  const mainColumns: ColumnsMap = {};
+  let leftFixedColumns: ColumnsMap = {};
+  let rightFixedColumns: ColumnsMap = {};
+  let mainColumns: ColumnsMap = {};
   columns.forEach((column: Column) => {
-    const { fixed, colKey } = column;
-    switch (fixed) {
-      case 'left': {
-        leftFixedColumns[colKey] = {
-          ...column,
-          verticalAlign,
-        };
-        break;
-      }
-      case 'right': {
-        rightFixedColumns[colKey] = {
-          ...column,
-          verticalAlign,
-        };
-        break;
-      }
-      default: {
-        mainColumns[colKey] = {
-          ...column,
-          verticalAlign,
-        };
+    const { fixed, colKey, children } = column;
+    if (children?.length) {
+      const {
+        leftFixedColumns: left,
+        rightFixedColumns: right,
+        mainColumns: main,
+      } = columnClassify(children, verticalAlign);
+      leftFixedColumns = {
+        ...leftFixedColumns,
+        ...left,
+      };
+      rightFixedColumns = {
+        ...rightFixedColumns,
+        ...right,
+      };
+      mainColumns = {
+        ...mainColumns,
+        ...main,
+      };
+    } else {
+      switch (fixed) {
+        case 'left': {
+          leftFixedColumns[colKey] = {
+            ...column,
+            verticalAlign,
+          };
+          break;
+        }
+        case 'right': {
+          rightFixedColumns[colKey] = {
+            ...column,
+            verticalAlign,
+          };
+          break;
+        }
+        default: {
+          mainColumns[colKey] = {
+            ...column,
+            verticalAlign,
+          };
+        }
       }
     }
   });
