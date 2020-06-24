f08b0f32572b52c617a94c7a1a018fbe426db052
qinmudi <qinmudi@139.com>
2020-06-24 15:34:14 +0800

    chore: add change menu active

--- examples/menu/demos/head-menu-dropdown.vue
+++ examples/menu/demos/head-menu-dropdown.vue
@@ -3,7 +3,7 @@
     <img slot="logo" class="t-menu__logo--center" src="https://main.qcloudimg.com/raw/4927884bb0c43e726c5915d5bef49ef3/head-logo-dark.png" alt="logo">
     <t-menu-item name="item1">菜单内容一</t-menu-item>
     <t-menu-item name="item2">已选内容</t-menu-item>
-    <t-menu-item name="item1">菜单内容二</t-menu-item>
+    <t-menu-item name="item3">菜单内容二</t-menu-item>
     <t-menu-item name="item4" :disabled="true">菜单内容三</t-menu-item>
     <div slot="options">
       <a href="javascript:;"><i class="t-icon t-icon-demo"></i></a>
