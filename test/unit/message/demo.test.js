35d03031281fd1fc25f801bd8e0ed16d707687cc
chaishi <974383157@qq.com>
2020-06-28 16:15:04 +0800

    test(message): add unit test

--- test/unit/message/demo.test.js
+++ test/unit/message/demo.test.js
@@ -2,9 +2,39 @@ import { mount } from '@vue/test-utils';
 
 // unit test for component in examples.
 describe('Message', () => {
-  it('base demo works fine', () => {
-    const demo = require('@/examples/message/demos/base.vue');
+  it('type demo works fine', () => {
+    const demo = require('@/examples/message/demos/type.vue');
     const wrapper = mount(demo);
     expect(wrapper.element).toMatchSnapshot();
   });
+  // it('close demo works fine', () => {
+  //   const demo = require('@/examples/message/demos/close.vue');
+  //   const wrapper = mount(demo);
+  //   expect(wrapper).toMatchSnapshot();
+  // });
+  // it('closeAll demo works fine', () => {
+  //   const demo = require('@/examples/message/demos/closeAll.vue');
+  //   const wrapper = mount(demo);
+  //   expect(wrapper.element).toMatchSnapshot();
+  // });
+  // it('offset demo works fine', () => {
+  //   const demo = require('@/examples/message/demos/offset.vue');
+  //   const wrapper = mount(demo);
+  //   expect(wrapper.element).toMatchSnapshot();
+  // });
+  // it('placement demo works fine', () => {
+  //   const demo = require('@/examples/message/demos/placement.vue');
+  //   const wrapper = mount(demo);
+  //   expect(wrapper.element).toMatchSnapshot();
+  // });
+  // it('plugin demo works fine', () => {
+  //   const demo = require('@/examples/message/demos/plugin.vue');
+  //   const wrapper = mount(demo);
+  //   expect(wrapper.element).toMatchSnapshot();
+  // });
+  // it('toggle demo works fine', () => {
+  //   const demo = require('@/examples/message/demos/toggle.vue');
+  //   const wrapper = mount(demo);
+  //   expect(wrapper.element).toMatchSnapshot();
+  // });
 });
--- test/unit/message/index.test.js
+++ test/unit/message/index.test.js
@@ -1,30 +1,226 @@
 import { mount } from '@vue/test-utils';
-import Message from '@/src/message/index.ts';
+import { Message } from '@/src/message/index.ts';
+import Button from '@/src/Button/index.ts';
+import TIconClose from '@/src/icon/close';
+import TIconMore from '@/src/icon/more';
+import TIconPromptFill from '@/src/icon/prompt-fill';
+import TIconSuccessFill from '@/src/icon/success-fill';
+import TIconWarningFill from '@/src/icon/warning-fill';
+import TIconHelpFill from '@/src/icon/help-fill';
+import TIconLoadingFill from '@/src/icon/loading';
 
 // every component needs four parts: props/events/slots/functions.
 describe('Message', () => {
   // test props api
   describe(':props', () => {
-    it('', () => {
+    const THEME_MAP = {
+      info: TIconPromptFill,
+      success: TIconSuccessFill,
+      warning: TIconWarningFill,
+      error: TIconWarningFill,
+      question: TIconHelpFill,
+      loading: TIconLoadingFill,
+    };
+    const THEME_LIST = Object.keys(THEME_MAP);
+
+    it('pure message contains two classes', () => {
+      const wrapper = mount(Message);
+      const classes = wrapper.classes();
+      expect(classes).toContain('t-message');
+      expect(classes).toContain('t-is-info');
+      expect(wrapper.contains(TIconClose)).toBe(false);
+      expect(wrapper).toMatchSnapshot();
+    });
+
+    it(`:theme ${THEME_LIST.join()}`, () => {
+      THEME_LIST.forEach((t) => {
+        const wrapper = mount({
+          render() {
+            return <Message theme={t}></Message>;
+          },
+        });
+        const msg = wrapper.find(Message);
+        expect(msg.classes()).toContain(`t-is-${t}`);
+        expect(msg.contains(THEME_MAP[t]));
+        expect(wrapper).toMatchSnapshot();
+      });
+    });
+
+    it(':closeBtn is true, render default close button.', () => {
       const wrapper = mount({
         render() {
-          return <Message></Message>;
+          return <Message closeBtn={true}></Message>;
         },
       });
-      expect(wrapper.isEmpty()).toBe(false);
+      const msg = wrapper.find(Message);
+      expect(msg.contains('.t-message-close')).toBe(true);
+      expect(msg.contains(TIconClose)).toBe(true);
+      expect(wrapper).toMatchSnapshot();
+    });
+
+    it(':closeBtn is a tring, equal "关闭".', () => {
+      const wrapper = mount({
+        render() {
+          return <Message closeBtn='关闭'></Message>;
+        },
+      });
+      const msg = wrapper.find(Message);
+      const close = msg.find('.t-message-close');
+      expect(close.isEmpty()).toBe(false);
+      expect(close.text()).toBe('关闭');
+      expect(wrapper).toMatchSnapshot();
+    });
+
+    it(':closeBtn is a function, () => VNode.', () => {
+      const wrapper = mount({
+        render() {
+          return (
+            <Message
+              closeBtn={() => <b class='t-message-close'>x</b>}
+            ></Message>
+          );
+        },
+      });
+      const msg = wrapper.find(Message);
+      const close = msg.find('.t-message-close');
+      expect(close.isEmpty()).toBe(false);
+      expect(close.html()).toBe('<b class="t-message-close">x</b>');
+      expect(wrapper).toMatchSnapshot();
+    });
+
+    it(':icon is false', () => {
+      const wrapper = mount({
+        render() {
+          return <Message icon={false} ></Message>;
+        },
+      });
+      const msg = wrapper.find(Message);
+      expect(msg.contains('.t-icon')).toBe(false);
+      expect(wrapper).toMatchSnapshot();
+    });
+
+    it(':icon is a funtion, () => TIconMore', () => {
+      const wrapper = mount({
+        render() {
+          return (
+            <Message
+              icon={() => <TIconMore></TIconMore>}
+            ></Message>
+          );
+        },
+      });
+      expect(wrapper.contains(TIconMore)).toBe(true);
+      expect(wrapper).toMatchSnapshot();
+    });
+
+    it(':default is a funtion, () => <b>这是重要信息</b>', () => {
+      const wrapper = mount({
+        render() {
+          return (
+            <Message
+              default={() => <b>这是重要信息</b>}
+            ></Message>
+          );
+        },
+      });
+      expect(wrapper).toMatchSnapshot();
+    });
+
+    it(':default is a string, 这是普通信息', () => {
+      const wrapper = mount({
+        render() {
+          return (
+            <Message default='这是普通信息'></Message>
+          );
+        },
+      });
+      expect(wrapper).toMatchSnapshot();
     });
   });
 
   // test events
-  // describe('@event', () => {});
+  describe('@event', () => {
+    it('@click-close-btn', async () => {
+      const wrapper = mount({
+        render() {
+          return <Message closeBtn={true}></Message>;
+        },
+      });
+      const msg = wrapper.find(Message);
+      await msg.find('.t-icon-close').trigger('click');
+      expect(msg.emitted()['click-close-btn']).toBeTruthy();
+    });
+
+    it('@duration-end', (done) => {
+      const wrapper = mount({
+        render() {
+          return <Message duration={3000}></Message>;
+        },
+      });
+      const msg = wrapper.find(Message);
+      expect(msg.emitted()['duration-end']).toBeFalsy();
+      const timer = setTimeout(() => {
+        const msg = wrapper.find(Message);
+        expect(msg.emitted()['duration-end']).toBeTruthy();
+        done();
+        clearTimeout(timer);
+      }, 3100);
+    });
+  });
 
   // test slots
   describe('<slot>', () => {
-    // it('', () => {});
-  });
+    it('<default>', () => {
+      const wrapper = mount({
+        render() {
+          return (
+            <Message
+              {...{
+                scopedSlots: {
+                  default: () => <Button class='custom-message'>自定义细心内容</Button>,
+                },
+              }}
+            ></Message>
+          );
+        },
+      });
+      expect(wrapper.contains(Button)).toBe(true);
+      expect(wrapper.contains('.custom-message')).toBe(true);
+    });
 
-  // test exposure function
-  describe('function', () => {
-    // it('', () => {});
+    it('<icon>', () => {
+      const wrapper = mount({
+        render() {
+          return (
+            <Message
+              {...{
+                scopedSlots: {
+                  icon: () => <TIconMore></TIconMore>,
+                },
+              }}
+            ></Message>
+          );
+        },
+      });
+      expect(wrapper.contains(TIconMore)).toBe(true);
+      expect(wrapper).toMatchSnapshot();
+    });
+
+    it('<closeBtn>', () => {
+      const wrapper = mount({
+        render() {
+          return (
+            <Message
+              {...{
+                scopedSlots: {
+                  closeBtn: () => <div class='custome-close-btn'>x</div>,
+                },
+              }}
+            ></Message>
+          );
+        },
+      });
+      expect(wrapper.contains('.custome-close-btn')).toBe(true);
+    });
   });
 });
