d7947b7fda8de16345bb6a639c616a6c0bad1852
gnauhca <576073927@qq.com>
2020-07-01 15:44:05 +0800

    refactor(button): button icon 支持 slot 写法

--- examples/button/demos/icon.vue
+++ examples/button/demos/icon.vue
@@ -3,19 +3,55 @@
     <div class="demo-button-row">
       <t-button
         theme="primary"
-        icon="delete"
+        icon="search"
       >
-        删除
+        搜索
       </t-button>
-      <t-button icon="delete">
-        删除
+      <t-button icon="search">
+        搜索
       </t-button>
       <t-button
         theme="link"
-        icon="delete"
+        icon="search"
       >
-        删除
+        搜索
       </t-button>
+      <t-button
+        theme="primary"
+        icon="search"
+      >
+      </t-button>
+      <t-button
+        icon="search"
+      >
+      </t-button>
+
+      <!-- 使用 function 或 slot 的形式定义 icon -->
+      <t-button
+        :icon="renderIcon"
+      >
+        Function Icon
+      </t-button>
+      <t-button>
+        <template slot="icon">
+          <t-icon name="search"></t-icon>
+        </template>
+        Slot Icon
+      </t-button>
+
     </div>
   </div>
-</template>;
+</template>
+<script>
+export default {
+  methods: {
+    renderIcon(h) {
+      return h('t-icon', {
+        props: {
+          name: 'search',
+        },
+      });
+    },
+  },
+};
+</script>
