66945bfed1af92ef4dbc2d8acd8cb95bffcc2c40
mrjzhang <mrjzhang>
2020-08-07 12:37:27 +0800

    test(table):  del abandon code of async loading and update unit

--- test/unit/table/demo.test.js
+++ test/unit/table/demo.test.js
@@ -3,7 +3,7 @@ import { mount } from '@vue/test-utils';
 // unit test for component in examples.
 describe('Table', () => {
   it('base demo works fine', () => {
-    const demo = require('@/examples/table/demos/base.vue');
+    const demo = require('@/examples/table/demos/base.vue').default;
     const wrapper = mount(demo);
     expect(wrapper.element).toMatchSnapshot();
   });
