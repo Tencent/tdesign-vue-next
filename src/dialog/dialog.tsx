44eed17e6b5b764a845b7b71b807895950f0671f
pengruiqing <pengruiqing@coding.net>
2020-09-27 18:18:48 +0800

    feat(dialog): add close event

--- src/dialog/dialog.tsx
+++ src/dialog/dialog.tsx
@@ -234,15 +234,14 @@ export default Vue.extend({
     },
     keyboardEvent(e: KeyboardEvent) {
       if (e.code === 'Escape') {
-        this.$emit('keydown-esc', this.close, e);
+        this.closeEventEmit('keydown-esc', e);
       }
     },
     overlayAction(e: Event) {
-      this.$emit('click-overlay', this.close, e);
+      this.closeEventEmit('click-overlay', e);
     },
     closeBtnAcion(e: Event) {
-      this.$emit('click-close-btn', this.close, e);
-      this.changeVisible(false);
+      this.closeEventEmit('click-close-btn', e);
     },
     close() {
       this.changeVisible(false);
@@ -251,11 +250,31 @@ export default Vue.extend({
       this.$emit('visible-change', visible);
     },
     cancelBtnAction(e: Event) {
-      this.$emit('click-cancel', this.close, e);
-      this.changeVisible(false);
+      this.closeEventEmit('click-cancel', e);
     },
     confirmBtnAction(e: Event) {
-      this.$emit('click-confirm', this.close, e);
+      this.closeEventEmit('click-confirm', e);
+    },
+    // 先判断是否有该事件，有则派发当前事件，传递close函数出去
+    // 再判断是否有close事件，有则派发，传递close函数出去
+    // 前面都没有则执行关闭组件的方法
+    closeEventEmit(name: string, event: Event) {
+      if (this.hasEventOn(name)) {
+        this.$emit(name, this.close, event);
+      } else if (this.hasEventOn('close')) {
+        this.$emit('close', this.close, event);
+      } else {
+        this.close();
+      }
+    },
+    // Vue在引入阶段对事件的处理还做了哪些初始化操作。Vue在实例上用一个_events属性存贮管理事件的派发和更新，
+    // 暴露出$on, $once, $off, $emit方法给外部管理事件和派发执行事件
+    // 所以通过判断_events某个事件下监听函数数组是否超过一个，可以判断出组件是否监听了当前事件
+    hasEventOn(name: string) {
+      // _events 因没有被暴露在vue实例接口中，只能把这个规则注释掉
+      /* eslint-disable dot-notation */
+      const eventFuncs = this['_events']?.[name];
+      return !!eventFuncs?.length;
     },
     renderTitle() {
       const target = this.header;
