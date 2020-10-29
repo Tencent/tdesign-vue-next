bb71b5f00c482762bfb3a2ce3a3e0c5a7bad7eb1
candy-Tong <563378816@qq.com>
2020-10-29 18:26:09 +0800

    feat: 实现分割线组件

---
+++ examples/divider/demos/vertical.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <span>正直</span>
+    <t-divider type="vertical" />
+    <span>进取</span>
+    <t-divider type="vertical" />
+    <span>合作</span>
+    <t-divider type="vertical" />
+    <span>创新</span>
+  </div>
+</template>
