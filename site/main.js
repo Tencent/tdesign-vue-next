f05d0e5119cc49b0a936fc679ee31cad8d154186
gnauhca <576073927@qq.com>
2020-11-06 15:46:47 +0800

    chore: use hash router in cdngo preview mode

--- site/main.js
+++ site/main.js
@@ -24,8 +24,9 @@ Vue.component('tdesign-layout', Layout);
 Vue.component('tdesign-demo', Demo);
 Vue.component('tdesign-component-contributors', ComponentContributors);
 
+console.log('process.env.NODE_ENV', process.env.NODE_ENV);
 const router = new VueRouter({
-  mode: 'history',
+  mode: process.env.PUBLICPATH === './' ? 'hash' : 'history',
   base: 'vue',
   routes,
 });
