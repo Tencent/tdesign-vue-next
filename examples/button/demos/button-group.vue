db958efecaf66376984f426b42dccb3d6dd056fe
HQ-Lin <a154571896@gmail.com>
2020-11-18 16:43:35 +0800

    feat: 改造 button demo页

---
+++ examples/button/demos/button-group.vue
@@ -0,0 +1,5 @@
+<template>
+  <t-button theme="primary">
+    按钮组
+  </t-button>
+</template>
---
+++ examples/button/demos/ghost.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="tdesign-demo-block">
+    <t-button theme="ghost">幽灵按钮</t-button>
+    <t-button theme="ghost" disabled>幽灵按钮</t-button>
+  </div>
+</template>
