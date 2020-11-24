56d429b829f3e1cc32abb5f5bdbeab59fa0b53e9
v_vmlgong(龚模良) <v_vmlgong()>
2020-11-24 19:30:37 +0800

    radio Demo 改造

--- examples/radio/demos/radio-button.vue
+++ examples/radio/demos/radio-button.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="tdesign-radio-button">
     <t-radio-group>
-      <t-radio-button value="bj">北京</t-radio-button>
-      <t-radio-button value="sh">上海</t-radio-button>
-      <t-radio-button value="gz">广州</t-radio-button>
-      <t-radio-button value="sz">深圳</t-radio-button>
+      <t-radio-button value="1">选项一</t-radio-button>
+      <t-radio-button value="2">选项二</t-radio-button>
+      <t-radio-button value="3" :checked="true" disabled>选项三</t-radio-button>
+      <t-radio-button value="4" :checked="false" disabled>选项四</t-radio-button>
     </t-radio-group>
     <br />
     <t-radio-group buttonStyle="solid">
-      <t-radio-button value="bj">北京</t-radio-button>
-      <t-radio-button value="sh">上海</t-radio-button>
-      <t-radio-button value="gz">广州</t-radio-button>
-      <t-radio-button value="sz">深圳</t-radio-button>
+      <t-radio-button value="bj">选项一</t-radio-button>
+      <t-radio-button value="sh">选项二</t-radio-button>
+      <t-radio-button value="3" :checked="true" disabled>选项三</t-radio-button>
+      <t-radio-button value="sz">选项四</t-radio-button>
     </t-radio-group>
   </div>
 </template>
