90f4d2420867def54aaa352e8a3fcbc88f80cd40
v_vmlgong(龚模良) <v_vmlgong()>
2020-11-24 15:16:12 +0800

    steps Demo 改造

--- examples/steps/demos/vertical.vue
+++ examples/steps/demos/vertical.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
-    <t-steps direction="vertical" :current="1" status="process">
+  <div class="setps-demo">
+    <div class="tdesign-demo-block">
+      <t-steps direction="vertical" :current="1" status="process">
       <t-step title="已完成的步骤" content="这里是提示文字"></t-step>
       <t-step title="进行中的步骤" content="这里是提示文字"></t-step>
       <t-step title="未进行的步骤" content="这里是提示文字"></t-step>
@@ -14,4 +15,13 @@
       <t-step title="未进行的步骤" content="这里是提示文字"></t-step>
     </t-steps>
   </div>
+
+    </div>
 </template>
+
+<style scoped>
+.setps-demo .tdesign-demo-block {
+  display: flex;
+  justify-content: space-around;
+}
+</style>
