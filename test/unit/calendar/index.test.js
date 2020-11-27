7490d48ee308f61793f41de7fea6a56cab4caccc
PsTiu <20109955@qq.com>
2020-11-27 21:50:17 +0800

    chore: add test

--- test/unit/calendar/index.test.js
+++ test/unit/calendar/index.test.js
@@ -13,6 +13,64 @@ describe('Calendar', () => {
       });
       expect(wrapper.isEmpty()).toBe(false);
     });
+
+    it(':defaultValue', () => {
+      const wrapper = mount({
+        render() {
+          return <Calendar defaultValue={new Date(1998, 10, 11)}></Calendar>;
+        },
+      });
+      expect(wrapper).toMatchSnapshot();
+    });
+
+    it(':mode', () => {
+      const wrapper = mount({
+        render() {
+          return <Calendar mode={'year'}></Calendar>;
+        },
+      });
+      expect(wrapper).toMatchSnapshot();
+    });
+
+    it(':firstDayOfWeek', () => {
+      const wrapper = mount({
+        render() {
+          return <Calendar firstDayOfWeek={3}></Calendar>;
+        },
+      });
+      expect(wrapper).toMatchSnapshot();
+    });
+
+    it(':isShowWeekendDefault', () => {
+      const wrapper = mount({
+        render() {
+          return <Calendar isShowWeekendDefault={false}></Calendar>;
+        },
+      });
+      expect(wrapper).toMatchSnapshot();
+    });
+
+    it(':isShowWeekendDefault', () => {
+      const wrapper = mount({
+        render() {
+          const testRange = {
+            from: new Date(1998, 7),  // 1998-08
+            to: new Date(2008, 3),  // 1998-04
+          };
+          return <Calendar range={testRange}></Calendar>;
+        },
+      });
+      expect(wrapper).toMatchSnapshot();
+    });
+
+    it(':theme', () => {
+      const wrapper = mount({
+        render() {
+          return <Calendar theme={'card'}></Calendar>;
+        },
+      });
+      expect(wrapper).toMatchSnapshot();
+    });
   });
 
   // test events
