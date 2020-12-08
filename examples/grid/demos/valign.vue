668e89f9274c38dfe937a0022516eff58eb79b3f
HQ-Lin <a154571896@gmail.com>
2020-12-08 16:49:49 +0800

    fix: 调整为12栅格

--- examples/grid/demos/valign.vue
+++ examples/grid/demos/valign.vue
@@ -3,13 +3,13 @@
     <p>align top</p>
     <t-row align="top" justify="center">
       <t-col :span="3">
-        <div style="height: 100px">col-3</div>
+        <div style="height: 100px;">col-3</div>
       </t-col>
       <t-col :span="3">
         <div>col-3</div>
       </t-col>
       <t-col :span="3">
-        <div style="height: 100px">col-3</div>
+        <div style="height: 100px;">col-3</div>
       </t-col>
       <t-col :span="3">
         <div>col-3</div>
@@ -19,13 +19,13 @@
     <p>align middle</p>
     <t-row align="middle" justify="space-around">
       <t-col :span="3">
-        <div style="height: 100px">col-3</div>
+        <div style="height: 100px;">col-3</div>
       </t-col>
       <t-col :span="3">
         <div>col-3</div>
       </t-col>
       <t-col :span="3">
-        <div style="height: 100px">col-3</div>
+        <div  style="height: 100px;">col-3</div>
       </t-col>
       <t-col :span="3">
         <div>col-3</div>
@@ -35,13 +35,13 @@
     <p>align bottom</p>
     <t-row align="bottom" justify="space-between">
       <t-col :span="3">
-        <div style="height: 100px">col-3</div>
+        <div  style="height: 100px;">col-3</div>
       </t-col>
       <t-col :span="3">
         <div>col-3</div>
       </t-col>
       <t-col :span="3">
-        <div style="height: 100px">col-3</div>
+        <div  style="height: 100px;">col-3</div>
       </t-col>
       <t-col :span="3">
         <div>col-3</div>
