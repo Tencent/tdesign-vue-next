23b11a9370c5fdd513a2dee76ad802b8753ff711
sharryling <jing_501375481@qq.com>
2020-12-08 14:17:30 +0800

    test(transfer unit tst): unit test

--- test/unit/transfer/index.test.js
+++ test/unit/transfer/index.test.js
@@ -27,6 +27,7 @@ const pagination = {
 };
 const checkedValue = ['1', '2', '5'];
 const targetValue = ['1'];
+
 describe('Transfer', () => {
   // test for props
   describe('Transfer:props', () => {
@@ -36,92 +37,107 @@ describe('Transfer', () => {
     });
 
     it(':data', async () => {
-      const wrapper = mount({
+      const wrapper = await mount({
         render() {
           return <Transfer data={data} pagination={pagination}></Transfer>;
         },
       });
-      await timeoutPromise(() => {
-        const len = wrapper.vm.$el.getElementsByTagName('li').length; // wrapper.vm.$el.getElementsByTagName('li').length
-        expect(len).toBe(data.length);
-        expect(wrapper.vm.$el.getElementsByClassName('t-checkbox t-is-disabled').length).toBe(7);
-      });
+
+      const len = wrapper.vm.$el.getElementsByTagName('li').length; // wrapper.vm.$el.getElementsByTagName('li').length
+      expect(len).toBe(data.length);
+      expect(wrapper.vm.$el.getElementsByClassName('t-checkbox t-is-disabled').length).toBe(7);
     });
 
     it(':checkedValue', async () => {
-      const wrapper = mount({
+      const wrapper = await mount({
         render() {
           return <Transfer data={data} checkedValue={checkedValue} pagination={pagination} />;
         },
       });
 
-      await timeoutPromise(() => {
-        checkedValue.forEach((item) => {
-          const i = Number(item);
-          const domLi = wrapper.vm.$el.querySelectorAll('.t-transfer-list-source')[0].querySelectorAll('li');
-          const [dom] = domLi[i].querySelectorAll('label');
-          expect(dom.className.indexOf('t-is-checked') > -1).toBe(true);
-        });
+      checkedValue.forEach((item) => {
+        const i = Number(item);
+        const domLi = wrapper.vm.$el.querySelectorAll('.t-transfer-list-source')[0].querySelectorAll('li');
+        const [dom] = domLi[i].querySelectorAll('label');
+        expect(dom.className.indexOf('t-is-checked') > -1).toBe(true);
       });
     });
 
     it(':targetValue', async () => {
-      const wrapper = mount({
+      const wrapper = await mount({
         render() {
           return <Transfer data={data} targetValue={targetValue} pagination={pagination}></Transfer>;
         },
       });
 
-      await timeoutPromise(() => {
-        let count = 0;
-        targetValue.forEach((item) => {
-          const i = Number(item);
-          const domLi = wrapper.vm.$el.querySelectorAll('.t-transfer-list-target')[0].querySelectorAll('li');
-          const dom = domLi[count].querySelectorAll('span');
-          expect(dom[2].innerHTML).toBe(`内容${i + 1}`);
-          count = count + 1;
-        });
+      let count = 0;
+      targetValue.forEach((item) => {
+        const i = Number(item);
+        const domLi = wrapper.vm.$el.querySelectorAll('.t-transfer-list-target')[0].querySelectorAll('li');
+        const dom = domLi[count].querySelectorAll('span');
+        expect(dom[2].innerHTML).toBe(`内容${i + 1}`);
+        count = count + 1;
       });
     });
 
     it(':disabled', async () => {
       const fn = jest.fn();
-      const wrapper = mount({
+      const wrapper = await mount({
         render() {
           return <Transfer data={data} disabled={true} checkChange={fn} pagination={pagination}></Transfer>;
         },
       });
 
-      await timeoutPromise(() => {
-        wrapper.vm.$el.getElementsByClassName('t-checkbox t-is-disabled')[0].click();
-        expect(fn).not.toHaveBeenCalled();
+      wrapper.vm.$el.getElementsByClassName('t-checkbox t-is-disabled')[0].click();
+      expect(fn).not.toHaveBeenCalled();
 
-        const doms = wrapper.vm.$el.getElementsByClassName('t-checkbox t-is-disabled');
-        expect(doms.length).toBe(data.length + 2);
-      });
+      const doms = wrapper.vm.$el.getElementsByClassName('t-checkbox t-is-disabled');
+      expect(doms.length).toBe(data.length + 2);
     });
 
-    it(':search', () => {
-      const wrapper = mount(Transfer, {
-        propsData: {
-          search: false,
+    it(':search', async () => {
+      const wrapper = await mount({
+        render() {
+          return <Transfer data={data} search={true}></Transfer>;
         },
       });
-      expect(wrapper.vm.$el.getElementsByClassName('t-input__inner').length).toBe(0);
-
-      const wrapper2 = mount(Transfer, {
-        propsData: {
-          search: true,
-        },
-      });
-      expect(wrapper2.vm.$el.getElementsByClassName('t-input__inner').length).toBe(2);
+      expect(wrapper.vm.$el.getElementsByClassName('t-input__inner').length).toBe(2);
     });
 
-    it(':empty', () => {
+    // it(':titles', () => {
+    //   const wrapper = mount(Transfer, {
+    //     propsData: {
+    //       titles: ['源列表', '目标列表'],
+    //     },
+    //   });
+    //   console.log('====/title:' , wrapper.vm.$el.getElementsByClassName('t-transfer-list__header-title')[0])
+    //   // expect(wrapper.vm.$el.getElementsByClassName('t-transfer-list__header-title')[0].innerText === '源列表').toBe(true);
+    //   // expect(wrapper.vm.$el.getElementsByClassName('t-transfer-list__header-title')[1].innerText === '目标列表').toBe(true);
+    // });
+
+    it(':pagination', async () => {
+      const pagConfig = {
+        pageSize: 8,
+        total: 20,
+        current: 1,
+      };
+      const { pageSize } = pagConfig;
+      const wrapper = await mount({
+        render() {
+          return <Transfer data={data} pagination={pagConfig}></Transfer>;
+        },
+      });
+      const el = wrapper.vm.$el;
+
+      const len = el.querySelectorAll('li').length; // wrapper.vm.$el.getElementsByTagName('li').length
+      expect(len).toBe(pageSize);
+    });
+
+    it(':empty', async () => {
       const empty = 'empty 空';
-      const wrapper = mount(Transfer, {
-        propsData: {
-          empty,
+      const wrapper = await mount({
+        render() {
+          return <Transfer empty={empty}></Transfer>;
         },
       });
       expect(wrapper.vm.$el.getElementsByClassName('t-transfer-empty')[0].innerHTML === empty).toBe(true);
@@ -131,7 +147,7 @@ describe('Transfer', () => {
   describe('@event', () => {
     it('@oncheckChange', async () => {
       const fn = jest.fn();
-      const wrapper = mount({
+      const wrapper = await mount({
         render() {
           return <Transfer data={data} pagination={pagination} oncheckChange={fn} />;
         },
@@ -142,12 +158,12 @@ describe('Transfer', () => {
         expect(fn).toHaveBeenCalled();
         const doms = wrapper.vm.$el.getElementsByClassName('t-checkbox');
         expect(doms[2].className).toBe('t-checkbox t-is-checked');
-      }, 2000);
+      }, 1000);
     });
 
     it('@change', async () => {
       const fn = jest.fn();
-      const wrapper = mount({
+      const wrapper = await mount({
         render() {
           return (
             <Transfer
