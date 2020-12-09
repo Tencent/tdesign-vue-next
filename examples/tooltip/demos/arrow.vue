f4c4335a8beed2f579b41ac49edbb45d89e2e2c2
mixiaola <fezhangnan@sina.com>
2020-12-09 16:57:31 +0800

    fix: 去除多余参数

--- examples/tooltip/demos/arrow.vue
+++ examples/tooltip/demos/arrow.vue
@@ -1,5 +1,5 @@
 <template>
-  <t-tooltip content="文字提示仅展示文本内容" :visibleArrow=true placement="top">
+  <t-tooltip content="文字提示仅展示文本内容" :visibleArrow=true>
     <t-button>带箭头的文字提示</t-button>
   </t-tooltip>
 </template>
