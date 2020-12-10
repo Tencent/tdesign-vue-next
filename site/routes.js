307e199068c532a8d9a983ea628f307c6700a11f
chrysalis1215 <785851812@qq.com>
2020-12-10 14:45:21 +0800

    feat(site): component按照字典序排序

--- site/routes.js
+++ site/routes.js
@@ -15,7 +15,17 @@ function getDocsRoutes(docs, type) {
     const docType = item.type || type;
     let { children } = item;
     if (item.type === 'component') {
-      children = item.children.sort();
+      children = item.children.sort((a, b) => {
+        const nameA = a.name.toUpperCase();
+        const nameB = b.name.toUpperCase();
+        if (nameA < nameB) {
+          return -1;
+        }
+        if (nameA > nameB) {
+          return 1;
+        }
+        return 0;
+      });
     }
     if (children) {
       docsRoutes = docsRoutes.concat(getDocsRoutes(children, docType));
