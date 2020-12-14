044b660c9713c132f452e8e9a806fd5579c2e820
chaishi <974383157@qq.com>
2020-12-14 10:41:02 +0800

    refactor(classnames): rename middle to medium

--- src/utils/classnames.ts
+++ src/utils/classnames.ts
@@ -1,32 +1,37 @@
 import config from '../config';
 
 const { prefix } = config;
+
+export const SIZE_CLASSNAMES = {
+  small: `${prefix}-size-s`,
+  medium: `${prefix}-size-m`,
+  large: `${prefix}-size-l`,
+  default: '',
+  xs: `${prefix}-size-xs`,
+  xl: `${prefix}-size-xl`,
+  block: `${prefix}-size-full-width`,
+};
+
+export const STATUS_CLASSNAMES = {
+  loading: `${prefix}-is-loading`,
+  disabled: `${prefix}-is-disabled`,
+  focused: `${prefix}-is-focused`,
+  success: `${prefix}-is-success`,
+  error: `${prefix}-is-error`,
+  warning: `${prefix}-is-warning`,
+  selected: `${prefix}-is-selected`,
+  active: `${prefix}-is-active`,
+  checked: `${prefix}-is-checked`,
+  current: `${prefix}-is-current`,
+  hidden: `${prefix}-is-hidden`,
+  visible: `${prefix}-is-visible`,
+  expanded: `${prefix}-is-expanded`,
+  indeterminate: `${prefix}-is-indeterminate`,
+};
+
 const ClASSNAMES = {
-  SIZE: {
-    default: '',
-    xs: `${prefix}-size-xs`,
-    small: `${prefix}-size-s`,
-    middle: `${prefix}-size-m`,
-    large: `${prefix}-size-l`,
-    xl: `${prefix}-size-xl`,
-    block: `${prefix}-size-full-width`,
-  },
-  STATUS: {
-    loading: `${prefix}-is-loading`,
-    disabled: `${prefix}-is-disabled`,
-    focused: `${prefix}-is-focused`,
-    success: `${prefix}-is-success`,
-    error: `${prefix}-is-error`,
-    warning: `${prefix}-is-warning`,
-    selected: `${prefix}-is-selected`,
-    active: `${prefix}-is-active`,
-    checked: `${prefix}-is-checked`,
-    current: `${prefix}-is-current`,
-    hidden: `${prefix}-is-hidden`,
-    visible: `${prefix}-is-visible`,
-    expanded: `${prefix}-is-expanded`,
-    indeterminate: `${prefix}-is-indeterminate`,
-  },
+  SIZE: SIZE_CLASSNAMES,
+  STATUS: STATUS_CLASSNAMES,
 };
 
 export default ClASSNAMES;
