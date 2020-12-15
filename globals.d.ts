4348e47082d32420679e44e7d09900d5cca38327
dellyoung <dellyoung@126.com>
2020-12-15 17:07:04 +0800

    feat(form): add TS TNode

--- globals.d.ts
+++ globals.d.ts
@@ -2,3 +2,4 @@ declare const __VERSION__: string;
 
 declare type ClassName = { [className: string]: any } | ClassName[] | string;
 declare type JsxNode = import('vue').VNode | import('vue').VNode[] | string | undefined | null | JsxNode[];
+declare type TNode = string | (() => import('vue').VNode | Element);
