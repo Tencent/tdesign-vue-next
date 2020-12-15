452bf2a08ea7b78c25950c90167c9d3dc06e00bb
enicli <enicli>
2020-12-15 16:50:19 +0800

    feat(grid): 补充一些grid的demo

--- examples/grid/demos/gutter.vue
+++ examples/grid/demos/gutter.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="tdesign-demo-item--grid">
+    <!-- 只指定水平gutter，垂直默认为0 -->
+    <span>水平gutter为固定值</span>
     <t-row :gutter="16">
       <t-col :span="3">
         <div>col-3</div>
@@ -15,6 +17,8 @@
       </t-col>
     </t-row>
 
+    <!-- 水平gutter为响应式 -->
+    <span>水平gutter为响应式</span>
     <t-row :gutter="{ xs: 8, sm: 16, md: 24, lg: 32, xl: 32, xxl: 40 }">
       <t-col :span="3">
         <div>col-3</div>
@@ -30,32 +34,30 @@
       </t-col>
     </t-row>
 
+    <!-- 指定水平和垂直gutter，注：垂直gutter只在同一个row组件下面的col组件之间生效 -->
+    <span>水平和垂直gutter均为固定值</span>
     <t-row :gutter="[16, 24]">
-      <t-col :span="3">
+      <t-col v-for="i in 8" :key="i" :span="3">
         <div>col-3</div>
       </t-col>
-      <t-col :span="3">
+    </t-row>
+
+    <!-- 水平gutter响应式，垂直gutter固定值-->
+    <span>水平gutter响应式，垂直gutter固定值</span>
+    <t-row :gutter="[{ xs: 8, sm: 16, md: 24, lg: 32, xl: 32, xxl: 40 }, 24]">
+      <t-col v-for="i in 8" :key="i" :span="3">
         <div>col-3</div>
       </t-col>
-      <t-col :span="3">
-        <div>col-3</div>
-      </t-col>
-      <t-col :span="3">
-        <div>col-3</div>
-      </t-col>
-      <t-col :span="3">
-        <div>col-3</div>
-      </t-col>
-      <t-col :span="3">
-        <div>col-3</div>
-      </t-col>
-      <t-col :span="3">
-        <div>col-3</div>
-      </t-col>
-      <t-col :span="3">
+    </t-row>
+
+    <!-- 水平和垂直gutter均为响应式 -->
+    <span>水平和垂直gutter均为响应式</span>
+    <t-row :gutter="[
+      { xs: 8, sm: 16, md: 24, lg: 32, xl: 32, xxl: 40 },
+      { xs: 8, sm: 16, md: 24, lg: 32, xl: 32, xxl: 40 }]">
+      <t-col v-for="i in 8" :key="i" :span="3">
         <div>col-3</div>
       </t-col>
     </t-row>
   </div>
 </template>
-
--- examples/grid/demos/responsive.vue
+++ examples/grid/demos/responsive.vue
@@ -1,10 +1,23 @@
 <template>
   <div class="tdesign-demo-item--grid">
+    <span>宽度响应式</span>
     <t-row>
       <t-col :xs="2" :sm="4" :md="6" :lg="8" :xl="10">
         <div>Col</div>
       </t-col>
       <t-col :xs="10" :sm="8" :md="6" :lg="4" :xl="2">
+        <div>Col</div>
+      </t-col>
+    </t-row>
+
+    <span>其他属性响应式（支持span，offset，order，pull，push）</span>
+    <t-row>
+      <t-col
+        :xs="{offset: 0, span: 3}"
+        :sm="{offset: 2, span: 3}"
+        :md="{offset: 4, span: 3}"
+        :lg="{offset: 6, span: 3}"
+        :xl="{offset: 8, span: 3}">
         <div>Col</div>
       </t-col>
     </t-row>
