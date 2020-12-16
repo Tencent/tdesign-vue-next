7f08bcbf781bf7a9a44c1a791b78947ed072fe1d
xiaosansiji <799552341@qq.com>
2020-12-16 20:41:55 +0800

    fix: button 不允许通过 icon= 'string' 来设置图标了，demo 中去除相关设置，防止官网运行时报错

--- examples/calendar/demos/controllerConfig.vue
+++ examples/calendar/demos/controllerConfig.vue
@@ -56,16 +56,15 @@
 </template>
 
 <script>
-
 export default {
   data() {
     return {
       controllerConfig: {
-        visible: true,	// 是否显示（全部控件）
-        disabled: false,	// 是否禁用（全部控件）
+        visible: true, // 是否显示（全部控件）
+        disabled: false, // 是否禁用（全部控件）
         // 模式切换单选组件设置
         mode: {
-          visible: true,	// 是否显示
+          visible: true, // 是否显示
           radioGroupProps: {
             disabled: false,
             buttonStyle: 'solid',
@@ -73,46 +72,48 @@ export default {
         },
         // 年份选择框组件相关设置
         year: {
-          visible: true,	// 是否显示
-          selecteProps: {	// 用于透传props给该select组件
+          visible: true, // 是否显示
+          selecteProps: {
+            // 用于透传props给该select组件
             disabled: false,
             prefixIcon: 'discount_fill',
           },
         },
         // 年份选择框组件相关设置
         month: {
-          visible: true,	// 是否显示（“year”模式下本身是不显示该组件的）
-          selecteProps: {	// 用于透传props给该select组件
+          visible: true, // 是否显示（“year”模式下本身是不显示该组件的）
+          selecteProps: {
+            // 用于透传props给该select组件
             disabled: false,
             prefixIcon: 'discount',
           },
         },
         // 隐藏\显示周末按钮组件相关设置
         weekendToggle: {
-          visible: true,	// 是否显示
-          showWeekendButtonProps: { // 用于透传props给显示周末按钮组件
+          visible: true, // 是否显示
+          showWeekendButtonProps: {
+            // 用于透传props给显示周末按钮组件
             disabled: false,
             theme: 'primary',
-            icon: 'browse',
           },
-          hideWeekendButtonProps: { // 用于透传props给隐藏周末按钮组件
+          hideWeekendButtonProps: {
+            // 用于透传props给隐藏周末按钮组件
             disabled: false,
             theme: 'warning',
-            icon: 'browse-off',
           },
         },
         // “今天\本月”按钮组件相关设置
         current: {
-          visible: true,	// 是否显示
-          currentDayButtonProps: { // 用于透传props给“今天”钮组件（“month”模式下有效）
+          visible: true, // 是否显示
+          currentDayButtonProps: {
+            // 用于透传props给“今天”钮组件（“month”模式下有效）
             disabled: false,
             theme: 'primary',
-            icon: 'rollback',
           },
-          currentMonthButtonProps: { // 用于透传props给“本月”按钮组件（“year”模式下有效）
+          currentMonthButtonProps: {
+            // 用于透传props给“本月”按钮组件（“year”模式下有效）
             disabled: false,
             theme: 'primary',
-            icon: 'rollback',
           },
         },
       },
