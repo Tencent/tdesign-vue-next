5835beca9fd19da91784246df20a3048a6ef3aa9
chaishi <974383157@qq.com>
2020-12-20 15:32:32 +0800

    feat(api): add some more globals

--- globals.d.ts
+++ globals.d.ts
@@ -1,5 +1,24 @@
 declare const __VERSION__: string;
 
-declare type ClassName = { [className: string]: any } | ClassName[] | string;
 declare type JsxNode = import('vue').VNode | import('vue').VNode[] | string | undefined | null | JsxNode[];
 declare type TNode = () => import('vue').VNode | Element;
+
+
+declare type TdVueNode = string | number | boolean | undefined | Vue.VNode | Vue.VNode[] | TdVueNode[];
+
+declare type OptionData = {
+  label?: string;
+  value?: string | number;
+} & { [key: string]: any };
+
+declare type SizeEnum = 'small' | 'medium' | 'large';
+
+declare type HorizontalAlignEnum = 'left' | 'center' | 'right';
+
+declare type VerticalAlignEnum = 'top' | 'middle' | 'bottom';
+
+declare type ClassName = { [className: string]: any } | ClassName[] | string;
+
+declare type CSSSelector = string;
+
+declare type AttachNode = CSSSelector | (() => (Window | HTMLDocument | Element));
