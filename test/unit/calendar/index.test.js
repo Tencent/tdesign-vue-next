c558f271b891c61207c455e2cca3a5d4d1c44ada
joriewong <stillba@icloud.com>
2020-12-28 15:16:44 +0800

    test: update calendar test

--- test/unit/calendar/index.test.js
+++ test/unit/calendar/index.test.js
@@ -17,7 +17,7 @@ describe('Calendar', () => {
     it(':defaultValue', () => {
       const wrapper = mount({
         render() {
-          return <Calendar defaultValue={new Date(1998, 10, 11)}></Calendar>;
+          return <Calendar defaultValue={new Date(1998, 10, 11).toUTCString()}></Calendar>;
         },
       });
       expect(wrapper).toMatchSnapshot();
