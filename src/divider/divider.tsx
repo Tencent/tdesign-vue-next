4637063c26f548c6ecc61ba88e031daa3579e842
chaishi <974383157@qq.com>
2021-01-05 19:41:22 +0800

    feat(divider): rename type to theme; rename orientation to align

--- src/divider/divider.tsx
+++ src/divider/divider.tsx
@@ -1,63 +1,26 @@
 import Vue from 'vue';
 import { prefix } from '../config';
+import props from '@TdTypes/divider/props';
 
 const name = `${prefix}-divider`;
 
 export default Vue.extend({
   name,
 
-  props: {
-    type: {
-      type: String,
-      default: 'horizontal',
-      validator(v: string): boolean {
-        return (
-          [
-            'horizontal',
-            'vertical',
-          ].indexOf(v) > -1
-        );
-      },
-    },
-    dashed: {
-      type: Boolean,
-      default: false,
-      validator(v: any): boolean {
-        return typeof v === 'boolean';
-      },
-    },
-    orientation: {
-      type: String,
-      default: 'center',
-      validator(v: string): boolean {
-        return (
-          [
-            'center',
-            'left',
-            'right',
-          ].indexOf(v) > -1
-        );
-      },
-    },
-  },
+  props: { ...props },
 
   render() {
-    const {
-      type = 'horizontal',
-      dashed,
-      orientation = 'center',
-    } = this.$props;
+    const { theme, dashed, align } = this;
 
     const children = this.$slots.default;
 
     const dividerClassNames = [
       `${name}`,
+      [`${prefix}-divider--${theme}`],
       {
-        [`${prefix}-divider--horizontal`]: type === 'horizontal',
-        [`${prefix}-divider--vertical`]: type === 'vertical',
         [`${prefix}-divider--dashed`]: !!dashed,
         [`${prefix}-divider--with-text`]: !!children,
-        [`${prefix}-divider--with-text-${orientation}`]: !!children,
+        [`${prefix}-divider--with-text-${align}`]: !!children,
       },
     ];
 
