ffb82852a96c6128eb30ec1f532d0fa6d7cda495
chaishi <974383157@qq.com>
2021-01-18 21:19:32 +0800

    docs(notification): add duration example

--- examples/notification/demos/base.vue
+++ examples/notification/demos/base.vue
@@ -5,9 +5,26 @@
       title="标题名称"
       content="这是一条消息通知"
     />
+    <t-notification
+      v-if="show"
+      theme="info"
+      title="标题名称"
+      content="这是一条消息通知（展示5秒后消失）"
+      :duration="5000"
+      @duration-end="show = false"
+    />
+    <t-button @click="show = true" v-if="!show">点击打开计时通知</t-button>
   </div>
 </template>
-
+<script>
+export default {
+  data() {
+    return {
+      show: true,
+    };
+  },
+};
+</script>
 <style scoped>
 .t-notification + .t-notification {
   margin-top: 20px;
