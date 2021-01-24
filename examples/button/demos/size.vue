fb5e17b14b3f4b90f85d210783e32f0d8bc2de1b
chaishi <974383157@qq.com>
2021-01-24 18:24:12 +0800

    docs(button): add shape and size examples

--- examples/button/demos/size.vue
+++ examples/button/demos/size.vue
@@ -19,4 +19,22 @@
       <t-button size="large" variant="text">文字按钮</t-button>
     </div>
   </div>
-</template>;
+</template>
+
+<style lang="less" scoped>
+.tdesign-demo-item__body {
+  .t-button + .t-button {
+    margin-left: 32px;
+  }
+  .tdesign-demo-block {
+    margin-top: 16px;
+  }
+}
+.tdesign-wrap {
+  display: inline-block;
+  background-color: #bbbbbb;
+  padding: 10px;
+  margin-right: 10px;
+
+}
+</style>
