93c181837ed0b94207519fc3ab65c840d1a14ff8
chaishi <974383157@qq.com>
2021-02-19 21:35:58 +0800

    refactor(tooltip): user generate types/props/api.md

--- src/popup/index.ts
+++ src/popup/index.ts
@@ -1,7 +1,9 @@
 import Popup from './popup.vue';
 import setInstallFn from '../utils/setInstallFn';
+import { TdPopupProps } from '@TdTypes/popup/TdPopupProps';
 
 setInstallFn('Popup', Popup);
 
+export type PopupProps = TdPopupProps;
 export { Popup };
 export default Popup;
