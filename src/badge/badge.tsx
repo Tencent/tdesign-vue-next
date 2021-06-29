bb4a0b9ed6f4d3825293016fc5c7a8015be962e8
LeeJim <273743552@qq.com>
2021-06-29 15:16:59 +0800

    fix(badge): errors

--- src/badge/badge.tsx
+++ src/badge/badge.tsx
@@ -1,72 +1,26 @@
-import { defineComponent, PropType } from 'vue';
+import { defineComponent } from 'vue';
 import { prefix } from '../config';
-import RenderComponent from '../utils/render-component';
+import props from '../../types/badge/props';
+import { renderTNodeJSX } from '../utils/render-tnode';
 
 const name = `${prefix}-badge`;
 
 export default defineComponent({
   name,
 
-  components: {
-    RenderComponent,
-  },
+  props: { ...props },
 
-  props: {
-    count: {
-      type: Number,
-      default: 0,
-    },
-    maxCount: {
-      type: Number,
-      default: 99,
-    },
-    content: {
-      type: [String, Function],
-      default: '',
-    },
-    dot: {
-      type: Boolean,
-    },
-    color: {
-      type: String,
-      default: '#E34D59',
-    },
-    shape: {
-      type: String,
-      default: 'circle',
-      validator(v: string): boolean {
-        return ['circle', 'round'].includes(v);
-      },
-    },
-    size: {
-      type: String,
-      default: 'medium',
-      validator(v: string): boolean {
-        return ['medium', 'small'].includes(v);
-      },
-    },
-    showZero: {
-      type: Boolean,
-    },
-    offset: {
-      type: Array as PropType<Array<number>>,
-      default: {},
-      validator(v: number[]): boolean {
-        return v.length === 2;
-      },
-    },
-  },
   methods: {
     getContent() {
       if (this.dot) return '';
-      if (typeof this.content === 'string') {
-        return this.content;
-      } if (typeof  this.content === 'function') {
-        return this.content();
+      if (typeof this.count === 'function') {
+        return renderTNodeJSX(this, 'count');
       }
-      if (typeof this.count === 'number') {
-        return this.count > this.maxCount ? `${this.maxCount}+` : this.count;
+      if (isNaN(Number(this.count))) {
+        return this.count;
       }
+      const count = Number(this.count);
+      return count > this.maxCount ? `${this.maxCount}+` : count;
     },
     isSmall() {
       return this.size === 'small';
@@ -79,18 +33,16 @@ export default defineComponent({
       return !this.showZero && this.isZero();
     },
     getOffset() {
-      if (!this.offset) return { xOffset: void 0, yOffset: void 0 };
-      const [xOffset, yOffset] = this.offset;
+      if (!this.offset) return {};
+      let [xOffset, yOffset]: Array<string | number> = this.offset;
+      xOffset = isNaN(Number(xOffset)) ? xOffset : `${xOffset}px`;
+      yOffset = isNaN(Number(yOffset)) ? yOffset : `${yOffset}px`;
       return { xOffset, yOffset };
     },
   },
 
   render() {
-    const {
-      dot,
-      shape,
-      color,
-    } = this.$props;
+    const { dot, shape, color } = this.$props;
 
     const content = this.getContent();
     const isHidden = this.isHidden();
@@ -101,14 +53,13 @@ export default defineComponent({
         [`${name}--dot`]: !!dot,
         [`${name}--circle`]: !dot && shape === 'circle',
         [`${name}--round`]: shape === 'round',
-        [`${name}--ribbon`]: shape === 'ribbon',
         ['t-size-s']: this.isSmall(),
       },
     ];
     const inlineStyle = {
       background: `${color}`,
-      right: xOffset ? `${-xOffset}px` : void 0,
-      top: yOffset ? `${-yOffset}px` : void 0,
+      right: xOffset,
+      top: yOffset,
     };
 
     return (
