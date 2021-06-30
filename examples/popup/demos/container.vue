422f83570752925302a049e7ab30fd548851037a
LeeJim <273743552@qq.com>
2021-06-30 15:59:11 +0800

    chore(popup): update demo

--- examples/popup/demos/container.vue
+++ examples/popup/demos/container.vue
@@ -10,8 +10,8 @@
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-export default Vue.extend({
+import { defineComponent } from 'vue';
+export default defineComponent({
   methods: {
     getContainer: () => document.getElementById('myPopup'),
   },
