03c17edfe6ab068a8ad6e96b28d219bca21001cb
pengYYYYY <pengyue970715@gmail.com>
2021-08-16 15:39:25 +0800

    fix: edit  Installation

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,15 @@
 import { App } from 'vue';
 
-import TransferDom from './utils/transfer-dom';
-import * as plugins from './plugins';
 import * as components from './components';
 
 function install(app: App, config?: Record<string, unknown>): void {
-  // 增加指令v-transfer-dom，用于元素指定挂载点的
-  app.directive('transfer-dom', TransferDom);
-
   Object.keys(components).forEach((key) => {
-    if (key.match(/plugin/)) {
-      return;
-    }
-    app.use(components[key], config);
-  });
-
-  Object.keys(plugins).forEach((key) => {
-    app.use(plugins[key]);
+    /plugin/i.test(key)
+      ? app.use(components[key])
+      : app.use(components[key], config);
   });
 }
 
-export * from './plugins';
 export * from './components';
 export default {
   install,
