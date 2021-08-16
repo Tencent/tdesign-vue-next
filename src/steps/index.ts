3fd506c8d444c53c5475bcf95f728ffae26e9ca6
pengYYYYY <pengyue970715@gmail.com>
2021-08-16 22:04:19 +0800

    feat: refactor step with compositionAPI

--- src/steps/index.ts
+++ src/steps/index.ts
@@ -1,6 +1,6 @@
 import _Steps from './steps.vue';
 import _StepItem from './step-item.vue';
-
+import mapProps from '../utils/map-props';
 import { withInstall, WithInstallType } from '../utils/withInstall';
 import { TdStepsProps, TdStepItemProps } from './type';
 
@@ -8,6 +8,8 @@ export * from './type';
 export type StepsProps = TdStepsProps;
 export type StepItemProps = TdStepItemProps;
 
-export const Steps: WithInstallType<typeof _Steps> = withInstall(_Steps);
+export const Steps: WithInstallType<typeof _Steps> = withInstall(mapProps([{
+  name: 'current', event: 'change', alias: ['modelValue'],
+}])(_Steps));
 export const StepItem: WithInstallType<typeof _StepItem> = withInstall(_StepItem);
 export default Steps;
