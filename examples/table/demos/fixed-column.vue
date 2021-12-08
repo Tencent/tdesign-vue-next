53d7d31e7f5ae2034cd6a47bda7d738061b3d927
pengYYY <pengyue970715@gmail.com>
2021-12-08 16:23:00 +0800

    docs: table demos update

--- examples/table/demos/fixed-column.vue
+++ examples/table/demos/fixed-column.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="tdesign-demo-block-column">
+  <div class="tdesign-demo-block-column" style="width: 500px">
     <div>
       <t-radio-group v-model="leftFixedColumn" variant="default-filled">
         <t-radio-button :value="1"> 左侧固定一列 </t-radio-button>
@@ -21,7 +21,7 @@
   </div>
 </template>
 <script>
-import { defineComponent, ref } from 'vue';
+import { defineComponent, ref, computed } from 'vue';
 
 const data = [];
 for (let i = 0; i < 5; i++) {
@@ -37,60 +37,62 @@ for (let i = 0; i < 5; i++) {
     description: '数据源',
   });
 }
-const columns = [
-  {
-    align: 'center',
-    width: 100,
-    colKey: 'index',
-    title: '序号',
-    fixed: 'left',
-  },
-  {
-    colKey: 'platform',
-    title: '平台',
-    width: 100,
-    fixed: 'left',
-  },
-  {
-    colKey: 'type',
-    title: '类型',
-    width: 150,
-  },
-  {
-    colKey: 'default',
-    title: '默认值',
-    width: 150,
-  },
-  {
-    colKey: 'detail.postion',
-    title: '详情信息',
-    width: 250,
-  },
-  {
-    colKey: 'description',
-    title: '说明',
-    width: 100,
-  },
-  {
-    colKey: 'needed',
-    title: '是否必传',
-    width: 150,
-    fixed: 'right',
-  },
-  {
-    colKey: 'operation',
-    title: '操作',
-    width: 100,
-    fixed: 'right',
-    cell: 'operation',
-  },
-];
 
 export default defineComponent({
   setup() {
-    const leftFixedColumn = ref(2);
-    const rightFixedColumn = ref(1);
+    const leftFixedColumn = ref(1);
+    const rightFixedColumn = ref(2);
 
+    const columns = computed(() => {
+      return [
+        {
+          align: 'center',
+          width: 100,
+          colKey: 'index',
+          title: '序号',
+          fixed: 'left',
+        },
+        {
+          colKey: 'platform',
+          title: '平台',
+          width: 100,
+          fixed: leftFixedColumn.value >= 2 ? 'left' : undefined,
+        },
+        {
+          colKey: 'type',
+          title: '类型',
+          width: 150,
+        },
+        {
+          colKey: 'default',
+          title: '默认值',
+          width: 150,
+        },
+        {
+          colKey: 'detail.postion',
+          title: '详情信息',
+          width: 250,
+        },
+        {
+          colKey: 'description',
+          title: '说明',
+          width: 100,
+        },
+        {
+          colKey: 'needed',
+          title: '是否必传',
+          width: 150,
+          fixed: rightFixedColumn.value >= 2 ? 'right' : undefined,
+        },
+        {
+          colKey: 'operation',
+          title: '操作',
+          width: 100,
+          fixed: 'right',
+          cell: 'operation',
+        },
+      ];
+    });
     return {
       leftFixedColumn,
       rightFixedColumn,
