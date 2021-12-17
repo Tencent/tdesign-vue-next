82d1b0bd757d55d6723ae04b8649610cfa60b66b
pengYYY <pengyue970715@gmail.com>
2021-12-18 00:12:20 +0800

    feat: upload xhr

--- src/upload/interface.ts
+++ src/upload/interface.ts
@@ -1,44 +1,21 @@
-import { UploadFile, ProgressContext, TdUploadProps, RequestMethodResponse } from './type';
+import { TdUploadProps } from './type';
+import {
+  XhrOptions,
+  InnerProgressContext,
+  HTMLInputEvent,
+  FlowRemoveContext,
+  UploadRemoveOptions,
+  SuccessContext,
+} from '../_common/js/upload/types';
 
 export type UploadProps = TdUploadProps;
 export * from './type';
 
-export interface InnerProgressContext {
-  event?: ProgressEvent;
-  file: UploadFile;
-  percent: number;
-  type?: ProgressContext['type'];
-}
-export interface SuccessContext {
-  event?: ProgressEvent;
-  file: UploadFile;
-  response: RequestMethodResponse['response'];
-}
-export interface HTMLInputEvent extends Event {
-  target: HTMLInputElement & EventTarget;
-}
-
-export interface XhrOptions {
-  action: string;
-  withCredentials: boolean;
-  headers: { [key: string]: string };
-  data: { [key: string]: any } | Function;
-  method: TdUploadProps['method'];
-  file: UploadFile;
-  name: string;
-  onError: ({ event, file, response }: { event?: ProgressEvent; file?: UploadFile; response?: any }) => void;
-  onSuccess: (context: SuccessContext) => void;
-  onProgress: (context: InnerProgressContext) => void;
-}
-
-export interface UploadRemoveOptions {
-  e: MouseEvent;
-  file?: UploadFile;
-  index: number;
-}
-
-export interface FlowRemoveContext {
-  e: MouseEvent;
-  index: number;
-  file: UploadFile;
-}
+export type {
+  XhrOptions,
+  HTMLInputEvent,
+  InnerProgressContext,
+  SuccessContext,
+  UploadRemoveOptions,
+  FlowRemoveContext,
+};
