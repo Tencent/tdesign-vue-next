b62ccfc3b8bb64c6afd612e5b993f2dd5da4664d
pengYYY <pengyue970715@gmail.com>
2021-12-22 16:16:02 +0800

    feat(table): demo update

--- src/tree-select/tree-select.tsx
+++ src/tree-select/tree-select.tsx
@@ -331,16 +331,23 @@ export default defineComponent({
       await this.value;
 
       if (tree && !this.multiple && this.value) {
-        const nodeValue = this.isObjectValue
-          ? (this.value as { label: string; value: string | number }).value
-          : this.value;
-        const node = (tree as any).getItem(nodeValue);
-        this.nodeInfo = { label: node.data[this.realLabel], value: node.data[this.realValue] };
+        const nodeValue = this.isObjectValue ? (this.value as NodeOptions).value : this.value;
+        // 数据源非空
+        if (!isEmpty(this.data)) {
+          const node = (tree as any).getItem(nodeValue);
+          this.nodeInfo = { label: node.data[this.realLabel], value: node.data[this.realValue] };
+        } else {
+          this.nodeInfo = { label: nodeValue, value: nodeValue };
+        }
       } else if (tree && this.multiple && isArray(this.value)) {
         this.nodeInfo = this.value.map((value) => {
-          const nodeValue = this.isObjectValue ? (value as { label: string; value: string | number }).value : value;
-          const node = (tree as any).getItem(nodeValue);
-          return { label: node.data[this.realLabel], value: node.data[this.realValue] };
+          const nodeValue = this.isObjectValue ? (value as NodeOptions).value : value;
+          // 数据源非空
+          if (!isEmpty(this.data)) {
+            const node = (tree as any).getItem(nodeValue);
+            return { label: node.data[this.realLabel], value: node.data[this.realValue] };
+          }
+          return { label: nodeValue, value: nodeValue };
         });
       } else {
         this.nodeInfo = null;
