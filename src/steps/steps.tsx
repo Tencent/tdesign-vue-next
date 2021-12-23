c95ba6d6f7ece0798abc207318f1e13d279f3002
pengYYY <pengyue970715@gmail.com>
2021-12-23 14:20:13 +0800

    refactor: bem steps and checkbox checkAll bug

--- src/steps/steps.tsx
+++ src/steps/steps.tsx
@@ -1,18 +1,18 @@
-import { defineComponent } from 'vue';
+import { defineComponent, VNode } from 'vue';
 import { prefix } from '../config';
 import props from './props';
 import TStepItem from './step-item';
 import { ClassName } from '../common';
-import { TdStepsProps } from './type';
-import { emitEvent } from '../utils/event';
-import { renderTNodeJSX } from '../utils/render-tnode';
-
 import mixins from '../utils/mixins';
+import getConfigReceiverMixins, { StepsConfig } from '../config-provider/config-receiver';
+import { TdStepsProps, TdStepItemProps } from './type';
+import { emitEvent } from '../utils/event';
 
 const name = `${prefix}-steps`;
 
 export default defineComponent({
   name: 'TSteps',
+  ...mixins(getConfigReceiverMixins<StepsConfig>('datePicker')),
   components: {
     TStepItem,
   },
@@ -33,25 +33,82 @@ export default defineComponent({
       if (this.direction) {
         console.warn('TDesign Steps Warn: `direction` is going to be deprecated. please use `layout` instead. ');
       }
-      const layout = this.layout || this.direction;
+      const layout = this.layout || this.direction || 'horizontal';
       return [
         name,
         `${name}--${layout}`,
-        `${name}--${this.theme}-anchor`,
+        `${name}--${this.handleTheme()}-anchor`,
         {
           [`${name}--${this.sequence}`]: layout === 'vertical',
         },
       ];
     },
   },
+  watch: {
+    options: {
+      immediate: true,
+      handler() {
+        if (!this.options) return;
+        this.options.forEach((item, index) => {
+          if (item.value !== undefined) {
+            this.indexMap[item.value] = index;
+          }
+        });
+      },
+    },
+  },
   methods: {
+    getOptions() {
+      let options: Array<TdStepItemProps>;
+      if (this.options && this.options.length) {
+        options = this.options;
+      } else {
+        const nodes = this.$slots.default && this.$slots.default(null);
+        options = this.getOptionListBySlots(nodes);
+      }
+      return options;
+    },
+    getOptionListBySlots(nodes: VNode[]) {
+      const arr: Array<TdStepItemProps> = [];
+      nodes?.forEach((node) => {
+        const option = node?.props;
+        option && arr.push(option as TdStepItemProps);
+      });
+      return arr;
+    },
+    handleTheme() {
+      let { theme } = this;
+      const options = this.getOptions();
+      options.forEach((item) => {
+        if (item?.icon !== undefined) {
+          // icon > theme
+          theme = 'default';
+        }
+      });
+      return theme;
+    },
+    handleStatus(item: TdStepItemProps, index: number) {
+      if (item.status && item.status !== 'default') return item.status;
+      if (this.current === 'FINISH') return 'finish';
+      // value 不存在时，使用 index 进行区分每一个步骤
+      if (item.value === undefined && index < this.current) return 'finish';
+      // value 存在，找匹配位置
+      if (item.value !== undefined) {
+        const matchIndex = this.indexMap[this.current];
+        if (matchIndex === undefined) {
+          console.warn('TDesign Steps Warn: The current `value` is not exist.');
+          return 'default';
+        }
+        if (index < matchIndex) return 'finish';
+      }
+      const key = item.value === undefined ? index : item.value;
+      if (key === this.current) return 'process';
+      return 'default';
+    },
     addItem(item: InstanceType<typeof TStepItem>) {
       const index = this.stepChildren.length;
       // eslint-disable-next-line
       item.index = index;
-      if (item.value !== undefined) {
-        this.indexMap[item.value] = index;
-      }
       this.stepChildren.push(item);
     },
     removeItem(item: InstanceType<typeof TStepItem>) {
@@ -62,14 +119,16 @@ export default defineComponent({
     },
   },
   render() {
-    let content;
-    if (this.options && this.options.length) {
-      content = this.options.map((item, index) => (
-        <t-step-item {...item} current={this.current} key={item.value || index}></t-step-item>
-      ));
-    } else {
-      content = renderTNodeJSX(this, 'default');
-    }
+    const options = this.getOptions();
+    const content = options.map((item, index) => (
+      <t-step-item
+        {...{
+          ...item,
+          status: this.handleStatus(item, index),
+        }}
+        key={item.value || index}
+      ></t-step-item>
+    ));
     return <div class={this.baseClass}>{content}</div>;
   },
 });
