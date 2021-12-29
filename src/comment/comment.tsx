7567708cb1d790d96c8387eabe3eb2ed43c9bd7b
loryliang <loryliang@tencent.com>
2021-12-29 23:57:30 +0800

    refactor(src/comment/): for better readability, rewrite comment.tsx by compositionApi

--- src/comment/comment.tsx
+++ src/comment/comment.tsx
@@ -1,81 +1,73 @@
-import { defineComponent, Slot, VNode } from 'vue';
+import { defineComponent } from 'vue';
 import { prefix } from '../config';
 import props from './props';
-import { renderTNodeJSX } from '../utils/render-tnode';
 
 const preName = `${prefix}-comment`;
 
 export default defineComponent({
   name: 'TComment',
   props,
-  methods: {
-    renderReply() {
-      const reply = renderTNodeJSX(this, 'reply');
-      return reply ? <div class={`${preName}__reply`}>{reply}</div> : null;
-    },
-    renderActions() {
-      const actions = renderTNodeJSX(this, 'actions');
-      return actions && actions.length ? (
-        <ul class={`${preName}__actions`}>
-          {actions.map((action: Slot, index: number) => (
-            <li key={`action-${index}`}>{action}</li>
-          ))}
-        </ul>
-      ) : null;
-    },
+  slots: ['avatar', 'reply', 'author', 'datetime', 'content', 'quote', 'actions'],
+  setup(props, { slots }) {
+    // slot precedence
+    const avatar = slots.avatar?.() ?? props.avatar;
+    const reply = slots.reply?.() ?? props.reply;
+    const author = slots.author?.() ?? props.author;
+    const datetime = slots.author?.() ?? props.datetime;
+    const quote = slots.quote?.() ?? props.quote;
+    const actions = slots.actions?.() ?? props.actions;
+    const content = slots.content?.() ?? props.content;
 
-    renderQuote() {
-      const quote = renderTNodeJSX(this, 'quote');
-      return quote ? <div class={`${preName}__quote`}>{quote}</div> : null;
-    },
+    return () => {
+      const showAuthorDatetime = author || datetime;
 
-    renderAuthorDatetime() {
-      const author = renderTNodeJSX(this, 'author');
-      const datetime = renderTNodeJSX(this, 'datetime');
+      const renderActions = () => {
+        if (!actions || !actions.length) return null;
 
-      return (
-        (author || datetime) && (
-          <div class={`${preName}__author`}>
-            {author && <span class={`${preName}__name`}>{author}</span>}
-            {datetime && <span class={`${preName}__time`}>{datetime}</span>}
-          </div>
-        )
-      );
-    },
+        return (
+          <ul class={`${preName}__actions`}>
+            {(Array.isArray(actions) ? actions : [actions]).map((action, index: number) => (
+              <li key={`action-${index}`}>{action}</li>
+            ))}
+          </ul>
+        );
+      };
 
-    renderContent() {
-      return (
-        <div class={`${preName}__content`}>
-          {this.renderAuthorDatetime()}
-          <div class={`${preName}__detail`}>{renderTNodeJSX(this, 'content')}</div>
-          {this.renderQuote()}
-          {this.renderActions()}
+      const renderAuthorDatetime = showAuthorDatetime && (
+        <div class={`${preName}__author`}>
+          {author && <span class={`${preName}__name`}>{author}</span>}
+          {datetime && <span class={`${preName}__time`}>{datetime}</span>}
         </div>
       );
-    },
 
-    renderAvatar() {
-      return this.avatar ? (
+      const renderQuote = quote ? <div class={`${preName}__quote`}>{quote}</div> : null;
+
+      const renderAvatar = avatar ? (
         <div class={`${preName}__avatar`}>
-          {typeof this.avatar === 'string' ? (
-            <img src={this.avatar} alt="" class={`${preName}__avatar-image`} />
-          ) : (
-            renderTNodeJSX(this, 'avatar')
-          )}
+          {typeof avatar === 'string' ? <img src={avatar} alt="" class={`${preName}__avatar-image`} /> : avatar}
         </div>
       ) : null;
-    },
-  },
 
-  render(): VNode {
-    return (
-      <div class={preName}>
-        <div class={`${preName}__inner`}>
-          {this.renderAvatar()}
-          {this.renderContent()}
+      const renderReply = reply ? <div class={`${preName}__reply`}>{reply}</div> : null;
+
+      const renderContent = (
+        <div class={`${preName}__content`}>
+          {renderAuthorDatetime}
+          <div class={`${preName}__detail`}>{content}</div>
+          {renderQuote}
+          {renderActions()}
         </div>
-        {this.renderReply()}
-      </div>
-    );
+      );
+
+      return (
+        <div class={preName}>
+          <div class={`${preName}__inner`}>
+            {renderAvatar}
+            {renderContent}
+          </div>
+          {renderReply}
+        </div>
+      );
+    };
   },
 });
