fc7a9db080d02a2220e0d9871d433d5913d749b3
pengYYY <pengyue970715@gmail.com>
2022-01-17 15:45:19 +0800

    docs(function doc): update function doc

--- src/hooks/tnode.ts
+++ src/hooks/tnode.ts
@@ -1,4 +1,4 @@
-import { h, getCurrentInstance, VNode, Slots, isVNode } from 'vue';
+import { h, getCurrentInstance, VNode, isVNode } from 'vue';
 import isEmpty from 'lodash/isEmpty';
 import isFunction from 'lodash/isFunction';
 import isObject from 'lodash/isObject';
@@ -31,7 +31,7 @@ export function getParams(options?: OptionsType) {
 }
 
 /**
- * 用于组合式的TNodeJSX，在组合时逻辑时渲染 TNode，props 和 插槽同时处理，也能处理默认值为 true 则渲染默认节点的情况，与render-tnode的大概处理逻辑相同。
+ * 用于setup的TNodeJSX，在组合时逻辑时渲染 TNode，props 和 插槽同时处理，也能处理默认值为 true 则渲染默认节点的情况，与render-tnode的大概处理逻辑相同。
  * @param name 插槽和属性名称
  * @param options 值可能为默认渲染节点，也可能是默认渲染节点和参数的集合
  * @example useTNodeJSX('closeBtn')  优先级 props function 大于 插槽
@@ -70,7 +70,7 @@ export const useTNodeJSX = (name: string, options?: OptionsType) => {
 };
 
 /**
- * 在组合逻辑时，通过JSX的方式 TNode，props 和 插槽同时处理。与 renderTNodeJSX 区别在于属性值为 undefined 时会渲染默认节点
+ * 在setup中，通过JSX的方式 TNode，props 和 插槽同时处理。与 renderTNodeJSX 区别在于属性值为 undefined 时会渲染默认节点
  * @param name 插槽和属性名称
  * @example useTNodeDefault('closeBtn')
  * @example useTNodeDefault('closeBtn', <close-icon />) closeBtn 为空时，则兜底渲染 <close-icon />
@@ -82,8 +82,7 @@ export const useTNodeDefault = (name: string, options?: VNode | JSXRenderContext
 };
 
 /**
- * 用于处理相同名称的 TNode 渲染
- * @param vm 组件实例
+ * 在setup中，用于处理相同名称的 TNode 渲染
  * @param name1 第一个名称，优先级高于 name2
  * @param name2 第二个名称
  * @param defaultNode 默认渲染内容：当 name1 和 name2 都为空时会启动默认内容渲染
