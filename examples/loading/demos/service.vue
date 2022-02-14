2f2dd1254db21cf928df09bc7bc4995d0535b7d4
pengYYY <pengyue970715@gmail.com>
2022-02-14 17:38:12 +0800

    fix(loading): fix loading attach

--- examples/loading/demos/service.vue
+++ examples/loading/demos/service.vue
@@ -1,23 +1,27 @@
 <template>
   <div class="tdesign-demo-block-column">
-    <div id="loading-service-demo" ref="content" class="loading-service-demo">我是service的容器</div>
+    <div id="loading-service-demo" ref="content" class="loading-service-demo">Loading 挂载容器</div>
 
     <p>this.$loading(true)</p>
     <p>this.$loading({ attach: '#loading-service-demo', showOverlay: true })</p>
 
     <div class="tdesign-demo-block-row">
-      <t-button size="small" @click="showFullScreen1"> 服务方式加载（全屏） </t-button>
-      <t-button size="small" :disabled="attachLoading" @click="showAttach1"> 服务方式加载（局部） </t-button>
+      <t-button class="t-loading__btn" size="small" :disabled="attachLoading" @click="showAttach1">
+        插件方式加载（局部）
+      </t-button>
+      <t-button size="small" @click="showFullScreen1">插件方式加载（全屏）</t-button>
+      <t-button size="small" @click="showFullScrollScreen1">插件方式加载（全屏-滚动穿透）</t-button>
     </div>
 
     <p>LoadingPlugin(true)</p>
     <p>LoadingPlugin({ attach: '#loading-service-demo', showOverlay: true })</p>
 
     <div class="tdesign-demo-block-row">
-      <t-button size="small" @click="showFullScreen2"> 函数方式加载（全屏） </t-button>
-      <t-button class="t-loading__btn" size="small" :disabled="attachLoading" @click="showAttach2">
-        函数方式加载（局部）
-      </t-button>
+      <t-button class="t-loading__btn" size="small" :disabled="attachLoading" @click="showAttach2"
+        >函数方式加载（局部）</t-button
+      >
+      <t-button size="small" @click="showFullScreen2">函数方式加载（全屏）</t-button>
+      <t-button size="small" @click="showFullScrollScreen2">函数方式加载（全屏-滚动穿透）</t-button>
     </div>
   </div>
 </template>
@@ -32,17 +36,12 @@ export default {
     };
   },
   methods: {
-    showFullScreen1() {
-      this.$loading(true);
-      const timer = setTimeout(() => {
-        this.$loading(false);
-        clearTimeout(timer);
-      }, 1000);
-    },
+    // 插件式：局部加载，局部加载模式添加 attach="body" 无效
     showAttach1() {
       const loadingAttachInstance = this.$loading({
         attach: '#loading-service-demo',
         showOverlay: true,
+        size: '20px',
       });
       this.attachLoading = true;
       const timer = setTimeout(() => {
@@ -51,6 +50,42 @@ export default {
         clearTimeout(timer);
       }, 1000);
     },
+    // 插件式：全屏加载，默认防止滚动穿透
+    showFullScreen1() {
+      this.$loading(true);
+      const timer = setTimeout(() => {
+        this.$loading(false);
+        clearTimeout(timer);
+      }, 10000);
+    },
+    // 插件式：全屏加载，允许滚动穿透
+    showFullScrollScreen1() {
+      const instance = this.$loading({
+        fullscreen: true,
+        attach: 'body',
+        preventScrollThrough: false,
+      });
+      const timer = setTimeout(() => {
+        instance.hide();
+        clearTimeout(timer);
+      }, 1000);
+    },
+
+    // 函数式：局部加载
+    showAttach2() {
+      const loadingAttachInstance = LoadingPlugin({
+        attach: () => this.$refs.content,
+        showOverlay: true,
+        size: '20px',
+      });
+      this.attachLoading = true;
+      const timer = setTimeout(() => {
+        loadingAttachInstance.hide();
+        this.attachLoading = false;
+        clearTimeout(timer);
+      }, 1000);
+    },
+    // 函数式：全屏加载，防止滚动穿透
     showFullScreen2() {
       LoadingPlugin(true);
       const timer = setTimeout(() => {
@@ -58,15 +93,15 @@ export default {
         clearTimeout(timer);
       }, 1000);
     },
-    showAttach2() {
-      const loadingAttachInstance = LoadingPlugin({
-        attach: () => this.$refs.content,
-        showOverlay: true,
+    // 函数式：全屏加载，允许滚动穿透
+    showFullScrollScreen2() {
+      const instance = LoadingPlugin({
+        fullscreen: true,
+        attach: 'body',
+        preventScrollThrough: false,
       });
-      this.attachLoading = true;
       const timer = setTimeout(() => {
-        loadingAttachInstance.hide();
-        this.attachLoading = false;
+        instance.hide();
         clearTimeout(timer);
       }, 1000);
     },
--- examples/loading/demos/text.vue
+++ examples/loading/demos/text.vue
@@ -1,29 +1,20 @@
 <template>
   <div class="t-loading-demo">
     <!-- 使用 props 实现自定义 -->
-    <t-loading text="静态文字加载中..." :indicator="false" />
+    <t-loading text="静态文字加载中..." :indicator="false"></t-loading>
     <!-- 使用 props 实现自定义 -->
-    <t-loading :text="renderText" :indicator="false" />
+    <t-loading :text="renderText" :indicator="false"></t-loading>
     <t-loading :indicator="false">
       <!-- 使用插槽实现自定义 -->
-      <template #text> 插槽文字加载中... </template>
+      <template #text>插槽文字加载中...</template>
     </t-loading>
   </div>
 </template>
-<script lang="jsx">
-import { defineComponent } from 'vue';
-
-export default defineComponent({
-  setup() {
-    return {
-      renderText() {
-        return <span>变量文字加载中...</span>;
-      },
-    };
-  },
-});
+<script lang="jsx" setup>
+const renderText = () => {
+  return <span>变量文字加载中...</span>;
+};
 </script>
-
 <style>
 .t-loading-demo .t-loading + .t-loading {
   margin-left: 100px;
