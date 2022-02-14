f5ddf62c342ed9c1710e55cdcfdced568a9d98ac
vnues <1518190293@qq.com>
2022-02-14 15:49:30 +0800

    refactor(hooks): fix useIcon hook

--- src/hooks/icon.tsx
+++ src/hooks/icon.tsx
@@ -1,30 +1,26 @@
-import { h, Slots } from 'vue';
+import { getCurrentInstance, h } from 'vue';
 
 /**
  * 渲染icon，用于icon、close等渲染图标的场景
- * 使用场景：useIcon(props, slots, 'icon', Component);  useIcon(props, slots, 'close');
- * @param props
- * @param slots
+ * @example const renderIconTNode = useIcon();
+ * @returns renderIconTNode
  * @param iconType 要渲染的icon元素
  * @param defaultIcons 默认icon集合
- * @returns
  */
-export function useIcon<P extends Record<string, any>>(
-  props: P,
-  slots: Slots,
-  iconType: string,
-  defaultIcons?: Record<string, any>,
-) {
-  let iconContent;
-  // 传入的是渲染函数
-  if (typeof props[iconType] === 'function') {
-    iconContent = props[iconType](h);
-  } else if (slots[iconType]) {
-    // 插槽slot
-    iconContent = slots[iconType] && slots[iconType](null)[0];
-  } else if (defaultIcons) {
-    const Component = defaultIcons[props.theme as string];
-    iconContent = <Component></Component>;
-  }
-  return iconContent;
+export function useIcon() {
+  const instance = getCurrentInstance();
+  return function renderIconTNode(iconType: string, defaultIcons?: Record<string, any>) {
+    let iconContent;
+    // 传入的是渲染函数
+    if (typeof instance.props[iconType] === 'function') {
+      iconContent = (instance.props as Object)[iconType](h);
+    } else if (instance.slots[iconType]) {
+      // 插槽slot
+      iconContent = instance.slots[iconType] && instance.slots[iconType](null)[0];
+    } else if (defaultIcons) {
+      const Component = defaultIcons[instance.props.theme as string];
+      iconContent = <Component></Component>;
+    }
+    return iconContent;
+  };
 }
