881cc1038e5a679828322acee7cb6ad7335ae4a6
pengYYY <pengyue970715@gmail.com>
2022-02-16 11:57:09 +0800

    fix: issue 213

--- site/src/components/demo-page.vue
+++ site/src/components/demo-page.vue
@@ -1,38 +1,67 @@
 <template>
   <component :is="demo" v-if="demo"></component>
-  <h1 v-else class="empty-demo">请输入正确的 demo 路径，例如：/vue/demos/:componentName/:demoName</h1>
+  <ul v-else class="empty-demo">
+    <li v-for="demoName in demoList[componentName]" :key="demoName">
+      <router-link :to="{ path: `/vue/demos/${componentName}/${demoName}` }">
+        <t-button theme="default" variant="text">{{ demoName }}</t-button>
+      </router-link>
+    </li>
+  </ul>
 </template>
 
 <script>
 const demoReq = import.meta.globEager('../../../examples/**/demos/*.vue');
-
 const demoObject = {};
+const demoList = {};
 Object.keys(demoReq).forEach((key) => {
   const match = key.match(/([\w-]+).demos.([\w-]+).vue/);
   const [, componentName, demoName] = match;
-
   demoObject[`${componentName}-${demoName}`] = demoReq[key].default;
+  demoList[componentName] = [demoName].concat(demoList[componentName]);
 });
-
 export default {
+  name: 'Demos',
   components: {
     ...demoObject,
   },
-
   data() {
     return {
       demo: null,
+      demoList,
     };
   },
+  computed: {
+    componentName() {
+      return this.$route.params.componentName;
+    },
+    currentDemos() {
+      return this.demoList[this.componentName].join('<br />');
+    },
+  },
+  watch: {
+    $route(v) {
+      if (v.name !== 'demos') return;
+      this.renderDemo();
+    },
+  },
   mounted() {
-    const { componentName, demoName } = this.$route.params;
-    this.demo = `${componentName}-${demoName}`;
+    this.renderDemo();
+  },
+  methods: {
+    renderDemo() {
+      const { componentName, demoName } = this.$route.params;
+      if (componentName && demoName) {
+        this.demo = `${componentName}-${demoName}`;
+      } else {
+        this.demo = '';
+      }
+    },
   },
 };
 </script>
 
-<style>
+<style scoped>
 .empty-demo {
-  margin: 100px 48px;
+  margin: 48px 240px;
 }
 </style>
