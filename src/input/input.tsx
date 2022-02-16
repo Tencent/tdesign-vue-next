b62578f80a51ae6b6385265ac7c2a932bcc55dd3
pengYYY <pengyue970715@gmail.com>
2022-02-16 12:51:34 +0800

    fix(input): fix input align and size

--- src/input/input.tsx
+++ src/input/input.tsx
@@ -235,9 +235,11 @@ export default defineComponent({
     const classes = [
       name,
       {
+        [CLASSNAMES.SIZE[this.size]]: this.size !== 'medium',
         [CLASSNAMES.STATUS.disabled]: this.disabled,
         [CLASSNAMES.STATUS.focused]: this.focused,
         [`${prefix}-is-${this.status}`]: this.status,
+        [`${prefix}-align-${this.align}`]: this.align !== 'left',
         [`${prefix}-is-disabled`]: this.disabled,
         [`${prefix}-is-readonly`]: this.readonly,
         [`${name}--prefix`]: prefixIcon || labelContent,
