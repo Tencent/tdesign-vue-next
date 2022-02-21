c0dd40284115dcb2b38d6dcfca73cb9ba4377ea8
HQ-Lin <a154571896@gmail.com>
2022-02-21 15:44:10 +0800

    feat: 新增 stackblitz 支持

--- site/src/main.js
+++ site/src/main.js
@@ -4,6 +4,7 @@ import App from './app.vue';
 import router from './routes';
 
 import CodeSandbox from './components/code-sandbox/index.vue';
+import Stackblitz from './components/stackblitz/index.vue';
 
 // import tdesign style
 import 'tdesign-vue-next/style/index.js';
@@ -21,5 +22,6 @@ import 'tdesign-icons-view';
 const app = createApp(App);
 
 app.component('CodeSandbox', CodeSandbox);
+app.component('Stackblitz', Stackblitz);
 
 app.use(TDesign).use(router).mount('#app');
