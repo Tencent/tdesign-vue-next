c78d4bf1faac7e29d854ca7eb2bbe99fc8d0b809
HQ-Lin <a154571896@gmail.com>
2022-02-21 15:44:10 +0800

    feat: 新增 stackblitz 支持

--- site/src/components/code-sandbox/index.vue
+++ site/src/components/code-sandbox/index.vue
@@ -91,24 +91,3 @@ export default defineComponent({
   },
 });
 </script>
-
-<style lang="less">
-.action-online {
-  width: 32px;
-  height: 32px;
-  box-sizing: border-box;
-  display: inline-flex;
-  align-items: center;
-  justify-content: center;
-  padding: 8px;
-  transition: all 0.2s linear;
-  cursor: pointer;
-  border-radius: 3px;
-  color: var(--text-secondary);
-
-  &:hover {
-    color: var(--text-primary);
-    background-color: var(--bg-color-demo-hover, rgb(243, 243, 243));
-  }
-}
-</style>
