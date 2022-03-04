7f31b2238d43d52c09ae847c573cba5aec6524b1
pengYYY <pengyue970715@gmail.com>
2022-03-04 10:39:19 +0800

    fix(form): fix issue 382

--- examples/drawer/demos/base.vue
+++ examples/drawer/demos/base.vue
@@ -1,33 +1,50 @@
 <template>
-  <div>
-    <t-drawer v-model:visible="visible" header="header" :on-confirm="onClickConfirm" :close-btn="true">
-      <p>This is a controlled drawer</p>
-    </t-drawer>
-    <t-button variant="outline" @click="handleClick"> 打开抽屉 </t-button>
+  <div class="list-common-table" style="background-color: grey; overflow: hidden">
+    <t-form ref="form" :data="formData" :label-width="80" colon>
+      <t-row>
+        <t-col :span="10">
+          <t-row :gutter="[16, 16]">
+            <t-col :flex="1">
+              <t-form-item label="合同编号" name="contractNo">
+                <t-input
+                  class="form-item-content"
+                  type="search"
+                  placeholder="合同编号"
+                  :style="{ minWidth: '134px' }"
+                />
+              </t-form-item>
+            </t-col>
+            <t-col :flex="1">
+              <t-form-item label="合同名称" name="contractName">
+                <t-input
+                  class="form-item-content"
+                  type="search"
+                  placeholder="合同名称"
+                  :style="{ minWidth: '134px' }"
+                />
+              </t-form-item>
+            </t-col>
+          </t-row>
+        </t-col>
+
+        <t-col :span="2" class="operation-container">
+          <t-button theme="primary" type="submit" :style="{ marginLeft: '8px' }"> 查询</t-button>
+          <t-button type="reset" variant="base" theme="default"> 重置</t-button>
+        </t-col>
+      </t-row>
+      <t-button variant="outline" @click="handleClick"> 打开抽屉 </t-button>
+    </t-form>
+    <detail-drawer v-model:visible="visible" />
   </div>
 </template>
 
 <script setup>
 import { ref } from 'vue';
-import { MessagePlugin } from 'tdesign-vue-next';
+import DetailDrawer from './com.vue';
 
 const visible = ref(false);
 
 const handleClick = () => {
   visible.value = true;
 };
-
-const onClickConfirm = () => {
-  MessagePlugin.info('数据保存中...', 1000);
-  const timer = setTimeout(() => {
-    clearTimeout(timer);
-    visible.value = false;
-    MessagePlugin.info('数据保存成功!');
-  }, 1000);
-};
 </script>
-<style scoped>
-.t-button {
-  margin-right: 20px;
-}
-</style>
---
+++ examples/drawer/demos/com.vue
@@ -0,0 +1,41 @@
+<template>
+  <t-drawer
+    v-model:visible="formVisible"
+    placement="right"
+    mode="overlay"
+    size="60%"
+    header=""
+    :close-btn="true"
+    show-in-attached-element
+  >
+  </t-drawer>
+</template>
+
+<script setup>
+import { ref, watch } from 'vue';
+
+// eslint-disable-next-line no-undef
+const props = defineProps({
+  visible: {
+    type: Boolean,
+    default: false,
+  },
+});
+const formVisible = ref(false);
+
+// eslint-disable-next-line no-undef
+const emit = defineEmits(['update:visible']);
+watch(
+  () => formVisible.value,
+  (val) => {
+    emit('update:visible', val);
+  },
+);
+
+watch(
+  () => props.visible,
+  (val) => {
+    formVisible.value = val;
+  },
+);
+</script>
