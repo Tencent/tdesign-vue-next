5d433ff66c85609e59e23a8c4c01a3e9846b323e
HQ-Lin <a154571896@gmail.com>
2022-03-07 18:29:43 +0800

    feat: 调整站点设置

--- site/vite.config.js
+++ site/vite.config.js
@@ -7,39 +7,47 @@ import { VitePWA } from 'vite-plugin-pwa';
 import tDocPlugin from './plugin-doc';
 import pwaConfig from './pwaConfig';
 
-export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/vue-next/' : '/',
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, '../'),
-      '@common': path.resolve(__dirname, '../src/_common'),
-      'tdesign-vue-next': path.resolve(__dirname, '../src'),
-    },
-  },
-  server: {
-    host: '127.0.0.1',
-    port: 17000,
-    open: '/',
-    https: false,
-    fs: {
-      allow: ['..'],
-    },
-  },
-  build: {
-    outDir: '../_site',
-  },
-  plugins: [
-    vue({
-      template: {
-        compilerOptions: {
-          isCustomElement: (tag) => tag.startsWith('td-'),
-        },
+const publicPathMap = {
+  preview: '/',
+  intranet: '/vue-next/',
+  production: 'https://static.tdesign.tencent.com/vue-next/',
+};
+
+export default ({ mode }) => {
+  return defineConfig({
+    base: publicPathMap[mode],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '../'),
+        '@common': path.resolve(__dirname, '../src/_common'),
+        'tdesign-vue-next': path.resolve(__dirname, '../src'),
       },
-    }),
-    vueJsx({
-      isCustomElement: (tag) => tag.startsWith('td-'),
-    }),
-    tDocPlugin(),
-    VitePWA(pwaConfig),
-  ],
-});
+    },
+    server: {
+      host: '127.0.0.1',
+      port: 17000,
+      open: '/',
+      https: false,
+      fs: {
+        allow: ['..'],
+      },
+    },
+    build: {
+      outDir: '../_site',
+    },
+    plugins: [
+      vue({
+        template: {
+          compilerOptions: {
+            isCustomElement: (tag) => tag.startsWith('td-'),
+          },
+        },
+      }),
+      vueJsx({
+        isCustomElement: (tag) => tag.startsWith('td-'),
+      }),
+      tDocPlugin(),
+      VitePWA(pwaConfig),
+    ],
+  });
+};
