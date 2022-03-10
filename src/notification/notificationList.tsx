e1423f439c9179de1190aedce34b53ec40967d74
qunbotop <yinqunbo@126.com>
2022-03-10 18:50:10 +0800

    refactor: refactor notification component used by composition , and add notification recycle effect

--- src/notification/notificationList.tsx
+++ src/notification/notificationList.tsx
@@ -1,4 +1,4 @@
-import { defineComponent } from 'vue';
+import { defineComponent, ref, computed, TransitionGroup } from 'vue';
 import Notification from './notification';
 import { prefix } from '../config';
 import { TdNotificationProps, NotificationOptions } from './type';
@@ -15,66 +15,80 @@ export default defineComponent({
       },
     },
   },
-  data() {
-    return {
-      list: [],
+  setup(props) {
+    const { placement } = props as NotificationOptions;
+
+    const list = ref([]);
+
+    const styles = computed<Styles>(() => ({
+      zIndex: DEFAULT_Z_INDEX,
+      ...PLACEMENT_OFFSET[placement],
+    }));
+
+    const add = (options: TdNotificationProps): number => {
+      list.value.push(options);
+      return list.value.length - 1;
     };
-  },
-  computed: {
-    styles(): Styles {
-      return {
-        zIndex: DEFAULT_Z_INDEX,
-        ...PLACEMENT_OFFSET[this.placement],
-      };
-    },
-  },
-  methods: {
-    add(options: TdNotificationProps): number {
-      this.list.push(options);
-      return this.list.length - 1;
-    },
-    remove(index: number) {
-      this.list.splice(index, 1);
-    },
-    removeAll() {
-      this.list = [];
-    },
-    getOffset(val: string | number) {
+
+    const remove = (index: number) => {
+      list.value.splice(index, 1);
+    };
+
+    const removeAll = () => {
+      list.value = [];
+    };
+
+    const getOffset = (val: string | number) => {
       if (!val) return;
       return isNaN(Number(val)) ? val : `${val}px`;
-    },
-    notificationStyles(item: { offset: NotificationOptions['offset']; zIndex: number }) {
+    };
+
+    const notificationStyles = (item: { offset: NotificationOptions['offset']; zIndex: number }) => {
       const styles: Styles = {
         marginBottom: DISTANCE,
       };
       if (item.offset) {
         styles.position = 'relative';
-        styles.left = this.getOffset(item.offset[0]);
-        styles.top = this.getOffset(item.offset[1]);
+        styles.left = getOffset(item.offset[0]);
+        styles.top = getOffset(item.offset[1]);
       }
       if (item.zIndex) styles['z-index'] = item.zIndex;
       return styles;
-    },
-    getListeners(index: number) {
+    };
+
+    const getListeners = (options: NotificationOptions, index: number) => {
       return {
-        onClickCloseBtn: () => this.remove(index),
-        onDurationEnd: () => this.remove(index),
+        onClickCloseBtn: () => remove(index),
+        onDurationEnd: () => remove(index),
       };
-    },
+    };
+
+    return {
+      list,
+      styles,
+      add,
+      remove,
+      removeAll,
+      getOffset,
+      notificationStyles,
+      getListeners,
+    };
   },
   render() {
-    if (!this.list.length) return;
+    const { placement, styles, list } = this;
     return (
-      <div class={`${prefix}-notification__show--${this.placement}`} style={this.styles}>
-        {this.list.map((item, index) => (
-          <Notification
-            ref={`notification${index}`}
-            key={item.id}
-            style={this.notificationStyles(item)}
-            {...item}
-            {...this.getListeners(index)}
-          />
-        ))}
+      <div class={`${prefix}-notification__show--${placement}`} style={styles}>
+        <TransitionGroup name="notification-slide-fade">
+          {list.map((item, index) => (
+            <Notification
+              ref={`notification${index}`}
+              key={item.id}
+              style={this.notificationStyles(item)}
+              {...item}
+              {...this.getListeners(item, index)}
+            />
+          ))}
+        </TransitionGroup>
       </div>
     );
   },
