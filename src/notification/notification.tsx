08c0a944aef8c77e44b779a78ed2a3978a68e9a6
qunbotop <yinqunbo@126.com>
2022-03-11 09:47:06 +0800

    refactor: refactor notification component used by composition
    BREAKING CHANGE: refactor notification component used by composition , and add notification recycle
    effect

--- src/notification/notification.tsx
+++ src/notification/notification.tsx
@@ -1,63 +1,76 @@
-import { defineComponent, h, VNodeChild } from 'vue';
+import { ComponentPublicInstance, defineComponent, h, onMounted } from 'vue';
 import { InfoCircleFilledIcon, CheckCircleFilledIcon, CloseIcon } from 'tdesign-icons-vue-next';
 import isFunction from 'lodash/isFunction';
 import { prefix } from '../config';
 import { renderTNodeJSX, renderContent } from '../utils/render-tnode';
 import props from './props';
-import { emitEvent } from '../utils/event';
+import { useEmitEvent } from '../hooks/event';
 
 const name = `${prefix}-notification`;
 
 export default defineComponent({
   name: 'TNotification',
-  props: { ...props },
-  emits: ['duration-end', 'click-close-btn'],
-  mounted() {
-    if (this.duration > 0) {
-      const timer = setTimeout(() => {
-        clearTimeout(timer);
-        emitEvent(this, 'duration-end', this);
-      }, this.duration);
-    }
+  props: {
+    ...props,
   },
-  methods: {
-    close(e?: MouseEvent) {
-      emitEvent(this, 'click-close-btn', e, this);
-    },
-    renderIcon() {
-      let icon;
-      if (this.icon === false) return null;
-      if (isFunction(this.icon)) {
-        icon = this.icon(h);
-      } else if (this.$slots.icon) {
-        icon = this.$slots.icon(null);
-      } else if (this.theme) {
+  emits: ['duration-end', 'click-close-btn'],
+  setup(props, { slots }) {
+    const emitEvent = useEmitEvent();
+    const close = (e?: MouseEvent) => {
+      emitEvent('click-close-btn', e);
+    };
+    const renderIcon = () => {
+      let iconContent;
+      if (props.icon === false) return null;
+      if (isFunction(props.icon)) {
+        iconContent = props.icon(h);
+      } else if (slots.icon) {
+        iconContent = slots.icon(null);
+      } else if (props.theme) {
         const iconType =
-          this.theme === 'success' ? (
-            <CheckCircleFilledIcon class={`t-is-${this.theme}`} />
+          props.theme === 'success' ? (
+            <CheckCircleFilledIcon class={`t-is-${props.theme}`} />
           ) : (
-            <InfoCircleFilledIcon class={`t-is-${this.theme}`} />
+            <InfoCircleFilledIcon class={`t-is-${props.theme}`} />
           );
-        icon = <div class="t-notification__icon">{iconType}</div>;
+        iconContent = <div class="t-notification__icon">{iconType}</div>;
       }
-      return icon;
-    },
-    renderClose() {
+      return iconContent;
+    };
+
+    const renderClose = (context: ComponentPublicInstance) => {
       const defaultClose = <CloseIcon />;
       return (
-        <span class={`${prefix}-message__close`} onClick={this.close}>
-          {renderTNodeJSX(this, 'closeBtn', defaultClose)}
+        <span class={`${prefix}-message__close`} onClick={close}>
+          {renderTNodeJSX(context, 'closeBtn', defaultClose)}
         </span>
       );
-    },
-    renderContent() {
-      return <div class={`${name}__content`}>{renderContent(this, 'default', 'content')}</div>;
-    },
+    };
+
+    const renderMainContent = (context: ComponentPublicInstance) => {
+      return <div class={`${name}__content`}>{renderContent(context, 'default', 'content')}</div>;
+    };
+
+    onMounted(() => {
+      if (props.duration > 0) {
+        const timer = setTimeout(() => {
+          clearTimeout(timer);
+          emitEvent('duration-end');
+        }, props.duration);
+      }
+    });
+
+    return {
+      renderIcon,
+      renderClose,
+      renderMainContent,
+    };
   },
   render() {
-    const icon = this.renderIcon();
-    const close = this.renderClose();
-    const content = this.renderContent();
+    const { renderIcon, renderClose, renderMainContent } = this;
+    const icon = renderIcon();
+    const close = renderClose(this);
+    const content = renderMainContent(this);
     const footer = renderTNodeJSX(this, 'footer');
     const title = renderTNodeJSX(this, 'title');
 
