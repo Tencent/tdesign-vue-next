bdd604b27918386e91e708440ccb3528efb40096
whylost <whylost@aliyun.com>
2022-03-13 23:06:04 +0800

    refactor(checkbox): refactor checkbox with vca

--- src/checkbox/group.tsx
+++ src/checkbox/group.tsx
@@ -1,8 +1,7 @@
-import { VNode, defineComponent, h } from 'vue';
+import { defineComponent, provide, computed, VNode, h, reactive, watchEffect, ref } from 'vue';
 import intersection from 'lodash/intersection';
 import Checkbox from './checkbox';
 import checkboxGroupProps from './checkbox-group-props';
-import { emitEvent } from '../utils/event';
 import { CheckboxOptionObj, TdCheckboxProps, CheckboxGroupValue, CheckboxGroupChangeContext } from './type';
 import { usePrefixClass } from '../config-provider';
 
@@ -11,96 +10,123 @@ export default defineComponent({
   components: {
     Checkbox,
   },
-  provide(): { checkboxGroup: any } {
-    return {
-      checkboxGroup: this,
-    };
-  },
   props: { ...checkboxGroupProps },
   emits: ['change'],
-  setup() {
-    const COMPONENT_NAME = usePrefixClass('checkbox-group');
 
-    return {
-      COMPONENT_NAME,
-    };
-  },
-  data() {
-    return {
-      checkedMap: {},
-      optionList: [] as Array<CheckboxOptionObj>,
-    };
-  },
-  computed: {
-    values(): string {
-      if (this.value instanceof Array) {
-        return this.value.join();
+  setup(props, { emit, slots }) {
+    // data
+    const checkedMap = ref({});
+    const optionList = ref<Array<CheckboxOptionObj>>([]);
+
+    // computed
+    const values = computed<string>(() => {
+      if (props.value instanceof Array) {
+        return props.value.join();
       }
       return '';
-    },
-    intersectionLen(): number {
-      const values = this.optionList.map((item) => item.value);
-      if (this.value instanceof Array) {
-        const n = intersection(this.value, values);
+    });
+
+    const intersectionLen = computed<number>(() => {
+      const values = optionList.value.map((item) => item.value);
+      if (props.value instanceof Array) {
+        const n = intersection(props.value, values);
         return n.length;
       }
       return 0;
-    },
-    isCheckAll(): boolean {
-      if (this.value instanceof Array && this.value.length !== this.optionList.length - 1) {
+    });
+
+    const isCheckAll = computed<boolean>(() => {
+      if (props.value instanceof Array && props.value.length !== optionList.value.length - 1) {
         return false;
       }
-      return this.intersectionLen === this.optionList.length - 1;
-    },
-    indeterminate(): boolean {
-      return !this.isCheckAll && this.intersectionLen < this.optionList.length && this.intersectionLen !== 0;
-    },
-    maxExceeded(): boolean {
-      return this.max !== undefined && this.value.length === this.max;
-    },
-  },
+      return intersectionLen.value === optionList.value.length - 1;
+    });
 
-  watch: {
-    values: {
-      immediate: true,
-      handler() {
-        if (this.value instanceof Array) {
-          const map = {};
-          this.value.forEach((item: string | number) => {
-            map[item] = true;
-          });
-          this.checkedMap = map;
-        }
-      },
-    },
-    options: {
-      immediate: true,
-      handler() {
-        if (!this.options) return [];
-        this.optionList = this.options.map((item) => {
-          let r: CheckboxOptionObj = {};
-          if (typeof item !== 'object') {
-            r = { label: String(item), value: item };
-          } else {
-            r = { ...item };
-            r.disabled = r.disabled === undefined ? this.disabled : r.disabled;
-          }
-          return r;
+    const indeterminate = computed<boolean>(
+      () => !isCheckAll.value && intersectionLen.value < optionList.value.length && intersectionLen.value !== 0,
+    );
+
+    const maxExceeded = computed<boolean>(() => props.max !== undefined && props.value.length === props.max);
+
+    // watch
+    watchEffect(() => {
+      if (props.value instanceof Array) {
+        const map = {};
+        props.value.forEach((item: string | number) => {
+          map[item] = true;
         });
-      },
-    },
-  },
+        checkedMap.value = map;
+      }
+    });
 
-  methods: {
-    onCheckedChange(p: { checked: boolean; checkAll: boolean; e: Event; option: TdCheckboxProps }) {
+    watchEffect(() => {
+      if (!props.options) return [];
+      optionList.value = props.options.map((item) => {
+        let r: CheckboxOptionObj = {};
+        if (typeof item !== 'object') {
+          r = { label: String(item), value: item };
+        } else {
+          r = { ...item };
+          r.disabled = r.disabled === undefined ? props.disabled : r.disabled;
+        }
+        return r;
+      });
+    });
+
+    // methods
+    const emitChange = (val: CheckboxGroupValue, context: CheckboxGroupChangeContext) => {
+      emit('change', val, context);
+    };
+    const getAllCheckboxValue = (): CheckboxGroupValue => {
+      const val = new Set<TdCheckboxProps['value']>();
+      for (let i = 0, len = optionList.value.length; i < len; i++) {
+        const item = optionList.value[i];
+        if (item.checkAll) continue;
+        val.add(item.value);
+        if (maxExceeded.value) break;
+      }
+      return [...val];
+    };
+
+    const onCheckAllChange = (checked: boolean, context: { e: Event; source?: 't-checkbox' }) => {
+      const value: CheckboxGroupValue = checked ? getAllCheckboxValue() : [];
+      emitChange(value, {
+        e: context.e,
+        type: checked ? 'check' : 'uncheck',
+        current: undefined,
+      });
+    };
+
+    const handleCheckboxChange = (data: { checked: boolean; e: Event; option: TdCheckboxProps }) => {
+      const currentValue = data.option.value;
+      if (props.value instanceof Array) {
+        const val = [...props.value];
+        if (data.checked) {
+          val.push(currentValue);
+        } else {
+          const i = val.indexOf(currentValue);
+          val.splice(i, 1);
+        }
+        emitChange(val, {
+          e: data.e,
+          current: data.option.value,
+          type: data.checked ? 'check' : 'uncheck',
+        });
+      } else {
+        console.warn(`TDesign CheckboxGroup Warn: \`value\` must be an array, instead of ${typeof props.value}`);
+      }
+    };
+
+    const onCheckedChange = (p: { checked: boolean; checkAll: boolean; e: Event; option: TdCheckboxProps }) => {
       const { checked, checkAll, e } = p;
       if (checkAll) {
-        this.onCheckAllChange(checked, { e });
+        onCheckAllChange(checked, { e });
       } else {
-        this.handleCheckboxChange(p);
+        handleCheckboxChange(p);
       }
-    },
-    getOptionListBySlots(nodes: VNode[]) {
+    };
+
+    const getOptionListBySlots = (nodes: VNode[]) => {
       const arr: Array<CheckboxOptionObj> = [];
       nodes?.forEach((node) => {
         const option = node.props as CheckboxOptionObj;
@@ -110,69 +136,44 @@ export default defineComponent({
         option && arr.push(option);
       });
       return arr;
-    },
-    renderLabel(option: CheckboxOptionObj) {
+    };
+
+    const renderLabel = (option: CheckboxOptionObj) => {
       if (typeof option.label === 'function') {
         return option.label(h);
       }
       return option.label;
-    },
-    emitChange(val: CheckboxGroupValue, context: CheckboxGroupChangeContext) {
-      emitEvent(this, 'change', val, context);
-    },
-    handleCheckboxChange(data: { checked: boolean; e: Event; option: TdCheckboxProps }) {
-      const currentValue = data.option.value;
-      if (this.value instanceof Array) {
-        const val = [...this.value];
-        if (data.checked) {
-          val.push(currentValue);
-        } else {
-          const i = val.indexOf(currentValue);
-          val.splice(i, 1);
-        }
-        this.emitChange(val, {
-          e: data.e,
-          current: data.option.value,
-          type: data.checked ? 'check' : 'uncheck',
-        });
-      } else {
-        console.warn(`TDesign CheckboxGroup Warn: \`value\` must be an array, instead of ${typeof this.value}`);
-      }
-    },
-    getAllCheckboxValue(): CheckboxGroupValue {
-      const val = new Set<TdCheckboxProps['value']>();
-      for (let i = 0, len = this.optionList.length; i < len; i++) {
-        const item = this.optionList[i];
-        if (item.checkAll) continue;
-        val.add(item.value);
-        if (this.maxExceeded) break;
-      }
-      return [...val];
-    },
-    onCheckAllChange(checked: boolean, context: { e: Event; source?: 't-checkbox' }) {
-      const value: CheckboxGroupValue = checked ? this.getAllCheckboxValue() : [];
-      this.emitChange(value, {
-        e: context.e,
-        type: checked ? 'check' : 'uncheck',
-        current: undefined,
-      });
-    },
-  },
+    };
 
-  render(): VNode {
-    const { COMPONENT_NAME } = this;
-    let children = null;
-    if (this.options?.length) {
-      children = this.optionList?.map((option, index) => (
-        <Checkbox key={`${option.value}${index}`} {...option} checked={this.checkedMap[option.value]}>
-          {this.renderLabel(option)}
-        </Checkbox>
-      ));
-    } else {
-      const nodes = this.$slots.default && this.$slots.default(null);
-      this.optionList = this.getOptionListBySlots(nodes);
-      children = nodes;
-    }
-    return <div class={COMPONENT_NAME}>{children}</div>;
+    // provide
+    provide(
+      'checkboxGroup',
+      reactive({
+        name: props.name,
+        isCheckAll,
+        checkedMap,
+        maxExceeded,
+        disabled: props.disabled,
+        indeterminate,
+        handleCheckboxChange,
+        onCheckedChange,
+      }),
+    );
+
+    return () => {
+      let children = null;
+      if (props.options?.length) {
+        children = optionList.value?.map((option, index) => (
+          <Checkbox key={`${option.value}${index}`} {...option} checked={checkedMap.value[option.value]}>
+            {renderLabel(option)}
+          </Checkbox>
+        ));
+      } else {
+        const nodes = slots.default && slots.default(null);
+        optionList.value = getOptionListBySlots(nodes);
+        children = nodes;
+      }
+      return <div class={name}>{children}</div>;
+    };
   },
 });
