b1c36392e927d247523f5931e254d8f90006b5eb
pengYYY <pengyue970715@gmail.com>
2022-03-25 23:24:11 +0800

    feat: use common config

--- src/config-provider/useConfig.tsx
+++ src/config-provider/useConfig.tsx
@@ -1,7 +1,9 @@
 import { computed, inject } from 'vue';
 import cloneDeep from 'lodash/cloneDeep';
 import _mergeWith from 'lodash/mergeWith';
-import { defaultGlobalConfig, GlobalConfig } from './context';
+import { defaultGlobalConfig } from './context';
+
+type GlobalConfig = typeof defaultGlobalConfig;
 
 // deal with https://github.com/lodash/lodash/issues/1313
 export const merge = (defaultGlobalConfig: GlobalConfig, injectConfig: GlobalConfig) =>
