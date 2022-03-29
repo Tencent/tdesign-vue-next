648065f61936daab4383df69b63e86f84368912c
Godlike-meteor <1814533740@qq.com>
2022-03-29 16:45:31 +0800

    refactor(tree-select): refactor component

--- src/tree-select/index.ts
+++ src/tree-select/index.ts
@@ -1,17 +1,8 @@
 import _TreeSelect from './tree-select';
-import mapProps from '../utils/map-props';
 import { withInstall, WithInstallType } from '../utils/withInstall';
 
 import './style';
 
 export * from './interface';
-export const TreeSelect: WithInstallType<typeof _TreeSelect> = withInstall(
-  mapProps([
-    {
-      name: 'value',
-      event: ['change', 'clear', 'focus', 'blur', 'remove', 'search'],
-      alias: ['modelValue'],
-    },
-  ])(_TreeSelect),
-);
+export const TreeSelect: WithInstallType<typeof _TreeSelect> = withInstall(_TreeSelect);
 export default TreeSelect;
