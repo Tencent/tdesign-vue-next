b4704cdcbe33a809044df9dcd90e2b444a842343
zihao <497244062@qq.com>
2022-04-02 14:16:34 +0800

    refactor: add expandOnRowClick demo

--- examples/collapse/demos/icon.vue
+++ examples/collapse/demos/icon.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="tdesign-collapse">
-    <t-collapse :expand-icon="showArrow" :expand-icon-placement="direction">
+    <t-collapse :expand-icon="showArrow" :expand-icon-placement="direction" :expand-on-row-click="!onlyIcon">
       <t-collapse-panel value="0" header="这是一个折叠标题">
         这部分是每个折叠面板折叠或展开的内容，可根据不同业务或用户的使用诉求，进行自定义填充。可以是纯文本、图文、子列表等内容形式。
       </t-collapse-panel>
@@ -25,6 +25,9 @@
         <t-radio value="right">右边</t-radio>
       </t-radio-group>
     </div>
+    <div style="margin-top: 10px">
+      <t-checkbox v-model="onlyIcon">仅图标响应点击</t-checkbox>
+    </div>
   </div>
 </template>
 
@@ -34,4 +37,5 @@ import { ref } from 'vue';
 const showArrow = ref(true);
 const direction = ref('left');
 const disable = ref(false);
+const onlyIcon = ref(false);
 </script>
