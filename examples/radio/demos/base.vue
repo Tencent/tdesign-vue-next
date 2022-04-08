7d253e6bd81bf1ee3fd8abb2fa262344ab8c74c1
k1nz <kinz.qiu@foxmail.com>
2022-04-08 13:54:46 +0800

    feat(radio): add allow uncheck demo
    * test(radio): update snap

--- examples/radio/demos/base.vue
+++ examples/radio/demos/base.vue
@@ -2,6 +2,7 @@
   <div class="tdesign-radio-base">
     <div class="tdesign-demo-block">
       <t-radio>未选中项</t-radio>
+      <t-radio allow-uncheck> 可取消选中项 </t-radio>
       <t-radio :checked="true"> 选中项 </t-radio>
       <t-radio :checked="true" disabled> 选中禁用态 </t-radio>
       <t-radio disabled> 未选中禁用态 </t-radio>
