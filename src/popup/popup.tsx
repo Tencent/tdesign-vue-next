90510bf7aace551a999879d5230d3c803867d437
cca313 <30116450@qq.com>
2022-04-10 18:02:57 +0800

    refactor(tooltip): rewrite tooltip with composition api

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -105,6 +105,7 @@ export default defineComponent({
   },
   watch: {
     visible(val) {
+      // console.log(val);
       if (val) {
         this.updatePopper();
         if (!this.hasDocumentEvent && (this.hasTrigger['context-menu'] || this.hasTrigger.click)) {
@@ -324,13 +325,18 @@ export default defineComponent({
       this.emitPopVisible(false, { trigger: 'document' });
     },
     emitPopVisible(val: boolean, context: PopupVisibleChangeContext) {
-      this.$nextTick(() => {
-        emitEvent(this, 'visible-change', val, context);
-      });
+      // this.$nextTick(() => {
+      // console.log(val, context);
+      emitEvent(this, 'visible-change', val, context);
+      // });
+      // setTimeout(() => {
+      //   emitEvent(this, 'visible-change', val, context);
+      // }, 1000);
     },
   },
 
   render() {
+    // console.log(this.visible);
     return (
       <div class={`${this.COMPONENT_NAME}__reference`}>
         <transition
