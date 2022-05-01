a64ee8d88b5f92e031812bab0bd26d61548043b7
btea <2356281422@qq.com>
2022-05-01 20:25:15 +0800

    docs: add demo

--- examples/swiper/demos/current.vue
+++ examples/swiper/demos/current.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="tdesign-demo-block--swiper-new">
-    <t-swiper direction="vertical" :navigation="{ showSlideBtn: 'never' }" :height="280" :autoplay="false">
+    <t-swiper :current="current" :navigation="{ showSlideBtn: 'never' }">
       <t-swiper-item>
         <div class="demo-item">1</div>
       </t-swiper-item>
@@ -20,5 +20,20 @@
         <div class="demo-item">6</div>
       </t-swiper-item>
     </t-swiper>
+    <div class="tdesign-demo-block-row">
+      <t-button size="small" @click="current = current + 2 > 6 ? 0 : current + 1">
+        跳转到第 {{ current + 2 >= 7 ? 1 : current + 2 }} 项
+      </t-button>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      current: 0,
+    };
+  },
+};
+</script>
