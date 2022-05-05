bde884dd99a9765b3258511f84b48c00137de998
arkfang <stephenark@163.com>
2022-05-05 10:56:23 +0800

    fix(#505): 修复 form-item 值校验响应问题

--- src/form/form-item.tsx
+++ src/form/form-item.tsx
@@ -66,6 +66,7 @@ export default defineComponent({
       resetValidating: false as boolean,
       needResetField: false as boolean,
       initialValue: undefined as ValueType,
+      value: undefined as ValueType,
     };
   },
 
@@ -133,10 +134,6 @@ export default defineComponent({
 
       return contentStyle;
     },
-    value(): ValueType {
-      const parent = this.form;
-      return parent && parent.data && lodashGet(parent.data, this.name);
-    },
     hasColon(): boolean {
       const parent = this.form;
       return !!(parent && parent.colon && this.getLabelContent());
@@ -163,8 +160,18 @@ export default defineComponent({
   },
 
   watch: {
-    value() {
-      this.validate('change');
+    'form.data': {
+      deep: true,
+      immediate: true,
+      handler(val) {
+        this.value = lodashGet(val, this.name);
+      },
+    },
+    value: {
+      deep: true,
+      handler() {
+        this.validate('change');
+      },
     },
   },
 
