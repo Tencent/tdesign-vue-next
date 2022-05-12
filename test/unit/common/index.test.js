69d65a02ff263002dd6fb5d6d41d82b39bddf40a
pengYYY <pengyue970715@gmail.com>
2022-05-12 12:31:45 +0800

    feat: combin csr test

---
+++ test/unit/common/index.test.js
@@ -0,0 +1,23 @@
+import glob from 'glob';
+import MockDate from 'mockdate';
+import { mount } from '@vue/test-utils';
+
+// 固定时间，当使用 new Date() 时，返回固定时间，防止“当前时间”的副作用影响，导致 snapshot 变更，mockdate 插件见 https://github.com/boblauer/MockDate
+MockDate.set('2020-12-28');
+
+function ssrSnapshotTest() {
+  const files = glob.sync('./examples/**/demos/*.vue');
+
+  describe('csr snapshot test', () => {
+    files.forEach((file) => {
+      it(`ssr test ${file}`, async () => {
+        const demo = await import(`../../.${file}`);
+        const realDemoComp = demo.default ? demo.default : demo;
+        const wrapper = mount(realDemoComp);
+        expect(wrapper.element).toMatchSnapshot();
+      });
+    });
+  });
+}
+
+ssrSnapshotTest();
