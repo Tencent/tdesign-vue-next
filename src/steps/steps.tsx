27ede0b1546f67eddf3784c016f2dca00fd2cd92
HQ-Lin <a154571896@gmail.com>
2022-05-13 17:32:56 +0800

    fix: 修复 steps extra 渲染问题

--- src/steps/steps.tsx
+++ src/steps/steps.tsx
@@ -89,10 +89,28 @@ export default defineComponent({
     const renderContent = () => {
       let content = null;
       const options = getOptions();
+      const nodes: VNode[] = getChildComponentByName('TStepItem') as VNode[];
+
       content = options.map((item, index) => {
         const stepIndex = props.sequence === 'reverse' ? options.length - index - 1 : index;
-        return <t-step-item {...item} index={stepIndex} status={handleStatus(item, index)} key={item.value || index} />;
+        const propsData = {
+          ...item,
+          index: stepIndex,
+          status: handleStatus(item, index),
+        };
+
+        const stepItem = <t-step-item props={{ ...propsData }} key={item.value || index} />;
+
+        if (nodes && nodes[index]) {
+          const vnode = nodes[index];
+          if (vnode.props) {
+            vnode.props = propsData;
+            return vnode;
+          }
+        }
+        return stepItem;
       });
+
       return content;
     };
 
