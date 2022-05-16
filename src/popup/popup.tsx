c971468e8c7f0e607f01ac1cc3f3b80e0d41a013
ikeq <ikeq@foxmail.com>
2022-05-16 17:00:55 +0800

    fix(popup): add `zIndex` api (#788)

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -341,7 +341,7 @@ export default defineComponent({
         <div
           class={prefixCls}
           ref="popperEl"
-          style={hidePopup && { visibility: 'hidden', pointerEvents: 'none' }}
+          style={[hidePopup && { visibility: 'hidden', pointerEvents: 'none' }, { zIndex: this.zIndex }]}
           vShow={innerVisible}
           onMousedown={() => {
             this.contentClicked = true;
@@ -396,8 +396,7 @@ export default defineComponent({
             <Transition
               name={this.expandAnimation ? `${prefixCls}--animation-expand` : `${prefixCls}--animation`}
               appear
-              onBeforeEnter={this.updatePopper}
-              onAfterEnter={this.updatePopper}
+              onEnter={this.updatePopper}
               onAfterLeave={this.destroyPopper}
             >
               {overlay}
