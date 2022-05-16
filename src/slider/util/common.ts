f44cfa5271d7f40cb105c42502d3b2e4294aa2c5
李铭辉 <1018596741@qq.com>
2022-05-16 13:26:23 +0800

    fix(slider): fix the timing of onChange event trigger

--- src/slider/util/common.ts
+++ src/slider/util/common.ts
@@ -7,3 +7,22 @@
 export const getStopStyle = (position: number, isVertical: boolean) => {
   return isVertical ? { top: `calc(${100 - position}% - 1px)` } : { left: `${position}%` };
 };
+
+/**
+ * 格式化返回slider初始值
+ * @param val slider传入的value
+ * @param type 第一个滑块or第二个滑块值
+ * @returns {number}
+ */
+export const formatSlderValue = (val: number | number[], type: 'first' | 'second') => {
+  if (type === 'first') {
+    if (val instanceof Array) {
+      return val[0];
+    }
+    return val;
+  }
+  if (val instanceof Array) {
+    return val[1];
+  }
+  return 0;
+};
