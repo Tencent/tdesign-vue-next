4205a282b5689cef23fb882b7cfb83a61790c543
chaishi <yaoyanhuoyi@qq.com>
2022-05-22 19:11:56 +0800

    fix(table): onBlur and onClear were triggered at the same time

--- src/input/useInput.ts
+++ src/input/useInput.ts
@@ -8,12 +8,14 @@ import useVModel from '../hooks/useVModel';
 export default function useInput(props: TdInputProps, expose: (exposed: Record<string, any>) => void) {
   const { value, modelValue } = toRefs(props);
   const inputValue = ref<InputValue>();
+  const clearIconRef = ref(null);
+  const innerClickElement = ref();
   const [innerValue, setInnerValue] = useVModel(value, modelValue, props.defaultValue, props.onChange);
 
   const isHover = ref(false);
   const focused = ref(false);
   const renderType = ref(props.type);
-  const inputRef = ref(null);
+  const inputRef = ref<HTMLInputElement>(null);
   const inputPreRef = ref(null);
 
   const showClear = computed(() => {
@@ -32,10 +34,16 @@ export default function useInput(props: TdInputProps, expose: (exposed: Record<s
     focused.value = true;
     props.onFocus?.(innerValue.value, { e });
   };
+
   const emitClear = ({ e }: { e: MouseEvent }) => {
     props.onClear?.({ e });
     setInnerValue('');
   };
+
+  const onClearIconMousedown = (e: MouseEvent) => {
+    innerClickElement.value = e.target;
+  };
+
   const emitPassword = () => {
     const toggleType = renderType.value === 'password' ? 'text' : 'password';
     renderType.value = toggleType;
@@ -63,10 +71,23 @@ export default function useInput(props: TdInputProps, expose: (exposed: Record<s
     // 受控
     nextTick(() => setInputElValue(innerValue.value));
   };
+
   const handleInput = (e: InputEvent) => {
     const checkInputType = e.inputType && e.inputType === 'insertCompositionText';
     if (e.isComposing || checkInputType) return;
     inputValueChangeHandle(e);
+  };
+
+  const isClearIcon = () => {
+    let tmp = innerClickElement.value;
+    if (!tmp || !tmp.tagName || !clearIconRef.value?.$el || !['path', 'svg'].includes(tmp.tagName)) return false;
+    while (tmp) {
+      if (clearIconRef.value?.$el === tmp) {
+        return true;
+      }
+      tmp = tmp.parentNode;
+    }
+    return false;
   };
 
   const formItem = inject(FormItemInjectionKey, undefined);
@@ -75,8 +96,11 @@ export default function useInput(props: TdInputProps, expose: (exposed: Record<s
       inputValue.value = props.format(innerValue.value);
     }
     focused.value = false;
-    props.onBlur?.(props.value, { e });
-    formItem?.handleBlur();
+    // @ts-ignore 点击清空按钮的时候，不应该触发 onBlur 事件。这个规则在表格单元格编辑中有很重要的应用
+    if (!isClearIcon()) {
+      props.onBlur?.(props.value, { e });
+      formItem?.handleBlur();
+    }
   };
 
   const onHandleCompositionend = (e: CompositionEvent) => {
@@ -122,6 +146,7 @@ export default function useInput(props: TdInputProps, expose: (exposed: Record<s
     renderType,
     showClear,
     inputRef,
+    clearIconRef,
     inputValue,
     emitFocus,
     formatAndEmitBlur,
@@ -131,6 +156,7 @@ export default function useInput(props: TdInputProps, expose: (exposed: Record<s
     emitPassword,
     handleInput,
     emitClear,
+    onClearIconMousedown,
     innerValue,
     inputPreRef,
   };
