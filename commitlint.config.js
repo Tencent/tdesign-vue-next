a9bd481d88b374eeed12fd9ac67d5302c3136839
Zhengqbbb <1074059947@qq.com>
2022-05-23 09:48:45 +0800

    chore: use cz-git commitizen adapter

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,18 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+const fs = require('fs');
+const path = require('path');
+
+const packages = fs.readdirSync(path.resolve(__dirname, 'src'));
+
+/** @type {import('cz-git').UserConfig} */
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  prompt: {
+    scopes: [...packages],
+    customScopesAlign: 'top-bottom',
+    scopeOverrides: {
+      test: ['unit', 'e2e'],
+    },
+    allowEmptyIssuePrefixs: false,
+    allowCustomIssuePrefixs: false,
+  },
+};
