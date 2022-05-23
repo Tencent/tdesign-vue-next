a0b6857c5d01b11db08dbc489ca1f5542de937b9
Zhengqbbb <1074059947@qq.com>
2022-05-23 23:21:21 +0800

    chore: add precomputed scope to cz-git

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,17 +1,29 @@
 const fs = require('fs');
 const path = require('path');
+const { execSync } = require('child_process');
 
-const packages = fs.readdirSync(path.resolve(__dirname, 'src'));
+const packages = fs
+  .readdirSync(path.resolve(__dirname, 'src'))
+  .filter((r) => ['.DS_Store', '_common'].indexOf(r) === -1);
+
+// precomputed scope
+const scopeComplete = execSync("git status -s | grep 'src' 2> /dev/null")
+  .toString()
+  .trim()
+  .split('\n')
+  .find((r) => r.indexOf('M  ') !== -1)
+  ?.match(/src\/(\S*)\//)?.[1];
 
 /** @type {import('cz-git').UserConfig} */
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   prompt: {
     scopes: [...packages],
-    customScopesAlign: 'top-bottom',
+    customScopesAlign: !scopeComplete ? 'top-bottom' : 'bottom',
     scopeOverrides: {
       test: ['unit', 'e2e'],
     },
+    defaultScope: scopeComplete,
     allowEmptyIssuePrefixs: false,
     allowCustomIssuePrefixs: false,
   },
