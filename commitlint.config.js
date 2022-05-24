9e15b79131a9e2a9d7ab1fce9fb0864a7ee61bbe
zhengqbbb <40693636+Zhengqbbb@users.noreply.github.com>
2022-05-24 11:07:05 +0800

    chore: use cz-git commitizen adapter (#847)
    * chore: use cz-git commitizen adapter
    
    * chore: add precomputed scope to cz-git
    
    * chore: update scope type filter dir to cz-git

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,31 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+const fs = require('fs');
+const path = require('path');
+const { execSync } = require('child_process');
+
+const packages = fs
+  .readdirSync(path.resolve(__dirname, 'src'), { withFileTypes: true })
+  .filter((dirent) => dirent.isDirectory())
+  .map((dirent) => dirent.name);
+
+// precomputed scope
+const scopeComplete = execSync("git status -s | grep 'src' 2> /dev/null")
+  .toString()
+  .trim()
+  .split('\n')
+  .find((r) => r.indexOf('M  ') !== -1)
+  ?.match(/src\/(\S*)\//)?.[1];
+
+/** @type {import('cz-git').UserConfig} */
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  prompt: {
+    scopes: [...packages],
+    customScopesAlign: !scopeComplete ? 'top-bottom' : 'bottom',
+    scopeOverrides: {
+      test: ['unit', 'e2e'],
+    },
+    defaultScope: scopeComplete,
+    allowEmptyIssuePrefixs: false,
+    allowCustomIssuePrefixs: false,
+  },
+};
