24e5fcfbe37df645380526b42c6cef427285ce4e
PY <pengyue970715@gmail.com>
2022-05-27 11:19:37 +0800

    Fix/tabs/addable (#880)
    * fix: tabs addable
    
    * fix: tabs normal addable
    
    * test: update snap

--- examples/tabs/usage/index.vue
+++ examples/tabs/usage/index.vue
@@ -2,7 +2,7 @@
 <template>
   <base-usage :code="usageCode" :config-list="configList" :panel-list="panelList" @PanelChange="onPanelChange">
     <template #tabs="{ configProps }"
-      ><t-tabs v-bind="configProps">
+      ><t-tabs v-bind="configProps" :default-value="1">
         <t-tab-panel :value="1" label="选项卡1">
           <p style="margin: 20px">选项卡1内容区</p>
         </t-tab-panel>
@@ -26,7 +26,7 @@ const configList = ref(configJson);
 const panelList = [{ label: 'tabs', value: 'tabs' }];
 
 const usageCodeMap = {
-  tabs: '\n        <t-tabs v-bind="configProps">\n          <t-tab-panel :value="1" label="选项卡1">\n            <p style="margin: 20px">选项卡1内容区</p>\n          </t-tab-panel>\n          <t-tab-panel :value="2" label="选项卡2">\n            <p style="margin: 20px">选项卡2内容区</p>\n          </t-tab-panel>\n          <t-tab-panel :value="3" label="选项卡3">\n            <p style="margin: 20px">选项卡3内容区</p>\n          </t-tab-panel>\n        </t-tabs>\n      ',
+  tabs: '\n        <t-tabs v-bind="configProps" :default-value="1">\n          <t-tab-panel :value="1" label="选项卡1">\n            <p style="margin: 20px">选项卡1内容区</p>\n          </t-tab-panel>\n          <t-tab-panel :value="2" label="选项卡2">\n            <p style="margin: 20px">选项卡2内容区</p>\n          </t-tab-panel>\n          <t-tab-panel :value="3" label="选项卡3">\n            <p style="margin: 20px">选项卡3内容区</p>\n          </t-tab-panel>\n        </t-tabs>\n      ',
 };
 const usageCode = ref(`<template>${usageCodeMap[panelList[0].value].trim()}</template>`);
 
