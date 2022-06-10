2fd0416d50b15c263dab56d2467673e598e09f45
pengYYY <pengyue970715@gmail.com>
2022-06-10 17:45:26 +0800

    chore: expand update

--- test/unit/tree/expand.test.jsx
+++ test/unit/tree/expand.test.jsx
@@ -202,26 +202,26 @@ describe('Tree:expand', () => {
       await delay(10);
       expect(wrapper.find('[data-value="t1.1"]').exists()).toBe(false);
     });
-  });
 
-  it('expandOnClickNode 设置为 true 时，点击父节点本身会触发展开子节点', async () => {
-    const data = [
-      {
-        value: 't1',
-        children: [
-          {
-            value: 't1.1',
-          },
-        ],
-      },
-    ];
-    const wrapper = mount({
-      render() {
-        return <Tree data={data} expandOnClickNode></Tree>;
-      },
+    it('expandOnClickNode 设置为 true 时，点击父节点本身会触发展开子节点', async () => {
+      const data = [
+        {
+          value: 't1',
+          children: [
+            {
+              value: 't1.1',
+            },
+          ],
+        },
+      ];
+      const wrapper = mount({
+        render() {
+          return <Tree data={data} expandOnClickNode></Tree>;
+        },
+      });
+      wrapper.find('[data-value="t1"]').trigger('click');
+      await delay(10);
+      expect(wrapper.find('[data-value="t1.1"]').exists()).toBe(true);
     });
-    wrapper.find('[data-value="t1"]').trigger('click');
-    await delay(10);
-    expect(wrapper.find('[data-value="t1.1"]').exists()).toBe(true);
   });
 });
