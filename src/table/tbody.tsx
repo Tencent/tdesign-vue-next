ef4b68598df0a5ceef43b85cbe29836612999cc3
chaishi <yaoyanhuoyi@qq.com>
2022-06-10 13:21:44 +0800

    fix(table): dynamic data fro rowspan and colspan

--- src/table/tbody.tsx
+++ src/table/tbody.tsx
@@ -1,4 +1,4 @@
-import { defineComponent, computed, PropType, SetupContext } from 'vue';
+import { defineComponent, computed, PropType, SetupContext, toRefs } from 'vue';
 import camelCase from 'lodash/camelCase';
 import get from 'lodash/get';
 import pick from 'lodash/pick';
@@ -10,6 +10,7 @@ import { useTNodeJSX } from '../hooks/tnode';
 import useClassName from './hooks/useClassName';
 import baseTableProps from './base-table-props';
 import { TNodeReturnValue } from '../common';
+import useRowspanAndColspan from './hooks/useRowspanAndColspan';
 
 export const ROW_AND_TD_LISTENERS = ROW_LISTENERS.concat('cell-click');
 export interface TableBodyProps extends BaseTableProps {
@@ -87,8 +88,10 @@ export default defineComponent({
   // eslint-disable-next-line
   setup(props: TableBodyProps, { emit }: SetupContext) {
     const renderTNode = useTNodeJSX();
+    const { data, columns } = toRefs(props);
     const { t, global } = useConfig('table');
     const { tableFullRowClasses, tableBaseClass } = useClassName();
+    const { skipSpansMap } = useRowspanAndColspan(data, columns, props.rowspanAndColspan);
 
     const tbodyClasses = computed(() => [tableBaseClass.body]);
 
@@ -99,6 +102,7 @@ export default defineComponent({
       tableFullRowClasses,
       tbodyClasses,
       tableBaseClass,
+      skipSpansMap,
     };
   },
 
@@ -139,28 +143,9 @@ export default defineComponent({
       );
     };
 
-    // 受合并单元格影响，部分单元格不显示
-    let skipSpansMap = new Map<any, boolean>();
-
-    const onTrRowspanOrColspan = (params: BaseTableCellParams<TableRowData>, cellSpans: RowspanColspan) => {
-      const { rowIndex, colIndex } = params;
-      if (!cellSpans.rowspan && !cellSpans.colspan) return;
-      const maxRowIndex = rowIndex + (cellSpans.rowspan || 1);
-      const maxColIndex = colIndex + (cellSpans.colspan || 1);
-      for (let i = rowIndex; i < maxRowIndex; i++) {
-        for (let j = colIndex; j < maxColIndex; j++) {
-          if (i !== rowIndex || j !== colIndex) {
-            skipSpansMap.set([i, j].join(), true);
-          }
-        }
-      }
-    };
-
     const columnLength = this.columns.length;
     const dataLength = this.data.length;
     const trNodeList: TNodeReturnValue[] = [];
-    // 每次渲染清空合并单元格信息
-    skipSpansMap = new Map<any, boolean>();
 
     const properties = [
       'rowAndColFixedPosition',
@@ -181,10 +166,9 @@ export default defineComponent({
         columns: this.columns,
         rowIndex,
         dataLength,
-        skipSpansMap,
+        skipSpansMap: this.skipSpansMap,
         ...pick(this.$props, properties),
         // 遍历的同时，计算后面的节点，是否会因为合并单元格跳过渲染
-        onTrRowspanOrColspan,
       };
       if (this.onCellClick) {
         trProps.onCellClick = this.onCellClick;
