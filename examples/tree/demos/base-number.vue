22efa57a03254f899b17d531c24d9f636efa4686
sufuwang <1137120948@qq.com>
2022-06-22 19:24:39 +0800

    feat(tree): 新增base-number

---
+++ examples/tree/demos/base-number.vue
@@ -0,0 +1,71 @@
+<template>
+  <t-tree hover :data="items" :checkable="true" @change="onChange" @click="onClick" />
+</template>
+
+<script setup>
+const items = [
+  {
+    label: '第一段',
+    value: 1,
+    children: [
+      {
+        value: 1.1,
+        label: '第一段',
+      },
+      {
+        value: 1.2,
+        label: '第二段',
+      },
+    ],
+  },
+  {
+    label: '第二段',
+    value: 2,
+    children: [
+      {
+        value: 2.1,
+        label: '第一段',
+      },
+      {
+        value: 2.2,
+        label: '第二段',
+      },
+    ],
+  },
+  {
+    label: '第三段',
+    value: 'a',
+    children: [
+      {
+        value: 'a.1',
+        label: '第一段',
+      },
+      {
+        value: 'a.2',
+        label: '第二段',
+      },
+    ],
+  },
+  {
+    label: '第四段',
+    value: 'b',
+    children: [
+      {
+        value: 'b.1',
+        label: '第一段',
+      },
+      {
+        value: 'b.2',
+        label: '第二段',
+      },
+    ],
+  },
+];
+
+const onClick = (context) => {
+  console.info('onClick:', context);
+};
+const onChange = (checked, context) => {
+  console.info('onChange:', checked, context);
+};
+</script>
