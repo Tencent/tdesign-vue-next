91899ce645e74adab1f4bc0cdfa74291ed97d6ac
pengYYY <pengyue970715@gmail.com>
2022-06-25 23:38:19 +0800

    test: snap update

--- src/input/input.tsx
+++ src/input/input.tsx
@@ -50,7 +50,8 @@ export default defineComponent({
         maxlength: props.maxlength,
         name: props.name || undefined,
         type: renderType.value,
-        autocomplete: renderType.value === 'password' ? 'on' : global.value.autocomplete ?? props.autocomplete,
+        autocomplete: global.value.autocomplete ?? props.autocomplete,
+        unselectable: props.readonly ? 'on' : undefined,
       }),
     );
 
