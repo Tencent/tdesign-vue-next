6c88ca66020a03664d9e72c9a291d4f65e86b17f
PY <pengyue970715@gmail.com>
2022-06-27 20:28:44 +0800

    fix: select filter (#1082)

--- src/select/hooks.ts
+++ src/select/hooks.ts
@@ -39,8 +39,8 @@ export const useSelectOptions = (props: TdSelectProps, keys: Ref<SelectKeysType>
     });
 
     // 处理 slots
-    const optionsSlots = getChildComponentSlots('TOption');
-    const groupSlots = getChildComponentSlots('TOptionGroup');
+    const optionsSlots = getChildComponentSlots('Option');
+    const groupSlots = getChildComponentSlots('OptionGroup');
 
     if (isArray(groupSlots)) {
       for (const group of groupSlots) {
