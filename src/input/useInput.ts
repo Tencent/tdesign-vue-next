9d33e5e6502446042e08e5a82537d4a42ef423c0
sinbadmaster <bless_lee@126.com>
2022-06-28 13:05:06 +0800

    fix(input): type=number支持maxlength限制输入长度
    closed #1079

--- src/input/useInput.ts
+++ src/input/useInput.ts
@@ -67,6 +67,9 @@ export default function useInput(props: TdInputProps, expose: (exposed: Record<s
       const stringInfo = getCharacterLength(val, props.maxcharacter);
       val = typeof stringInfo === 'object' && stringInfo.characters;
     }
+    if (props.type === 'number' && typeof props.maxlength === 'number' && props.maxlength >= 0) {
+      val = val.substring(0, props.maxlength);
+    }
     setInnerValue(val, { e } as { e: InputEvent });
     // 受控
     nextTick(() => setInputElValue(innerValue.value));
