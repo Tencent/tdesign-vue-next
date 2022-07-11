3b3ac1ae736336c8d9a75bca453189aec1b0abc5
Uyarn <uyarnchen@gmail.com>
2022-07-11 16:49:32 +0800

    feat(timepicker): support millisecond scene

--- examples/time-picker/demos/hm.vue
+++ examples/time-picker/demos/hm.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="tdesign-demo-block">
+    <h3>禁用整个选择器</h3>
+    <br />
     <t-time-picker format="HH:mm" />
+    <br />
+    <br />
+    <h3>禁用整个选择器</h3>
+    <br />
+    <t-time-picker default-value="12:59:59:000" format="HH:mm:ss:SSS" />
   </div>
 </template>
