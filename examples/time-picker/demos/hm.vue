33c487363ffc0caa1739dbd0c849de9ad0419347
Uyarn <uyarnchen@gmail.com>
2022-07-11 16:54:05 +0800

    chore: update snapshot

--- examples/time-picker/demos/hm.vue
+++ examples/time-picker/demos/hm.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="tdesign-demo-block">
-    <h3>禁用整个选择器</h3>
+    <h3>时分选择</h3>
     <br />
     <t-time-picker format="HH:mm" />
     <br />
     <br />
-    <h3>禁用整个选择器</h3>
+    <h3>毫秒选择</h3>
     <br />
     <t-time-picker default-value="12:59:59:000" format="HH:mm:ss:SSS" />
   </div>
