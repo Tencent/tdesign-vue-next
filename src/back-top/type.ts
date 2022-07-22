4812eb2767d956494fd7aeedcaa153b3de2836bc
五月的夏天 <285258675@qq.com>
2022-07-23 00:15:25 +0800

    feat(backTop): 完成backTop组件全部功能及文档和demo说明

---
+++ src/back-top/type.ts
@@ -0,0 +1,28 @@
+/* eslint-disable */
+
+/**
+ * 该文件为脚本自动生成文件，请勿随意修改。如需修改请联系 PMC
+ * updated at 2021-12-12 19:17:30
+ * */
+ export interface TdBackTopProps {
+  /**
+   * 回到顶部按钮距离底部位置
+   * @default 50
+   */
+  bottom: string | number,
+  /**
+   * 回到顶部按钮距离右边位置
+   * @default 50
+   */
+  right: string | number,
+  /**
+   * 触发滚动的对象
+   * @default window
+   */
+  target: string,
+  /**
+   * 滚动高度达到此参数值才出现
+   * @default 200
+   */
+  visibleHeight: string | number
+ }
