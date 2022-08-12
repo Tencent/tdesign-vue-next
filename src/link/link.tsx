402c94106b6bcc8d0cc02617be757c1468bb4a6b
18651688439 <>
2022-08-12 17:35:12 +0800

    feat(link): icon渲染

--- src/link/link.tsx
+++ src/link/link.tsx
@@ -1,16 +1,15 @@
-import { computed, defineComponent, h } from 'vue';
-import camelCase from 'lodash/camelCase';
-import kebabCase from 'lodash/kebabCase';
+import { computed, defineComponent } from 'vue';
 import { useConfig, usePrefixClass, useCommonClassName } from '../hooks/useConfig';
-import { useContent } from '../hooks/tnode';
+import { useContent, useTNodeJSX } from '../hooks/tnode';
 import props from './props';
 
 export default defineComponent({
   name: 'TLink',
   props: { ...props },
   emits: ['click'],
-  setup(props, { slots, emit }) {
+  setup(props, { emit }) {
     const renderContent = useContent();
+    const renderTNodeJSX = useTNodeJSX();
     const COMPONENT_NAME = usePrefixClass('link');
     const { STATUS, SIZE } = useCommonClassName();
     const { classPrefix } = useConfig('classPrefix');
@@ -25,29 +24,14 @@ export default defineComponent({
         [`${COMPONENT_NAME.value}--hover-${props.hover}`]: !props.disabled,
       },
     ]);
-    // 渲染前后icon
-    const renderIcon = (icon: string | Function | undefined, iconType: 'prefix-icon' | 'suffix-icon') => {
-      if (typeof icon === 'function') {
-        return icon(h);
-      }
-      // 插槽名称为中划线
-      if (slots[kebabCase(iconType)]) {
-        return slots[kebabCase(iconType)](null);
-      }
-      // 插槽名称为驼峰
-      if (slots[camelCase(iconType)]) {
-        return slots[camelCase(iconType)](null);
-      }
-      return null;
-    };
     // 禁用时 无点击事件
     const handleClick = (event: MouseEvent) => {
       if (!props.disabled) emit('click', event);
     };
     return () => {
       const linkContent = renderContent('default', 'content');
-      const prefixIcon = renderIcon(props.prefixIcon, 'prefix-icon');
-      const suffixIcon = renderIcon(props.suffixIcon, 'suffix-icon');
+      const prefix = renderTNodeJSX('prefix-icon');
+      const suffix = renderTNodeJSX('suffix-icon');
 
       return (
         <a
@@ -56,9 +40,9 @@ export default defineComponent({
           target={props.target}
           onClick={handleClick}
         >
-          {prefixIcon ? <span class={`${COMPONENT_NAME.value}__prefix-icon`}>{prefixIcon}</span> : null}
+          {prefix ? <span class={`${COMPONENT_NAME.value}__prefix-icon`}>{prefix}</span> : null}
           {linkContent}
-          {suffixIcon ? <span class={`${COMPONENT_NAME.value}__suffix-icon`}>{suffixIcon}</span> : null}
+          {suffix ? <span class={`${COMPONENT_NAME.value}__suffix-icon`}>{suffix}</span> : null}
         </a>
       );
     };
