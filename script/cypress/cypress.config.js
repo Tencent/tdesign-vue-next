df111285a94b0cfafc1e2566757b91673c8ee7c9
pengYYY <pengyue970715@gmail.com>
2022-08-28 17:56:59 +0800

    feat: cypress config

---
+++ script/cypress/cypress.config.js
@@ -0,0 +1,41 @@
+import { defineConfig } from 'cypress';
+import viteConfig from '../../site/vite.config';
+
+export default defineConfig({
+  fileServerFolder: 'script/cypress',
+  fixturesFolder: 'script/cypress/fixtures',
+  screenshotsFolder: 'script/cypress/screenshots',
+  videosFolder: 'script/cypress/videos',
+  video: true,
+  videoCompression: 15,
+  viewportWidth: 1024,
+  viewportHeight: 600,
+  reporter: 'junit',
+  reporterOptions: {
+    mochaFile: 'results/cypress-output-[hash].xml',
+    toConsole: true,
+  },
+  component: {
+    // specPattern: '**/*.cy.{js,mjs,cjs,ts,mts,cts,jsx,tsx}',
+    supportFile: 'script/cypress/support/component.js',
+    indexHtmlFile: 'script/cypress/support/component-index.html',
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    devServer: {
+      framework: 'vue',
+      bundler: 'vite',
+      viteConfig: {
+        ...viteConfig(''),
+        server: {},
+      },
+    },
+  },
+
+  e2e: {
+    supportFile: false,
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
