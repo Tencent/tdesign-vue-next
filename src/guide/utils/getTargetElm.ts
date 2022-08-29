9424f73e7bdf768da7f1259576b5fc471b230cf0
zhangpaopao <zhangpaopao0609@gmail.com>
2022-08-29 21:28:52 +0800

    chore(guide): type

--- src/guide/utils/getTargetElm.ts
+++ src/guide/utils/getTargetElm.ts
@@ -1,12 +1,12 @@
-type fn = () => HTMLElement;
+import { AttachNode } from '../../common';
 
-export default function getTargetElm(elm: string | fn): HTMLElement {
+export default function getTargetElm(elm: AttachNode): HTMLElement {
   if (elm) {
     let targetElement: HTMLElement = null;
     if (typeof elm === 'string') {
       targetElement = document.querySelector(elm);
     } else if (typeof elm === 'function') {
-      targetElement = elm();
+      targetElement = elm() as HTMLElement;
     } else {
       throw new Error('elm should be string or function');
     }
