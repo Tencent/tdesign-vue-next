4ec1b1e143c9a3da03aa567bf2338597205f9838
azhe <xiaosansiji@gmail.com>
2022-08-31 14:01:01 +0800

    Feat/dir refactor (#1547)
    * feat: demo path modify
    
    * fix: demo import error
    
    * feat: git mv files
    
    * feat: remove useless doc
    
    * fix: ssr test demo path
    
    * test: update snapshot
    
    * fix: vue lint fix
    
    * chore: remove mv.js
    
    Co-authored-by: pengYYY <pengyue970715@gmail.com>

---
+++ src/transfer/_example/tree.vue
@@ -0,0 +1,86 @@
+<template>
+  <div>
+    <t-transfer
+      v-model="targetValue"
+      v-model:checked="checked"
+      :data="items"
+      @change="onChange"
+      @checked-change="handleCheckedChange"
+    >
+      <template #tree="slotProps">
+        <t-tree v-bind="slotProps" checkable hover expand-all transition />
+      </template>
+    </t-transfer>
+  </div>
+</template>
+<script setup>
+import { ref } from 'vue';
+
+const items = [
+  {
+    value: '1',
+    label: '1',
+    children: [
+      {
+        value: '1.1',
+        label: '1.1',
+      },
+      {
+        value: '1.2',
+        label: '1.2',
+        children: [
+          {
+            value: '1.2.1',
+            label: '1.2.1',
+            children: [
+              {
+                value: '1.2.1.1',
+                label: '1.2.1.1',
+              },
+              {
+                value: '1.2.1.2',
+                label: '1.2.1.2',
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    value: '2',
+    label: '2',
+    children: [
+      {
+        value: '2.1',
+        label: '2.1',
+      },
+      {
+        value: '2.2',
+        label: '2.2',
+      },
+    ],
+  },
+  {
+    value: '3',
+    label: '3',
+    children: [],
+  },
+];
+
+const targetValue = ref([]);
+const checked = ref([]);
+
+const handleCheckedChange = ({ checked, sourceChecked, targetChecked, type }) => {
+  console.log('handleCheckedChange', {
+    checked,
+    sourceChecked,
+    targetChecked,
+    type,
+  });
+};
+
+const onChange = (newTargetValue) => {
+  console.log('onChange', newTargetValue);
+};
+</script>
