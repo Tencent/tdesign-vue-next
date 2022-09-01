75e757d71204249a80306afe2255af0c921872c4
zhangpaopao <zhangpaopao0609@gmail.com>
2022-09-01 19:36:24 +0800

    ci(loading): update snap and spell error

--- src/loading/_example/directive.vue
+++ src/loading/_example/directive.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="loading-directive-demo">
     <div v-loading="loading1" class="loading-container">Loading...</div>
-    <t-button size="small" :disabled="loading1" @click="handelLoading">支持指令</t-button>
+    <t-button size="small" :disabled="loading1" @click="handleLoading">支持指令</t-button>
 
     <div v-loading.fullscreen="loading2" class="loading-container">Loading...</div>
-    <t-button size="small" :disabled="loading2" @click="handelLoadingFullscreen">支持 fullscreen 修饰符</t-button>
+    <t-button size="small" :disabled="loading2" @click="handleLoadingFullscreen">支持 fullscreen 修饰符</t-button>
   </div>
 </template>
 <script setup>
@@ -13,7 +13,7 @@ import { ref } from 'vue';
 const loading1 = ref(false);
 const loading2 = ref(false);
 
-const handelLoading = () => {
+const handleLoading = () => {
   loading1.value = true;
   const timer = setTimeout(() => {
     loading1.value = false;
@@ -21,7 +21,7 @@ const handelLoading = () => {
   }, 1000);
 };
 
-const handelLoadingFullscreen = () => {
+const handleLoadingFullscreen = () => {
   loading2.value = true;
   const timer = setTimeout(() => {
     loading2.value = false;
