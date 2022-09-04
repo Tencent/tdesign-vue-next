f30dce72960c2bb617fc12fbecbd94b4327bbc51
sinbadmaster <40019023+sinbadmaster@users.noreply.github.com>
2022-09-04 15:15:11 +0800

    feat(image-viewer): 新增image-viewer组件 (#1516)
    * feat: rebase
    
    * feat(image-viewer): 新增image-viewer
    
    closed #1402
    
    * fix(image-viewer): 双指缩放异常
    
    closed #1402
    
    * fix(image-viewer): ts检测异常
    
    closed #1402
    
    * fix(image-viewer): props类型检测
    
    closed #1402
    
    * fix(image-viewer): 阻止双指缩放默认行为
    
    closed #1402
    
    * fix(image-viewer): 箭头函数及props检测
    
    closed #1402

---
+++ src/image-viewer/utils.ts
@@ -0,0 +1,56 @@
+import { TdImageViewerProps, ImageInfo } from './type';
+
+export const downloadFile = function (imgSrc: string) {
+  const image = new Image();
+  const name = imgSrc?.split?.('/').pop() || Math.random().toString(32).slice(2);
+
+  image.setAttribute('crossOrigin', 'anonymous');
+
+  image.onload = () => {
+    const canvas = document.createElement('canvas');
+    canvas.width = image.width;
+    canvas.height = image.height;
+
+    const context = canvas.getContext('2d');
+    context.drawImage(image, 0, 0, image.width, image.height);
+    canvas.toBlob((blob) => {
+      const url = URL.createObjectURL(blob);
+      const a = document.createElement('a');
+      a.download = name;
+      a.href = url;
+      a.click();
+      a.remove();
+      URL.revokeObjectURL(url);
+    });
+  };
+  image.src = imgSrc;
+};
+
+const isImageInfo = (image: string | ImageInfo): image is ImageInfo => {
+  return typeof image !== 'string';
+};
+
+export const formatImages = (images: TdImageViewerProps['images']): ImageInfo[] => {
+  if (!Array.isArray(images)) return [];
+  return images.map((item) => {
+    if (isImageInfo(item)) {
+      return {
+        download: true,
+        thumbnail: item.mainImage,
+        ...item,
+      };
+    }
+    return {
+      mainImage: item,
+      thumbnail: item,
+      download: true,
+    };
+  });
+};
+
+export const getOverlay = (props: TdImageViewerProps) => {
+  if (props.showOverlay !== undefined) {
+    return props.showOverlay;
+  }
+  return props.mode === 'modal';
+};
