ed1df3c2061c96466a53aed47d2e25b988472045
_lmmmmmm <lmmmmmm12138@gmail.com>
2022-09-04 11:15:22 +0800

    docs(tabs): tabs demo space

--- src/tabs/_example/position.vue
+++ src/tabs/_example/position.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="t-demo-tabs">
-    <div class="t-demo-tabs__desc">
+  <t-space direction="vertical">
+    <t-space>
       <t-radio-group v-model="placement" variant="default-filled">
-        <t-radio-button value="top"> top </t-radio-button>
-        <t-radio-button value="right"> right </t-radio-button>
-        <t-radio-button value="bottom"> bottom </t-radio-button>
-        <t-radio-button value="left"> left </t-radio-button>
+        <t-radio-button value="top">top</t-radio-button>
+        <t-radio-button value="right">right</t-radio-button>
+        <t-radio-button value="bottom">bottom</t-radio-button>
+        <t-radio-button value="left">left</t-radio-button>
       </t-radio-group>
-    </div>
+    </t-space>
     <t-tabs :value="value" :placement="placement" @change="handlerChange">
       <t-tab-panel value="first" label="选项卡1">
         <p style="padding: 25px">选项卡1</p>
@@ -19,7 +19,7 @@
         <p style="padding: 25px">选项卡3</p>
       </t-tab-panel>
     </t-tabs>
-  </div>
+  </t-space>
 </template>
 
 <script setup>
@@ -32,25 +32,3 @@ const handlerChange = (newValue) => {
   value.value = newValue;
 };
 </script>
-
-<style lang="less">
-.t-demo-tabs {
-  &__desc {
-    margin-bottom: 20px;
-    color: #333;
-    font-size: 14px;
-
-    &:not(:first-of-type) {
-      margin-top: 20px;
-    }
-
-    p {
-      margin-bottom: 20px;
-    }
-  }
-
-  &__btn {
-    margin-right: 20px;
-  }
-}
-</style>
