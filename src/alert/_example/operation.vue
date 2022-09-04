5b407870e9a8377d6ded559503d904d4bcd2361d
_lmmmmmm <lmmmmmm12138@gmail.com>
2022-09-04 15:29:06 +0800

    docs(alert): alert demo space

--- src/alert/_example/operation.vue
+++ src/alert/_example/operation.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="tdesign-demo-block">
+  <t-space style="width: 100%">
     <t-alert theme="info" message="这是一条普通的消息提示" close>
       <template #operation>
         <span @click="handleOperation">相关操作</span>
       </template>
     </t-alert>
-  </div>
+  </t-space>
 </template>
 <script setup>
 const handleOperation = () => {
