797ccce55e419dc29acacd8afb4c62699facc441
luopei <285258675@qq.com>
2022-09-29 18:27:52 +0800

    test(unit): checkbox: add checkbox test (#1781)
    Co-authored-by: 骆沛 <luopei@11.com>

--- src/checkbox/__tests__/index.test.jsx
+++ src/checkbox/__tests__/index.test.jsx
@@ -1,141 +1,181 @@
 import { mount } from '@vue/test-utils';
+import { describe, expect, it, vi } from 'vitest';
+import { ref } from 'vue';
 import Checkbox, { CheckboxGroup } from '@/src/checkbox/index.ts';
 
-// every component needs four parts: props/events/slots/functions.
 describe('Checkbox', () => {
-  // test props api
   describe(':props', () => {
     it(':checked', () => {
-      const wrapper = mount({
-        render() {
-          return <Checkbox checked={true}></Checkbox>;
-        },
-      });
-      expect(wrapper.element).toMatchSnapshot();
+      const checked = ref(true);
+      const wrapper = mount(() => <Checkbox v-model={checked.value} />);
+      const checkbox = wrapper.find('.t-checkbox');
+      expect(checkbox.classes()).toContain('t-is-checked');
     });
+
     it(':defaultChecked', () => {
-      const wrapper = mount({
-        render() {
-          return <Checkbox defaultChecked={true}></Checkbox>;
-        },
-      });
-      expect(wrapper.element).toMatchSnapshot();
+      const checked = ref(true);
+      const wrapper = mount(() => <Checkbox defaultChecked={checked.value} />);
+      const checkbox = wrapper.find('.t-checkbox');
+      expect(checkbox.classes()).toContain('t-is-checked');
     });
+
+    it(':default', () => {
+      const checked = ref(true);
+      const wrapper = mount(() => <Checkbox defaultChecked={checked.value} default="checkbox" />);
+      const label = wrapper.find('.t-checkbox__label');
+      expect(label.exists()).toBeTruthy();
+      expect(label.text()).toBe('checkbox');
+    });
+
     it(':disabled', () => {
-      const wrapper = mount({
-        render() {
-          return <Checkbox disabled={true}></Checkbox>;
-        },
-      });
-      expect(wrapper.element).toMatchSnapshot();
+      const wrapper = mount(() => <Checkbox disabled default="checkbox" />);
+      const checkbox = wrapper.find('.t-checkbox');
+      expect(checkbox.classes()).toContain('t-is-disabled');
     });
+
     it(':indeterminate', () => {
-      const wrapper = mount({
-        render() {
-          return <Checkbox indeterminate={true}></Checkbox>;
-        },
-      });
-      expect(wrapper.element).toMatchSnapshot();
+      const wrapper = mount(() => <Checkbox indeterminate default="checkbox" />);
+      const checkbox = wrapper.find('.t-checkbox');
+      expect(checkbox.classes()).toContain('t-is-indeterminate');
     });
+
+    it(':label', () => {
+      const wrapper = mount(() => <Checkbox label="label" />);
+      const label = wrapper.find('.t-checkbox__label');
+      expect(label.exists()).toBeTruthy();
+      expect(label.text()).toBe('label');
+    });
+
     it(':name', () => {
-      const wrapper = mount({
-        render() {
-          return <Checkbox name={'checkbox-name'}></Checkbox>;
-        },
-      });
-      expect(wrapper.element).toMatchSnapshot();
+      const wrapper = mount(() => <Checkbox name="name" />);
+      const input = wrapper.find('.t-checkbox input');
+      expect(input.element.getAttribute('name')).toBe('name');
+    });
+  });
+  describe(': events', () => {
+    it(':onChange', async () => {
+      const checked = ref(true);
+      const fn = vi.fn();
+      const wrapper = mount(() => <Checkbox v-model={checked.value} label="label" onChange={fn} />);
+      const input = wrapper.find('input');
+      await input.trigger('change');
+      expect(fn).toBeCalled();
+      expect(checked.value).toBeFalsy();
     });
   });
 });
 
-describe('Checkbox CheckboxGroup', () => {
-  // test props api
+describe('CheckboxGroup', () => {
   describe(':props', () => {
-    it(':defaultChecked', () => {
-      const wrapper = mount({
-        render() {
-          return (
-            <CheckboxGroup defaultChecked={['sz']}>
-              <Checkbox value="gz">广州</Checkbox>
-              <Checkbox value="sz" disabled>
-                深圳
-              </Checkbox>
-            </CheckboxGroup>
-          );
-        },
-      });
-      expect(wrapper.element).toMatchSnapshot();
+    it('', () => {
+      const wrapper = mount(() => (
+        <CheckboxGroup>
+          <Checkbox value="1">选项一</Checkbox>
+          <Checkbox value="2">选项二</Checkbox>
+        </CheckboxGroup>
+      ));
+      const checkboxs = wrapper.findAll('.t-checkbox');
+      expect(checkboxs.length).toBe(2);
     });
-    it(':checked', () => {
-      const wrapper = mount({
-        render() {
-          return (
-            <CheckboxGroup checked={['sz']}>
-              <Checkbox value="gz">广州</Checkbox>
-              <Checkbox value="sz" disabled>
-                深圳
-              </Checkbox>
-            </CheckboxGroup>
-          );
-        },
-      });
-      expect(wrapper.element).toMatchSnapshot();
-    });
+
     it(':disabled', () => {
-      const wrapper = mount({
-        render() {
-          return (
-            <CheckboxGroup disabled={true}>
-              <Checkbox value="bj">北京</Checkbox>
-              <Checkbox value="gz">广州</Checkbox>
-              <Checkbox value="sz" disabled={false}>
-                深圳
-              </Checkbox>
-            </CheckboxGroup>
-          );
-        },
-      });
-      expect(wrapper.element).toMatchSnapshot();
+      const wrapper = mount(() => (
+        <CheckboxGroup disabled>
+          <Checkbox value="1">选项一</Checkbox>
+          <Checkbox value="2">选项二</Checkbox>
+        </CheckboxGroup>
+      ));
+      const checkboxs = wrapper.findAll('.t-checkbox');
+      expect(checkboxs[0].classes()).toContain('t-is-disabled');
+      expect(checkboxs[1].classes()).toContain('t-is-disabled');
     });
-    it(':max', () => {
-      const options = [
-        { value: 'bj', label: '北京' },
-        { value: 'gz', label: '广州' },
-        { value: 'sz', label: '深圳' },
-      ];
-      const wrapper = mount({
-        render() {
-          return <CheckboxGroup options={options} value={['sz', 'gz']} max={2} />;
-        },
-      });
-      expect(wrapper.element).toMatchSnapshot();
-    });
-    it(':options', () => {
-      const options = [
-        { value: 'gz', label: '广州', disabled: true },
-        { value: 'sz', label: '深圳' },
-      ];
-      const wrapper = mount({
-        render() {
-          return <CheckboxGroup options={options}></CheckboxGroup>;
-        },
-      });
-      expect(wrapper.element).toMatchSnapshot();
-    });
+
     it(':name', () => {
-      const wrapper = mount({
-        render() {
-          return (
-            <CheckboxGroup name={'checkbox-name'}>
-              <Checkbox value="gz">广州</Checkbox>
-              <Checkbox value="sz" disabled>
-                深圳
-              </Checkbox>
-            </CheckboxGroup>
-          );
+      const wrapper = mount(() => (
+        <CheckboxGroup name="name">
+          <Checkbox value="1">选项一</Checkbox>
+          <Checkbox value="2">选项二</Checkbox>
+        </CheckboxGroup>
+      ));
+      const checkboxs = wrapper.findAll('.t-checkbox input');
+      expect(checkboxs[0].element.getAttribute('name')).toBe('name');
+      expect(checkboxs[1].element.getAttribute('name')).toBe('name');
+    });
+
+    it(':value', () => {
+      const checked = ref(['1']);
+      const wrapper = mount(() => (
+        <CheckboxGroup v-model={checked.value}>
+          <Checkbox value="1">选项一</Checkbox>
+          <Checkbox value="2">选项二</Checkbox>
+        </CheckboxGroup>
+      ));
+      const checkboxs = wrapper.findAll('.t-checkbox');
+      expect(checkboxs[0].classes()).toContain('t-is-checked');
+      expect(checkboxs[1].classes()).not.toContain('t-is-checked');
+    });
+
+    it(':value', () => {
+      const defaultValue = ref(['1']);
+      const wrapper = mount(() => (
+        <CheckboxGroup defaultValue={defaultValue.value}>
+          <Checkbox value="1">选项一</Checkbox>
+          <Checkbox value="2">选项二</Checkbox>
+        </CheckboxGroup>
+      ));
+      const checkboxs = wrapper.findAll('.t-checkbox');
+      expect(checkboxs[0].classes()).toContain('t-is-checked');
+      expect(checkboxs[1].classes()).not.toContain('t-is-checked');
+    });
+
+    it(':options', () => {
+      const defaultValue = ref(['1']);
+      const options = [
+        {
+          label: '选项一',
+          value: '1',
         },
-      });
-      expect(wrapper.element).toMatchSnapshot();
+        {
+          label: '选项二',
+          value: '2',
+        },
+      ];
+      const wrapper = mount(() => <CheckboxGroup defaultValue={defaultValue.value} options={options}></CheckboxGroup>);
+      const checkboxs = wrapper.findAll('.t-checkbox');
+      expect(checkboxs.length).toBe(2);
+      expect(checkboxs[0].classes()).toContain('t-is-checked');
+      expect(checkboxs[1].classes()).not.toContain('t-is-checked');
+    });
+  });
+
+  describe(':events', () => {
+    it(':checkAll', async () => {
+      const checked = ref([]);
+      const wrapper = mount(() => (
+        <CheckboxGroup v-model={checked.value}>
+          <Checkbox checkAll>全选</Checkbox>
+          <Checkbox value="1">选项一</Checkbox>
+          <Checkbox value="2">选项二</Checkbox>
+        </CheckboxGroup>
+      ));
+      const checkboxs = wrapper.findAll('.t-checkbox input');
+      await checkboxs[0].trigger('change');
+      expect(checked.value).toEqual(['1', '2']);
+    });
+
+    it(':onChange', async () => {
+      const checked = ref(['1']);
+      const fn = vi.fn();
+      const wrapper = mount(() => (
+        <CheckboxGroup v-model={checked.value} onChange={fn}>
+          <Checkbox value="1">选项一</Checkbox>
+          <Checkbox value="2">选项二</Checkbox>
+        </CheckboxGroup>
+      ));
+      const checkboxs = wrapper.findAll('.t-checkbox input');
+      await checkboxs[1].trigger('change');
+      expect(fn).toBeCalled();
+      expect(checked.value).toEqual(['1', '2']);
     });
   });
 });
