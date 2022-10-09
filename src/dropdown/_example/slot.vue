ae2133d9cbe2daf9ed3e83a07eaf2c31909775ed
Uyarn <uyarnchen@gmail.com>
2022-10-09 17:24:51 +0800

    feat(Dropdown): new dropdown structure

--- src/dropdown/_example/slot.vue
+++ src/dropdown/_example/slot.vue
@@ -1,13 +1,38 @@
 <template>
   <t-space>
-    <t-dropdown :min-column-width="88" trigger="click" @click="handleClick">
+    <t-dropdown :min-column-width="88" @click="handleClick">
       <t-button variant="text">下拉菜单</t-button>
-      <template #dropdown>
-        <t-dropdown-menu>
-          <t-dropdown-item :value="1">操作一</t-dropdown-item>
-          <t-dropdown-item :value="2">操作二</t-dropdown-item>
-        </t-dropdown-menu>
-      </template>
+      <t-dropdown-menu>
+        <t-dropdown-item :value="1">
+          操作一
+          <t-dropdown-menu>
+            <t-dropdown-item :value="11">操作1-1</t-dropdown-item>
+            <t-dropdown-item :value="12">
+              操作1-2
+              <t-dropdown-menu>
+                <t-dropdown-item :value="111">操作1-1-1</t-dropdown-item>
+                <t-dropdown-item :value="112">操作1-1-2</t-dropdown-item>
+              </t-dropdown-menu>
+            </t-dropdown-item>
+          </t-dropdown-menu>
+        </t-dropdown-item>
+
+        <t-dropdown-item :value="2">
+          操作二
+          <t-dropdown-menu>
+            <t-dropdown-item :value="21">
+              操作2-1
+              <t-dropdown-menu>
+                <t-dropdown-item :value="211">操作2-1-1</t-dropdown-item>
+                <t-dropdown-item :value="211">操作2-1-2</t-dropdown-item>
+              </t-dropdown-menu>
+            </t-dropdown-item>
+            <t-dropdown-item :value="22" @click="clickHandler"> 操作2-2 </t-dropdown-item>
+          </t-dropdown-menu>
+        </t-dropdown-item>
+
+        <t-dropdown-item :value="3">操作三</t-dropdown-item>
+      </t-dropdown-menu>
     </t-dropdown>
   </t-space>
 </template>
---
+++ src/dropdown/hooks/useDropdownOptions.ts
@@ -0,0 +1,44 @@
+import { computed, ComputedRef, VNode } from 'vue';
+import { DropdownOption, TdDropdownProps } from '../type';
+import { useChildComponentSlots } from '../../hooks/slot';
+
+export const getOptionsFromChildren = (menuGroup: any): DropdownOption[] => {
+  if (!menuGroup) return [];
+
+  // 处理内部嵌套场景
+  if (menuGroup[0].type?.name === 'TDropdownMenu') {
+    const groupChildren = menuGroup[0].children.default?.();
+    if (Array.isArray(groupChildren)) {
+      return getOptionsFromChildren(groupChildren);
+    }
+  }
+
+  if (Array.isArray(menuGroup)) {
+    return menuGroup.map((item: any) => {
+      const groupChildren = item.children.default();
+
+      const contentIdx = groupChildren.findIndex((v: VNode) => typeof v.children === 'string');
+      const childrenContent = groupChildren.filter((v: VNode) => typeof v.children !== 'string');
+      return {
+        ...item.props,
+        content: groupChildren[contentIdx],
+        children: childrenContent.length > 0 ? getOptionsFromChildren(childrenContent) : null,
+      };
+    });
+  }
+
+  return [];
+};
+
+export default function useDropdownOptions(props: TdDropdownProps): ComputedRef<DropdownOption[]> {
+  const getChildComponentSlots = useChildComponentSlots();
+
+  const menuSlot = (getChildComponentSlots('DropdownMenu')?.[0] as any).children.default?.();
+
+  const dropdownOptions = computed(() => {
+    if (props.options && props.options.length > 0) return props.options;
+
+    return getOptionsFromChildren(menuSlot);
+  });
+  return dropdownOptions;
+}
