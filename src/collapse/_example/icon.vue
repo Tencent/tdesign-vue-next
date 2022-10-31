03779b4788cbc5cd04fb8981becd12ab0a315ca9
zihao <497244062@qq.com>
2022-10-31 21:45:19 +0800

    docs(collapse): modify demo
    modify demo

--- src/collapse/_example/icon.vue
+++ src/collapse/_example/icon.vue
@@ -11,10 +11,11 @@
         >
       </t-collapse-panel>
       <t-collapse-panel value="2">
-        <template #header>这是一个折叠标题</template>
+        <template #expandIcon><StarIcon /></template>
+        <template #header>这是一个折叠标题2</template>
         这部分是每个折叠面板折叠或展开的内容，可根据不同业务或用户的使用诉求，进行自定义填充。可以是纯文本、图文、子列表等内容形式。
       </t-collapse-panel>
-      <t-collapse-panel value="3" header="这是一个折叠标题">
+      <t-collapse-panel :expand-icon="renderStarIcon" value="3" header="这是一个折叠标题">
         这部分是每个折叠面板折叠或展开的内容，可根据不同业务或用户的使用诉求，进行自定义填充。可以是纯文本、图文、子列表等内容形式。
       </t-collapse-panel>
     </t-collapse>
@@ -31,10 +32,15 @@
   </t-space>
 </template>
 
-<script setup>
+<script setup lang="jsx">
 import { ref } from 'vue';
+import { StarIcon, LogoGithubIcon } from 'tdesign-icons-vue-next';
 
 const showArrow = ref(true);
 const direction = ref('left');
 const onlyIcon = ref(false);
+
+const renderStarIcon = () => {
+  return <LogoGithubIcon />;
+};
 </script>
