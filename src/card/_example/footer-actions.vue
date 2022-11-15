42aad10ff14db63c62ef754f519eb99e4d27de4b
wenkang <manhongchung555@gmail.com>
2022-11-15 14:41:41 +0800

    chore: optimize demo

--- src/card/_example/footer-actions.vue
+++ src/card/_example/footer-actions.vue
@@ -2,26 +2,22 @@
   <div>
     <t-card :cover="cover" theme="poster2" :style="{ width: '400px' }">
       <template #footer>
-        <t-row :align="'middle'" justify="center">
-          <t-col flex="auto">
-            <t-button block variant="text" shape="square">
-              <thumb-up-icon size="24" />
+        <t-row :align="'middle'" justify="center" style="gap: 24px">
+          <t-col flex="auto" style="display: inline-flex; justify-content: center">
+            <t-button variant="text" shape="square">
+              <thumb-up-icon />
             </t-button>
           </t-col>
-          <t-col flex="none">
-            <t-divider layout="vertical" />
-          </t-col>
-          <t-col flex="auto">
-            <t-button block variant="text" shape="square">
-              <chat-icon size="24" />
+
+          <t-col flex="auto" style="display: inline-flex; justify-content: center">
+            <t-button variant="text" shape="square">
+              <chat-icon />
             </t-button>
           </t-col>
-          <t-col flex="none">
-            <t-divider layout="vertical" />
-          </t-col>
-          <t-col flex="auto">
-            <t-button block variant="text" shape="square">
-              <share-icon size="24" />
+
+          <t-col flex="auto" style="display: inline-flex; justify-content: center">
+            <t-button variant="text" shape="square">
+              <share-icon />
             </t-button>
           </t-col>
         </t-row>
@@ -31,16 +27,19 @@
     <t-card :cover="cover" theme="poster2" :style="{ width: '400px' }">
       <template #footer>
         <t-button variant="text" shape="square" :style="{ 'margin-right': '8px' }">
-          <heart-icon size="16" />
+          <heart-icon />
+        </t-button>
+        <t-button variant="text" shape="square" :style="{ 'margin-right': '8px' }">
+          <chat-icon />
         </t-button>
         <t-button variant="text" shape="square">
-          <share-icon size="16" />
+          <share-icon />
         </t-button>
       </template>
       <template #actions>
         <t-dropdown :options="options" :min-column-width="112" @click="clickHandler">
           <t-button variant="text" shape="square">
-            <more-icon size="24" />
+            <more-icon />
           </t-button>
         </t-dropdown>
       </template>
@@ -59,7 +58,7 @@
       <template #actions>
         <t-dropdown :options="options" :min-column-width="112" @click="clickHandler">
           <t-button variant="text" shape="square">
-            <more-icon size="24" />
+            <more-icon />
           </t-button>
         </t-dropdown>
       </template>
--- src/card/_example/header-footer-actions.vue
+++ src/card/_example/header-footer-actions.vue
@@ -1,46 +1,32 @@
 <template>
   <t-card :cover="cover" title="标题" description="卡片内容" :style="{ width: '400px' }">
     <template #avatar>
-      <t-avatar image="https://tdesign.gtimg.com/site/avatar-boy.jpg" size="40px"></t-avatar>
+      <t-avatar image="https://tdesign.gtimg.com/site/avatar-boy.jpg" size="56px"></t-avatar>
     </template>
     <template #actions>
       <t-dropdown :options="options" :min-column-width="112" @click="clickHandler">
         <div class="tdesign-demo-dropdown-trigger">
           <t-button variant="text" shape="square">
-            <more-icon size="24" />
+            <more-icon />
           </t-button>
         </div>
       </t-dropdown>
     </template>
     <template #footer>
-      <t-row :align="'middle'" justify="center">
-        <t-col flex="auto">
-          <t-button block variant="text">
-            <thumb-up-icon size="24" />
-          </t-button>
-        </t-col>
-        <t-col flex="none">
-          <t-divider layout="vertical" />
-        </t-col>
-        <t-col flex="auto">
-          <t-button block variant="text">
-            <chat-icon size="24" />
-          </t-button>
-        </t-col>
-        <t-col flex="none">
-          <t-divider layout="vertical" />
-        </t-col>
-        <t-col flex="auto">
-          <t-button block variant="text">
-            <share-icon size="24" />
-          </t-button>
-        </t-col>
-      </t-row>
+      <t-button variant="text" shape="square" :style="{ 'margin-right': '8px' }">
+        <heart-icon />
+      </t-button>
+      <t-button variant="text" shape="square" :style="{ 'margin-right': '8px' }">
+        <chat-icon />
+      </t-button>
+      <t-button variant="text" shape="square">
+        <share-icon />
+      </t-button>
     </template>
   </t-card>
 </template>
 <script setup lang="jsx">
-import { ThumbUpIcon, ChatIcon, ShareIcon, MoreIcon } from 'tdesign-icons-vue-next';
+import { HeartIcon, ChatIcon, ShareIcon, MoreIcon } from 'tdesign-icons-vue-next';
 import { MessagePlugin } from 'tdesign-vue-next';
 
 const cover = 'https://tdesign.gtimg.com/site/source/card-demo.png';
