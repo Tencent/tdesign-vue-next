9615513680aa78eb78c52ab55e19b2ecfcc7a5e9
chaishi <974383157@qq.com>
2022-11-20 21:30:11 +0800

    docs(auto-complete): add search-icon demo to base.vue

--- src/auto-complete/_example/base.vue
+++ src/auto-complete/_example/base.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <t-space direction="vertical" class="t-demo-auto-complete__base">
     <t-auto-complete
       v-model="value"
       :options="options"
@@ -7,13 +7,27 @@
       placeholder="请输入关键词搜索"
       @change="onChange"
     />
-  </div>
+
+    <t-auto-complete
+      v-model="value2"
+      :options="options"
+      placeholder="请输入关键词搜索（右侧搜索按钮可以使用插槽自定义）"
+      highlight-keyword
+      filterable
+    >
+      <template #suffix>
+        <t-button shape="square"><SearchIcon /></t-button>
+      </template>
+    </t-auto-complete>
+  </t-space>
 </template>
 
 <script setup>
 import { ref } from 'vue';
+import { SearchIcon } from 'tdesign-icons-vue-next';
 
 const value = ref('');
+const value2 = ref('');
 const options = ref(['第一个默认联想词', '第二个默认联想词', '第三个默认联想词']);
 const timer = ref(null);
 
@@ -29,3 +43,12 @@ function onChange(value) {
   }, 200);
 }
 </script>
+
+<style>
+.t-demo-auto-complete__base .t-input {
+  padding-right: 0;
+}
+.t-demo-auto-complete__base .t-button svg {
+  font-size: 20px;
+}
+</style>
