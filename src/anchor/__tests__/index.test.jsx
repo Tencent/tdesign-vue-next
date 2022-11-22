62f8ffd499a031630c9cc32865eea3485383dc8f
pengYYY <pengyue970715@gmail.com>
2022-11-22 17:18:32 +0800

    test: add anchor auto-complete select unit

--- src/anchor/__tests__/index.test.jsx
+++ src/anchor/__tests__/index.test.jsx
@@ -1,10 +1,41 @@
+import { mount } from '@vue/test-utils';
+import { Anchor, AnchorItem, AnchorTarget } from '@/src/anchor/index.ts';
+
 describe('Anchor', () => {
-  describe(':props', () => {
-    test('_______', () => {
-      expect(true).toEqual(true);
+  describe(':base', () => {
+    it('render', () => {
+      const wrapper = mount({
+        render() {
+          return (
+            <Anchor>
+              <AnchorItem href="#基础锚点" title="基础锚点"></AnchorItem>
+              <AnchorItem href="#多级锚点" title="多级锚点"></AnchorItem>
+              <AnchorItem href="#指定容器锚点" title="指定容器锚点"></AnchorItem>
+            </Anchor>
+          );
+        },
+      });
+      expect(wrapper.find('.t-anchor').exists()).toBeTruthy();
+      const AnchorItemList = wrapper.findAllComponents(AnchorItem);
+      expect(AnchorItemList.length).toBe(3);
+    });
+    it('render', () => {
+      const wrapper = mount({
+        render() {
+          return (
+            <Anchor>
+              <AnchorTarget></AnchorTarget>
+              <AnchorTarget></AnchorTarget>
+              <AnchorTarget></AnchorTarget>
+            </Anchor>
+          );
+        },
+      });
+      expect(wrapper.find('.t-anchor').exists()).toBeTruthy();
+      const AnchorTargetList = wrapper.findAllComponents(AnchorTarget);
+      expect(AnchorTargetList.length).toBe(3);
     });
   });
-
   describe('@event', () => {
     test('_______', () => {
       expect(true).toEqual(true);
