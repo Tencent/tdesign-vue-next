5a9ace22feab5418461f2dfb8b96db4cf3f959f2
sheepluo <yaoyanhuoyi@qq.com>
2022-11-30 14:05:33 +0800

    fix(tag-input): some bugs (#2087)
    * fix(tag-input): some bugs
    
    * fix(tag-input): onRemove new Value

--- src/tag-input/_example/excess.vue
+++ src/tag-input/_example/excess.vue
@@ -1,7 +1,7 @@
 <template>
   <t-space direction="vertical">
     <!-- 标签数量超出时，滚动显示 -->
-    <t-tag-input v-model="tags" label="Scroll: " clearable />
+    <t-tag-input v-model="tags" label="Scroll: " excess-tags-display-type="scroll" clearable />
 
     <!-- 标签数量超出时，换行显示 -->
     <t-tag-input v-model="tags" label="BreakLine: " excess-tags-display-type="break-line" clearable />
