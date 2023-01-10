75444eb9754a655bfa4a26662d37cadcd01e2867
Yuhang <aicmortal@gmail.com>
2023-01-10 14:15:58 +0800

    fix(tooltip): modify the way the triggering element is passed (#2253)
    * fix(tooltip): modify the way the triggering element is passed
    
    * fix(tooltip): modify the way content is passed to use slots

--- src/tooltip/tooltip.tsx
+++ src/tooltip/tooltip.tsx
@@ -65,8 +65,6 @@ export default defineComponent({
       ...(vm?.vnode.props || {}),
       placement: props.placement === 'mouse' ? 'bottom-left' : props.placement,
       showArrow: props.placement === 'mouse' ? false : props.showArrow,
-      content: () => renderTNodeJSX('content'),
-      default: () => renderContent('default', 'triggerElement'),
       overlayClassName: tooltipOverlayClassName.value,
       onVisibleChange: onTipVisibleChange,
       disabled: props.disabled,
@@ -116,7 +114,12 @@ export default defineComponent({
         {...popupProps.value}
         overlayInnerStyle={overlayInnerStyle.value}
         visible={innerVisible.value}
-      />
+        v-slots={{
+          content: () => renderTNodeJSX('content'),
+        }}
+      >
+        {renderContent('default', 'triggerElement')}
+      </Popup>
     );
   },
 });
