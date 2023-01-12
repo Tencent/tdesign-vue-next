a9d80951436ee97197d36bd1ebbac62401a7153e
pengYYY <pengyue970715@gmail.com>
2023-01-12 14:30:14 +0800

    chore: release 1.0.3 and fix type

--- src/config-provider/config-provider.tsx
+++ src/config-provider/config-provider.tsx
@@ -3,11 +3,13 @@ import { GlobalConfigProvider } from './type';
 import { useTNodeJSX } from '../hooks/tnode';
 import { provideConfig } from './useConfig';
 
-const configProviderProps = {
+export const configProviderProps = {
   globalConfig: Object as PropType<GlobalConfigProvider>,
-} as const;
+};
 
-export type ConfigProviderProps = ExtractPropTypes<typeof configProviderProps>;
+export type ConfigProviderProps = {
+  globalConfig: GlobalConfigProvider;
+};
 
 export default defineComponent({
   name: 'TConfigProvider',
