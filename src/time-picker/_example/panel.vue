186f836a25c749beef22fb2212739d0a8e07998c
Uyarn <uyarnchen@gmail.com>
2023-01-17 14:08:53 +0800

    fix(timepicker): register panel component

--- src/time-picker/_example/panel.vue
+++ src/time-picker/_example/panel.vue
@@ -1,9 +1,8 @@
 <template>
-  <time-picker-panel :value="value" @change="handleChange" />
+  <t-time-picker-panel :value="value" @change="handleChange" />
 </template>
 <script setup>
 import { ref } from 'vue';
-import { TimePickerPanel } from 'tdesign-vue-next';
 
 const value = ref('11:11:11');
 
