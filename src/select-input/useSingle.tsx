b9ff190f723701a634ddcdd4aca3dc727dcf1e95
yuyang <uyarnchen@gmail.com>
2023-02-20 10:40:41 +0800

    fix(select-input): backspace delete (#2429)

--- src/select-input/useSingle.tsx
+++ src/select-input/useSingle.tsx
@@ -67,9 +67,7 @@ export default function useSingle(props: TdSelectInputProps, context: SetupConte
 
   const renderSelectSingle = (popupVisible: boolean) => {
     const singleValueDisplay = renderTNode('valueDisplay');
-    const pureValue = getInputValue(value.value, keys.value);
-    const displayedValue =
-      popupVisible && props.allowInput ? inputValue.value || pureValue : getInputValue(value.value, keys.value);
+    const displayedValue = popupVisible && props.allowInput ? inputValue.value : getInputValue(value.value, keys.value);
     const prefixContent = [singleValueDisplay, renderTNode('label')];
     const inputProps = {
       ...commonInputProps.value,
