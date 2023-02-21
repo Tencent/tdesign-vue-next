e399f2bdb121f593438774c42163204fb85532b1
yuyang <uyarnchen@gmail.com>
2023-02-21 10:28:29 +0800

    chore: move blur logic into select input (#2435)

--- src/tree-select/tree-select.tsx
+++ src/tree-select/tree-select.tsx
@@ -6,7 +6,7 @@ import isFunction from 'lodash/isFunction';
 import isNil from 'lodash/isNil';
 
 import Tree, { TreeProps, TreeNodeModel, TreeNodeValue } from '../tree';
-import SelectInput, { SelectInputValueChangeContext } from '../select-input';
+import SelectInput from '../select-input';
 import { TagInputChangeContext } from '../tag-input';
 import { InputValue } from '../input';
 import FakeArrow from '../common-components/fake-arrow';
@@ -222,8 +222,7 @@ export default defineComponent({
       changeNodeInfo();
     };
 
-    const inputChange = (value: InputValue, ctx: SelectInputValueChangeContext): boolean => {
-      if (ctx.trigger === 'blur') return;
+    const inputChange = (value: InputValue): boolean => {
       // 未打开状态不处理输入框输入
       if (!innerVisible.value) {
         props.onSearch?.(String(value));
