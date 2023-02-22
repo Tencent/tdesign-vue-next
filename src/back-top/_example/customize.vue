b31afb81c92b4eb05836f549463002d87e7b5196
haoyang.ding <2698300641@qq.com>
2023-02-23 00:48:41 +0800

    feat(back-top): update back-top example

--- src/back-top/_example/customize.vue
+++ src/back-top/_example/customize.vue
@@ -1,12 +1,10 @@
 <template>
   <t-space style="transform: translateZ(0)">
     <div class="container"></div>
-    <t-back-top container=".container" :visible-height="0" :offset="[750, 0]">
-      <template #default>TEXT</template>
-    </t-back-top>
-    <t-back-top :visible-height="0" :offset="[700, 0]">
+    <t-back-top container=".container" :visible-height="0" :offset="[750, 0]"> TEXT </t-back-top>
+    <t-back-top container=".container" :visible-height="0" :offset="[700, 0]">
       <template #content>TEST TEXT</template>
     </t-back-top>
-    <t-back-top container=".container" :visible-height="0" :offset="[650, 0]" content="返回顶部"> </t-back-top>
+    <t-back-top container=".container" :visible-height="0" :offset="[650, 0]"> 返回顶部 </t-back-top>
   </t-space>
 </template>
