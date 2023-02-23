b3c439693bab1c8203a3098fea3049feec7e41b2
Aicmortal <aicmortal@gmail.com>
2023-02-23 13:58:19 +0800

    chore: 优化useTeleport函数

--- src/hooks/useTeleport.ts
+++ src/hooks/useTeleport.ts
@@ -1,25 +1,30 @@
 import { getSSRAttach, getAttach } from '../utils/dom';
 import { computed, Ref, onMounted, ref, watch } from 'vue';
 import { AttachNode } from '../common';
+import isFunction from 'lodash/isFunction';
 
 /**
  * @description 返回挂载的节点, 用于teleport
- * @param attach AttachGetter
- * @param triggerNode triggerNodeRef
+ * @param attach 既可以是一个函数, 也可以是一个ref
+ * @param triggerNode 既可以是一个函数, 也可以是一个ref
  */
-const useTeleport = (attach: () => AttachNode, triggerNode?: Ref<any>): Ref<string | Element> => {
-  const to = computed(attach);
+const useTeleport = (
+  attach: (() => AttachNode) | Ref<AttachNode>,
+  triggerNode?: (() => any) | Ref<any>,
+): Ref<string | Element> => {
+  // 如果是函数, 则使用computed包裹 否则直接使用ref
+  const to = isFunction(attach) ? computed(attach) : ref(attach);
+  const innerTriggerNode = isFunction(triggerNode) ? computed(triggerNode) : ref(triggerNode);
 
   const element = ref<string | Element>();
 
   const getElement = () => {
-    element.value = getSSRAttach() || getAttach(to.value, triggerNode?.value);
+    element.value = getSSRAttach() || getAttach(to.value, innerTriggerNode.value);
   };
 
   onMounted(() => getElement());
 
-  // 监听to变化,重新获取挂载节点; triggerNode可能为undefined, 所以使用Getter
-  watch([to, () => triggerNode?.value], () => getElement());
+  watch([to, innerTriggerNode], () => getElement());
 
   return element;
 };
