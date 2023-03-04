71ad84ee76e4e262686fb25dbad8d5529c8baea1
chaishi <974383157@qq.com>
2023-03-04 20:44:23 +0800

    docs(upload): update custom uploadMethod demo

--- src/upload/_example/img-flow-list.vue
+++ src/upload/_example/img-flow-list.vue
@@ -5,6 +5,7 @@
       <t-switch v-model="autoUpload" />
     </div>
     <br />
+    <!-- :requestMethod="requestMethod1" -->
     <t-upload
       v-model="files"
       action="https://service-bv448zsw-1257786608.gz.apigw.tencentcs.com/api/upload-demo"
@@ -12,6 +13,7 @@
       theme="image-flow"
       accept="image/*"
       multiple
+      :request-method="requestMethod2"
       :auto-upload="autoUpload"
       :max="8"
     ></t-upload>
@@ -21,11 +23,38 @@
 <script setup>
 import { ref } from 'vue';
 
-const autoUpload = ref(false);
+const autoUpload = ref(true);
 const files = ref([
-  // {
-  //   url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
-  //   name: 'loading.svg',
-  // },
+  {
+    url: 'https://tdesign.gtimg.com/demo/demo-image-1.png',
+    name: 'loading.svg',
+  },
 ]);
+
+// 示例代码：自定义上传方法，一个请求上传一个文件
+const requestMethod1 = () => {
+  return new Promise((resolve) => {
+    resolve({
+      status: 'success',
+      response: {
+        url: 'https://tdesign.gtimg.com/site/avatar.jpg',
+      },
+    });
+  });
+};
+
+// 示例代码：自定义上传方法，一个请求上传多个文件
+const requestMethod2 = () => {
+  return new Promise((resolve) => {
+    resolve({
+      status: 'success',
+      response: {
+        files: [
+          { name: 'avatar1.jpg', url: 'https://tdesign.gtimg.com/site/avatar.jpg' },
+          { name: 'avatar2.jpg', url: 'https://avatars.githubusercontent.com/u/11605702?v=4' },
+        ],
+      },
+    });
+  });
+};
 </script>
