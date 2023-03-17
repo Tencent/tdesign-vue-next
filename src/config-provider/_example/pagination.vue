6888057dad22f1418d2551fcffa41f7b6999bebb
antyonygong <745710524@qq.com>
2023-03-17 11:16:28 +0800

    style: 还原示例中分页组件样式

--- src/config-provider/_example/pagination.vue
+++ src/config-provider/_example/pagination.vue
@@ -1,6 +1,8 @@
 <template>
   <t-config-provider :global-config="globalConfig">
-    <t-pagination v-model="current" :total="36" show-jumper :max-page-btn="5" />
+    <div class="tdesign-demo-item--locale-provider-base">
+      <t-pagination v-model="current" :total="36" show-jumper :max-page-btn="5" />
+    </div>
   </t-config-provider>
 </template>
 
@@ -19,7 +21,7 @@ const globalConfig = {
 </script>
 
 <style scoped>
-.tdesign-demo-item--locale-provider-base {
+/* .tdesign-demo-item--locale-provider-base {
   margin: 24px -120px 0 0;
-}
+} */
 </style>
