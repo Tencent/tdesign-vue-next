ffa3ba2caa3d957f86899d96b6122ac2ceb67e45
Nined <nined@protonmail.com>
2023-04-11 11:27:15 +0800

    fix(dropdown): 修复下拉菜单配置template的content或prefixIcon插槽时未进行渲染的问题 (#2696)
    * fix(dropdown): 修复下拉菜单配置template的content或prefixIcon插槽时未进行渲染的问题
    
    * fix(dropdown): 防止item.props为空报错

--- src/dropdown/hooks/useDropdownOptions.ts
+++ src/dropdown/hooks/useDropdownOptions.ts
@@ -1,6 +1,7 @@
 import { computed, ComputedRef, VNode, getCurrentInstance, Slots } from 'vue';
 import isString from 'lodash/isString';
 import isArray from 'lodash/isArray';
+import camelCase from 'lodash/camelCase';
 
 import { useChildComponentSlots } from '../../hooks/slot';
 import type { DropdownOption, TdDropdownProps } from '../type';
@@ -26,6 +27,8 @@ export const getOptionsFromChildren = (menuNode: VNode | VNode[]): DropdownOptio
     }, []);
     return menuNode
       .map((item) => {
+        const slotContent = (item.children as any)?.content?.();
+        const slotPrefixIcon = (item.children as any)?.prefixIcon?.() || (item.children as any)?.['prefix-icon']?.();
         const groupChildren = (item.children as any)?.default?.();
 
         // 当前节点的渲染内容
@@ -38,9 +41,16 @@ export const getOptionsFromChildren = (menuNode: VNode | VNode[]): DropdownOptio
             !isString(v.children) && ['TDropdownMenu', 'TDropdownItem'].includes((v.type as { name: string })?.name),
         );
 
+        // 将item.props的属性名都转成驼峰，再进行传递
+        const itemProps = Object.keys(item.props || {}).reduce((props, propName) => {
+          props[camelCase(propName)] = item.props[propName];
+          return props;
+        }, {});
+
         return {
-          content: contentCtx || groupChildren,
-          ...item.props,
+          content: slotContent || contentCtx || groupChildren,
+          ...itemProps,
+          ...(slotPrefixIcon ? { prefixIcon: () => slotPrefixIcon } : {}),
           children: childrenCtx?.length > 0 ? getOptionsFromChildren(childrenCtx) : null,
         };
       })
