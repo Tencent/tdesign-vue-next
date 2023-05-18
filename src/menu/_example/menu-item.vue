ae5497e1fb9ca7e5904275985aff433006776a18
sinbadmaster <bless_lee@126.com>
2023-05-18 16:29:29 +0800

    fix(menu): head-menu插槽封装缩略异常处理
    closed #2779

---
+++ src/menu/_example/menu-item.vue
@@ -0,0 +1,58 @@
+<template>
+  <t-submenu value="1">
+    <template #title>
+      <span>菜单1</span>
+    </template>
+    <t-menu-item value="1-1">子菜单1-1</t-menu-item>
+    <t-menu-item value="1-2">子菜单1-2</t-menu-item>
+    <t-menu-item value="1-3">子菜单1-3</t-menu-item>
+  </t-submenu>
+  <t-submenu value="2">
+    <template #title>
+      <span>菜单2</span>
+    </template>
+    <t-menu-item value="2-1">子菜单2-1</t-menu-item>
+    <t-menu-item value="2-2">子菜单2-2</t-menu-item>
+    <t-menu-item value="2-3">子菜单2-3</t-menu-item>
+  </t-submenu>
+  <t-submenu value="3">
+    <template #title>
+      <span>菜单3</span>
+    </template>
+    <t-menu-item value="3-1">子菜单3-1</t-menu-item>
+    <t-menu-item value="3-2">子菜单3-2</t-menu-item>
+    <t-menu-item value="3-3">子菜单3-3</t-menu-item>
+  </t-submenu>
+  <t-submenu value="4">
+    <template #title>
+      <span>菜单4</span>
+    </template>
+    <t-menu-item value="4-1">子菜单4-1</t-menu-item>
+    <t-menu-item value="4-2">子菜单4-2</t-menu-item>
+    <t-menu-item value="4-3">子菜单4-3</t-menu-item>
+  </t-submenu>
+  <t-submenu value="5">
+    <template #title>
+      <span>菜单5</span>
+    </template>
+    <t-menu-item value="5-1">子菜单5-1</t-menu-item>
+    <t-menu-item value="5-2">子菜单5-2</t-menu-item>
+    <t-menu-item value="5-3">子菜单5-3</t-menu-item>
+  </t-submenu>
+  <t-submenu value="6">
+    <template #title>
+      <span>菜单6</span>
+    </template>
+    <t-menu-item value="6-1">子菜单6-1</t-menu-item>
+    <t-menu-item value="6-2">子菜单6-2</t-menu-item>
+    <t-menu-item value="6-3">子菜单6-3</t-menu-item>
+  </t-submenu>
+  <t-submenu value="7">
+    <template #title>
+      <span>菜单7</span>
+    </template>
+    <t-menu-item value="7-1">子菜单7-1</t-menu-item>
+    <t-menu-item value="7-2">子菜单7-2</t-menu-item>
+    <t-menu-item value="7-3">子菜单7-3</t-menu-item>
+  </t-submenu>
+</template>
