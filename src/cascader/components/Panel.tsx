3f854822cf03034264ba18acdf03209dfa5d153b
ZekunWu <44672491+ZekunWu@users.noreply.github.com>
2023-05-30 11:45:32 +0800

    feat(cascader): 选项支持自定义样式 (#2878)
    * feat: 同步API
    
    * feat: 完成自定义选项
    
    * feat: 同步API以及修复onBlur
    
    * fix: 修复demo样式
    
    * chore: update snapshot
    
    ---------
    
    Co-authored-by: Uyarn <uyarnchen@gmail.com>

--- src/cascader/components/Panel.tsx
+++ src/cascader/components/Panel.tsx
@@ -1,17 +1,18 @@
-import { defineComponent, PropType, computed } from 'vue';
+import { defineComponent, PropType, computed, h } from 'vue';
 
 import Item from './Item';
 import { TreeNode, CascaderContextType } from '../interface';
 import CascaderProps from '../props';
 import { usePrefixClass, useConfig } from '../../hooks/useConfig';
 import { useTNodeDefault } from '../../hooks/tnode';
-
+import { getDefaultNode } from '../../utils/render-tnode';
 import { getPanels } from '../core/helper';
 import { expendClickEffect, valueChangeEffect } from '../core/effect';
 
 export default defineComponent({
   name: 'TCascaderSubPanel',
   props: {
+    option: CascaderProps.option,
     empty: CascaderProps.empty,
     trigger: CascaderProps.trigger,
     onChange: CascaderProps.onChange,
@@ -34,24 +35,32 @@ export default defineComponent({
       expendClickEffect(propsTrigger, trigger, node, cascaderContext);
     };
 
-    const renderItem = (node: TreeNode) => (
-      <Item
-        key={node.value}
-        node={node}
-        cascaderContext={props.cascaderContext}
-        onClick={() => {
-          handleExpand(node, 'click');
-        }}
-        onMouseenter={() => {
-          handleExpand(node, 'hover');
-        }}
-        onChange={() => {
-          valueChangeEffect(node, props.cascaderContext);
-        }}
-      />
-    );
+    const renderItem = (node: TreeNode, index: number) => {
+      const optionChild = node.data.content
+        ? getDefaultNode(node.data.content(h))
+        : renderTNodeJSXDefault('option', {
+            params: { item: node.data, index },
+          });
+      return (
+        <Item
+          key={node.value}
+          node={node}
+          optionChild={optionChild}
+          cascaderContext={props.cascaderContext}
+          onClick={() => {
+            handleExpand(node, 'click');
+          }}
+          onMouseenter={() => {
+            handleExpand(node, 'hover');
+          }}
+          onChange={() => {
+            valueChangeEffect(node, props.cascaderContext);
+          }}
+        />
+      );
+    };
 
-    const renderList = (treeNodes: TreeNode[], isFilter = false, segment = true, key = '1') => (
+    const renderList = (treeNodes: TreeNode[], isFilter = false, segment = true, index = 1) => (
       <ul
         class={[
           `${COMPONENT_NAME.value}__menu`,
@@ -61,9 +70,9 @@ export default defineComponent({
             [`${COMPONENT_NAME.value}__menu--filter`]: isFilter,
           },
         ]}
-        key={key}
+        key={`${COMPONENT_NAME}__menu${index}`}
       >
-        {treeNodes.map((node: TreeNode) => renderItem(node))}
+        {treeNodes.map((node: TreeNode) => renderItem(node, index))}
       </ul>
     );
 
@@ -72,7 +81,7 @@ export default defineComponent({
       return inputVal
         ? renderList(treeNodes, true)
         : panels.value.map((treeNodes, index: number) =>
-            renderList(treeNodes, false, index !== panels.value.length - 1, `${COMPONENT_NAME.value}__menu${index}`),
+            renderList(treeNodes, false, index !== panels.value.length - 1, index),
           );
     };
 
