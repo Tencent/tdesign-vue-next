54f9367177d9a1d5a33cba0b6ef9b1973e4a351e
黎伟杰 <674416404@qq.com>
2023-07-21 18:00:58 +0800

    feat:  Priority

--- src/form/hooks.ts
+++ src/form/hooks.ts
@@ -14,11 +14,13 @@ export function useFormDisabled(extend?: Ref<boolean>) {
   const ctx = getCurrentInstance();
   const propsDisabled = computed(() => ctx.props.disabled as boolean);
   const { disabled } = inject<FormDisabledProvider>('formDisabled', Object.create(null));
+  //Priority: Component.disabled > ComponentGroup.disabled > Form.disabled
   return computed(() => {
-    if (isBoolean(extend?.value)) return extend.value;
-
+    // Component
     if (isBoolean(propsDisabled.value)) return propsDisabled.value;
-
+    // ComponentGroup
+    if (isBoolean(extend?.value)) return extend.value;
+    // Form
     if (isBoolean(disabled?.value)) return disabled.value;
 
     return false;
