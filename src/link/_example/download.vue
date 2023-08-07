de7bca215e9f903061729ca03739b943c8f0bcf3
xiaosansiji <xiaosansiji@gmail.com>
2023-08-07 14:34:52 +0800

    feat: add download demo

---
+++ src/link/_example/download.vue
@@ -0,0 +1,6 @@
+<template>
+  <t-space>
+    <t-link theme="primary" download href="/starter/assets/vue.ee956b80.svg">跳转链接</t-link>
+    <t-link theme="primary" download="test/download-file.png" href="/starter/assets/vue.ee956b80.svg">跳转链接</t-link>
+  </t-space>
+</template>
