4d50983fb9c6ced7cf1a7342a9e3453d53af679e
pengYYY <pengyue970715@gmail.com>
2023-08-15 11:37:39 +0800

    test: fix test warning

--- src/table/_example/editable-cell.vue
+++ src/table/_example/editable-cell.vue
@@ -148,7 +148,6 @@ const columns = computed(() => [
       // props, 透传全部属性到 Select 组件
       // props 为函数时，参数有：col, row, rowIndex, colIndex, editedRow。一般用于实现编辑组件之间的联动
       props: ({ col, row, rowIndex, colIndex, editedRow }) => {
-        console.log(col, row, rowIndex, colIndex, editedRow);
         return {
           multiple: true,
           minCollapsedNum: 1,
