9f3b20df1f6624a568bd9472d930df491212ea80
ngyyuusora <jiliangsoft@live.com>
2023-08-30 15:05:13 +0800

    feat(tree-select): 树选择支持suffix和suffixIcon (#3290)
    * fix(tree-select): 树选择支持suffix和suffixIcon
    
    * feat(tree-select): 增加自定义前后缀、自定义翻转箭头图标用例
    
    * test(unit): 增加自定义前后缀、自定义翻转箭头图标用例

---
+++ src/tree-select/_example/arrowicon.vue
@@ -0,0 +1,67 @@
+<template>
+  <t-tree-select
+    v-model="value"
+    :data="options"
+    clearable
+    placeholder="请选择"
+    :popup-visible="popupVisible"
+    :suffix-icon="renderArrowIcon"
+    @popup-visible-change="onVisibleChange"
+  >
+    <!-- 也可以通过插槽方式传递箭头图标，示例有效勿删 -->
+    <!-- <template #suffixIcon>
+      <Icon name="center-focus-strong" />
+    </template> -->
+  </t-tree-select>
+</template>
+<script setup lang="jsx">
+import { ref, computed } from 'vue';
+import { Icon } from 'tdesign-icons-vue-next';
+
+const options = [
+  {
+    label: '广东省',
+    value: 'guangdong',
+    children: [
+      {
+        label: '广州市',
+        value: 'guangzhou',
+      },
+      {
+        label: '深圳市',
+        value: 'shenzhen',
+      },
+    ],
+  },
+  {
+    label: '江苏省',
+    value: 'jiangsu',
+    disabled: true,
+    children: [
+      {
+        label: '南京市',
+        value: 'nanjing',
+      },
+      {
+        label: '苏州市',
+        value: 'suzhou',
+      },
+    ],
+  },
+];
+
+const value = ref('');
+const popupVisible = ref(false);
+
+const onVisibleChange = (visible, context) => {
+  console.log(visible, context);
+  if (context.trigger || context.node?.label !== '广州市') {
+    popupVisible.value = visible;
+  }
+};
+
+// props传递suffixIcon，如需实现激活/未激活状态不同图标，可参考此用例onVisibleChange接收事件自行维护状态，传递不同图标。
+const renderArrowIcon = computed(() =>
+  popupVisible.value ? () => '我是选择器激活时内容' : () => <Icon name="center-focus-strong" />,
+);
+</script>
---
+++ src/tree-select/_example/prefixsuffix.vue
@@ -0,0 +1,73 @@
+<template>
+  <t-tree-select
+    v-model="value"
+    :data="options"
+    clearable
+    placeholder="请选择"
+    :popup-visible="popupVisible"
+    :prefix-icon="getPrefixIcon"
+    :suffix="getSuffix"
+    @popup-visible-change="onVisibleChange"
+  >
+    <!-- 也可以通过插槽方式传递，示例有效勿删 -->
+    <!-- <template #prefixIcon>
+      <Icon name="happy" />
+    </template>
+    <template #suffix>foobar suffix</template> -->
+  </t-tree-select>
+</template>
+<script setup lang="jsx">
+import { ref } from 'vue';
+import { Icon } from 'tdesign-icons-vue-next';
+
+const options = [
+  {
+    label: '广东省',
+    value: 'guangdong',
+    children: [
+      {
+        label: '广州市',
+        value: 'guangzhou',
+      },
+      {
+        label: '深圳市',
+        value: 'shenzhen',
+      },
+    ],
+  },
+  {
+    label: '江苏省',
+    value: 'jiangsu',
+    disabled: true,
+    children: [
+      {
+        label: '南京市',
+        value: 'nanjing',
+      },
+      {
+        label: '苏州市',
+        value: 'suzhou',
+      },
+    ],
+  },
+];
+
+const value = ref('');
+const popupVisible = ref(false);
+
+const onVisibleChange = (visible, context) => {
+  console.log(visible, context);
+  if (context.trigger || context.node?.label !== '广州市') {
+    popupVisible.value = visible;
+  }
+};
+
+// props传递prefixIcon、suffix，请注意属性名拼写不同，区分箭头图标的suffixIcon
+const getPrefixIcon = () => <Icon name="center-focus-strong" />;
+const getSuffix = () => (
+  <div style="color: var(--td-text-color-secondary)">
+    <span>后缀内容</span>
+    <Icon name="happy" />
+  </div>
+);
+</script>
