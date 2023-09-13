019ba52522aca2128cc6467cbeac9cca7e620747
wū yāng <uyarnchen@gmail.com>
2023-09-13 21:26:52 +0800

    chore: update demo (#3365)

--- src/list/_example/virtual-scroll.vue
+++ src/list/_example/virtual-scroll.vue
@@ -1,13 +1,15 @@
 <template>
   <t-list style="height: 300px" :scroll="{ type: 'virtual' }"
-    ><t-list-item v-for="(item, index) in listRef" :key="index"> {{ item.content }}</t-list-item></t-list
-  >
+    ><t-list-item v-for="(item, index) in listRef" :key="index">
+      <t-list-item-meta :image="imageUrl" title="列表标题" :description="item.content" /></t-list-item
+  ></t-list>
 </template>
 <script setup>
 import { ref } from 'vue';
 const list = [];
-for (let i = 0; i < 10000; i++) {
-  list.push({ content: `选项${i + 1}` });
+const imageUrl = 'https://tdesign.gtimg.com/site/avatar.jpg';
+for (let i = 0; i < 3000; i++) {
+  list.push({ content: `列表内容的描述性文字` });
 }
 
 const listRef = ref(list);
