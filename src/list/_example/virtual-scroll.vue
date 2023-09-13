a46ef302d792def1b5621debb14285a766abadc0
wū yāng <uyarnchen@gmail.com>
2023-09-13 14:53:13 +0800

    feat(List): support virtual scroll (#3360)
    * feat(List): support virtual scroll
    
    * chore: update snapshot

---
+++ src/list/_example/virtual-scroll.vue
@@ -0,0 +1,14 @@
+<template>
+  <t-list style="height: 300px" :scroll="{ type: 'virtual' }"
+    ><t-list-item v-for="(item, index) in listRef" :key="index"> {{ item.content }}</t-list-item></t-list
+  >
+</template>
+<script setup>
+import { ref } from 'vue';
+const list = [];
+for (let i = 0; i < 10000; i++) {
+  list.push({ content: `选项${i + 1}` });
+}
+
+const listRef = ref(list);
+</script>
--- src/list/props.ts
+++ src/list/props.ts
@@ -2,7 +2,6 @@
 
 /**
  * 该文件为脚本自动生成文件，请勿随意修改。如需修改请联系 PMC
- * updated at 2021-12-12 19:17:30
  * */
 
 import { TdListProps } from './type';
@@ -26,14 +25,20 @@ export default {
     type: String as PropType<TdListProps['layout']>,
     default: 'horizontal' as TdListProps['layout'],
     validator(val: TdListProps['layout']): boolean {
+      if (!val) return true;
       return ['horizontal', 'vertical'].includes(val);
     },
+  },
+  /** 懒加载和虚拟滚动。为保证组件收益最大化，当数据量小于阈值 `scroll.threshold` 时，无论虚拟滚动的配置是否存在，组件内部都不会开启虚拟滚动，`scroll.threshold` 默认为 `100` */
+  scroll: {
+    type: Object as PropType<TdListProps['scroll']>,
   },
   /** 尺寸 */
   size: {
     type: String as PropType<TdListProps['size']>,
     default: 'medium' as TdListProps['size'],
     validator(val: TdListProps['size']): boolean {
+      if (!val) return true;
       return ['small', 'medium', 'large'].includes(val);
     },
   },
