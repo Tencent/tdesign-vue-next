70e0736cfc4ac8f8357f24995f15aa3a9f19df65
chaishi <974383157@qq.com>
2023-09-23 20:04:17 +0800

    feat(table): support keyboard operation without row highlight

--- src/table/_example/select-single.vue
+++ src/table/_example/select-single.vue
@@ -1,5 +1,15 @@
 <template>
   <div>
+    <t-space>
+      <!-- 默认支持单项键盘操作选中或取消 -->
+      <!-- 设置 active-row-type="single" 行高亮后，支持键盘批量操作，
+          可尝试使用 Shift/Space/ArrowUp/ArrowDown/KeyC/KeyA/KeyL 等键盘操作选中和取消选择，
+          其中键 KeyC 表示 Clear，清空高亮，清空全选
+          其中键 KeyA/KeyL 表示全选，全部高亮
+      -->
+      <t-checkbox v-model="activeRow">行高亮</t-checkbox>
+    </t-space>
+
     <!-- 支持非受控属性 default-selected-row-keys -->
     <!-- 支持语法糖 v-model:selected-row-keys -->
     <t-table
@@ -8,7 +18,7 @@
       :data="data"
       :selected-row-keys="selectedRowKeys"
       select-on-row-click
-      active-row-type="single"
+      :active-row-type="activeRow ? 'single' : undefined"
       lazy-load
       @select-change="rehandleSelectChange"
       @row-dblclick="onRowDblclick"
@@ -79,6 +89,7 @@ const columns = [
   { colKey: 'createTime', title: '申请时间' },
 ];
 
+const activeRow = ref(false);
 const selectedRowKeys = ref([2]);
 
 const rehandleSelectChange = (value, { selectedRowData }) => {
