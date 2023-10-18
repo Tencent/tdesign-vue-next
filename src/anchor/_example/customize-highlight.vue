8d995a1f9af6d14720dc0c4ab5f3cd6c08ccb616
Uyarn <uyarnchen@gmail.com>
2023-10-18 13:46:27 +0800

    chore: resolve conflict

--- src/anchor/_example/customize-highlight.vue
+++ src/anchor/_example/customize-highlight.vue
@@ -1,8 +1,6 @@
 <template>
   <t-space>
     <div class="flex: 1"></div>
-    <t-button @click="() => testClick('#不同尺寸的锚点')">#不同尺寸的锚点</t-button>
-    <t-button @click="() => testClick('#特定交互锚点')">#特定交互锚点</t-button>
     <t-anchor :bounds="150" :get-current-anchor="getCurrentAnchor" @click="handleClick">
       <t-anchor-item href="#基础锚点" title="基础锚点"></t-anchor-item>
       <t-anchor-item href="#多级锚点" title="多级锚点"></t-anchor-item>
@@ -20,10 +18,6 @@ const getCurrentAnchor = ref(() => {
   // return oldHref || '#不同尺寸的锚点';
   return '#不同尺寸的锚点';
 });
-
-const testClick = (href) => {
-  getCurrentAnchor.value = () => href;
-};
 
 const handleClick = (link) => {
   console.log('link', link);
