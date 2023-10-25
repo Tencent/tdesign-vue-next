38f93786cd3f457a3d4c5b3c793a9196c215e2b7
Yuhang <aicmortal@gmail.com>
2023-10-25 16:47:53 +0800

    fix(Tree-Select): 修复未使用keys别名 (#3520)
    * fix: 修复未使用keys别名
    
    * fix: 修复未使用keys别名

--- src/tree-select/tree-select.tsx
+++ src/tree-select/tree-select.tsx
@@ -127,21 +127,21 @@ export default defineComponent({
       if (!isEmpty(props.treeProps) && !isEmpty((props.treeProps as TreeProps).keys)) {
         return (props.treeProps as TreeProps).keys.label || 'label';
       }
-      return 'label';
+      return props.keys?.label || 'label';
     });
 
     const realValue = computed(() => {
       if (!isEmpty(props.treeProps) && !isEmpty((props.treeProps as TreeProps).keys)) {
         return (props.treeProps as TreeProps).keys.value || 'value';
       }
-      return 'value';
+      return props.keys?.value || 'value';
     });
 
     const realChildren = computed(() => {
       if (!isEmpty(props.treeProps) && !isEmpty((props.treeProps as TreeProps).keys)) {
         return (props.treeProps as TreeProps).keys.children || 'children';
       }
-      return 'children';
+      return props.keys?.children || 'children';
     });
 
     // timelifes
