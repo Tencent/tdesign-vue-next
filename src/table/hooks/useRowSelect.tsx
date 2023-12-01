a1dd4e5c9ec8995ed249409c363f9c2d3e47713c
chaishi <974383157@qq.com>
2023-12-01 14:33:13 +0800

    fix(table): lint error

--- src/table/hooks/useRowSelect.tsx
+++ src/table/hooks/useRowSelect.tsx
@@ -24,7 +24,7 @@ export default function useRowSelect(
   props: TdPrimaryTableProps,
   tableSelectedClasses: TableClassName['tableSelectedClasses'],
 ) {
-  const { selectedRowKeys, columns, rowKey, data, pagination, reserveSelectedRowOnPaginate } = toRefs(props);
+  const { selectedRowKeys, columns, rowKey, data, reserveSelectedRowOnPaginate } = toRefs(props);
   const currentPaginateData = ref<TableRowData[]>(data.value);
   const selectedRowClassNames = ref();
   const [tSelectedRowKeys, setTSelectedRowKeys] = useDefaultValue(
