979b01f147d2d50b01a5bf9cdbf01966d9cbe2ea
Uyarn <uyarnchen@gmail.com>
2023-12-19 14:49:25 +0800

    fix(tree-select): clear input value when panel display

--- src/tree-select/tree-select.tsx
+++ src/tree-select/tree-select.tsx
@@ -143,6 +143,12 @@ export default defineComponent({
       return props.keys?.children || 'children';
     });
 
+    watch(innerVisible, (value) => {
+      if (value) {
+        innerInputValue.value && setInnerInputValue(''); // 原本存在搜索内容 重新打开时清空
+      }
+    });
+
     // timelifes
     onMounted(async () => {
       if (!treeSelectValue.value && props.defaultValue) {
