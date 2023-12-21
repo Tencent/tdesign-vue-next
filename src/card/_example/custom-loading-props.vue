dc79211664487e9e658f0c9a31d5f960748cdbb3
iiimix <690360602@qq.com>
2023-12-21 15:18:57 +0800

    feat(card): card组件支持传入loadingProps参数 (#3731)
    * feat(card): card组件支持传入loadingProps参数
    
    card组件支持传入loadingProps参数
    
    closed #423
    
    * chore: update snapshot
    
    update snapshot
    
    * chore: update snapshot
    
    ---------
    
    Co-authored-by: Uyarn <uyarnchen@gmail.com>

---
+++ src/card/_example/custom-loading-props.vue
@@ -0,0 +1,19 @@
+<template>
+  <t-card
+    bordered
+    title="自定义loadingProps"
+    :loading="isLoading"
+    :style="{ width: '400px' }"
+    :loading-props="customProps"
+  >
+    {{ infoMessage }}
+  </t-card>
+</template>
+
+<script setup lang="jsx">
+const customProps = {
+  text: 'TDesign努力加载中...',
+};
+const isLoading = true;
+const infoMessage = `卡片内容，以描述性为主，可以是文字、图片或图文组合的形式。按业务需求进行自定义组合。`;
+</script>
