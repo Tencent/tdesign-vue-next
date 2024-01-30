257a0cd90bebf93114013b650852b587f690631a
wū yāng <uyarnchen@gmail.com>
2024-01-30 21:53:54 +0800

    feat(loading): directive object (#3911)

--- src/loading/_example/directive.vue
+++ src/loading/_example/directive.vue
@@ -1,10 +1,15 @@
 <template>
   <t-space direction="vertical">
     <div v-loading="loading1" class="loading-container">Loading...</div>
-    <t-button size="small" :disabled="loading1" @click="handleLoading">支持指令</t-button>
+    <t-button size="small" :disabled="loading1" @click="handleLoading">v-loading</t-button>
 
     <div v-loading.fullscreen="loading2" class="loading-container">Loading...</div>
-    <t-button size="small" :disabled="loading2" @click="handleLoadingFullscreen">支持 fullscreen 修饰符</t-button>
+    <t-button size="small" :disabled="loading2" @click="handleLoadingFullscreen">v-loading.fullscreen</t-button>
+
+    <div v-loading="customLoading" class="loading-container"></div>
+    <t-button size="small" :disabled="customLoading.loading" @click="handleCustomLoading"
+      >custom configuration for v-loading</t-button
+    >
   </t-space>
 </template>
 <script setup>
@@ -12,6 +17,11 @@ import { ref } from 'vue';
 
 const loading1 = ref(false);
 const loading2 = ref(false);
+const customLoading = ref({
+  loading: false,
+  showOverlay: true,
+  text: 'loading...',
+});
 
 const handleLoading = () => {
   loading1.value = true;
@@ -25,6 +35,15 @@ const handleLoadingFullscreen = () => {
   loading2.value = true;
   const timer = setTimeout(() => {
     loading2.value = false;
+    clearTimeout(timer);
+  }, 1000);
+};
+
+const handleCustomLoading = () => {
+  customLoading.value = { ...customLoading.value, loading: true };
+  const timer = setTimeout(() => {
+    customLoading.value = { ...customLoading.value, loading: false };
+
     clearTimeout(timer);
   }, 1000);
 };
