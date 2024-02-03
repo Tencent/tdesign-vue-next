b59aa8120576f7b049704f351205cf335a08229a
黎伟杰 <674416404@qq.com>
2024-02-03 11:01:54 +0800

    chore: 编辑器代码提示更新

--- global.d.ts
+++ global.d.ts
@@ -1,6 +1,7 @@
-/* eslint-disable */
 /**
  * 该文件为脚本自动生成文件，请勿随意修改。如需修改请联系 PMC
+ * https://github.com/TDesignOteam/tdesign-api
+ * eslint-disable
  * */
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
