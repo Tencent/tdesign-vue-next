738eaf039205508bf5a7f29bcb4ed9c12f7b0d81
chaishi <974383157@qq.com>
2024-02-06 16:55:12 +0800

    docs(color-picker): add ts demo

---
+++ src/color-picker/_example-ts/status-disabled.vue
@@ -0,0 +1,8 @@
+<template>
+  <t-color-picker v-model="color" disabled />
+</template>
+<script lang='ts' setup>
+import { ref } from 'vue';
+const color = ref('#0052d9');
+
+</script>
--- src/color-picker/index.ts
+++ src/color-picker/index.ts
@@ -7,6 +7,7 @@ import './style';
 
 export * from './type';
 export type ColorPickerProps = TdColorPickerProps;
+export type ColorPickerPanelProps = TdColorPickerProps;
 
 export const ColorPickerPanel = withInstall(_ColorPickerPanel);
 export const ColorPicker = withInstall(_ColorPicker);
