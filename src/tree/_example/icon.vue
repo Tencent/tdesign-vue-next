e3d02996c992e50237bfc96aec35eb9f56d828c0
Haixing <65376724+HaixingOoO@users.noreply.github.com>
2024-02-08 11:47:14 +0800

    docs(tree): fix tree ts error (#3944)

--- src/tree/_example/icon.vue
+++ src/tree/_example/icon.vue
@@ -51,15 +51,15 @@ const load = (node) => {
   return new Promise((resolve) => {
     setTimeout(() => {
       let nodes = [];
-      if (node.level < maxLevel) {
+      if (node.getLevel() < maxLevel) {
         nodes = [
           {
             label: `${node.label}.1`,
-            children: node.level < maxLevel - 1,
+            children: node.getLevel() < maxLevel - 1,
           },
           {
             label: `${node.label}.2`,
-            children: node.level < maxLevel - 1,
+            children: node.getLevel() < maxLevel - 1,
           },
         ];
       }
