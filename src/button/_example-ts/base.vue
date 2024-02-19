73b219a69496064539117418476a69f6ede89c83
Uyarn <uyarnchen@gmail.com>
2024-02-19 17:40:41 +0800

    chore: remove extra change

--- src/button/_example-ts/base.vue
+++ src/button/_example-ts/base.vue
@@ -1,8 +1,8 @@
 <template>
   <t-space size="24px">
-    <t-button theme="default" variant="base">TS填充按钮</t-button>
-    <t-button theme="default" variant="outline">TS描边按钮</t-button>
-    <t-button theme="default" variant="dashed">TS虚框按钮</t-button>
-    <t-button theme="default" variant="text">TS文字按钮</t-button>
+    <t-button theme="default" variant="base">填充按钮</t-button>
+    <t-button theme="default" variant="outline">描边按钮</t-button>
+    <t-button theme="default" variant="dashed">虚框按钮</t-button>
+    <t-button theme="default" variant="text">文字按钮</t-button>
   </t-space>
 </template>
