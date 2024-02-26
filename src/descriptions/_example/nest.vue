b0b0da338b94aa5abdf4bcbe0a9457a54e6874b8
zhangpaopao <zhangpaopao0609@gmail.com>
2024-02-27 00:01:30 +0800

    feat(descriptions): support nest (#3970)
    * feat(descriptions): support nest
    
    * test: update snap
    
    * chore(_common): update common

---
+++ src/descriptions/_example/nest.vue
@@ -0,0 +1,13 @@
+<template>
+  <t-descriptions title="Shipping address" bordered>
+    <t-descriptions-item label="Name">TDesign</t-descriptions-item>
+    <t-descriptions-item label="Telephone Number">139****0609</t-descriptions-item>
+    <t-descriptions-item label="Area">China Tencent Headquarters</t-descriptions-item>
+    <t-descriptions-item label="Address">
+      <t-descriptions :label-style="{ background: '#f4f4f4' }" colon>
+        <t-descriptions-item label="City">Shenzhen</t-descriptions-item>
+        <t-descriptions-item label="Detail">Penguin Island D1 4A Mail Center</t-descriptions-item>
+      </t-descriptions>
+    </t-descriptions-item>
+  </t-descriptions>
+</template>
