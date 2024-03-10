5048bfbfb99dceb85408388bc7e8bb806a8d8a65
zwkang <kang95630@gmail.com>
2024-03-10 20:53:20 +0800

    feat(drawer): drawer component support size drag event and size drage limit

--- src/drawer/hooks.ts
+++ src/drawer/hooks.ts
@@ -2,6 +2,22 @@ import { computed, ref } from 'vue';
 import { Styles } from '../common';
 import type { TdDrawerProps } from './type';
 
+function getSizeDraggable(sizeDraggable: TdDrawerProps['sizeDraggable'], limit: { max: number; min: number }) {
+  if (typeof sizeDraggable === 'boolean') {
+    return {
+      allowSizeDraggable: sizeDraggable,
+      max: limit.max,
+      min: limit.min,
+    };
+  }
+
+  return {
+    allowSizeDraggable: true,
+    max: sizeDraggable.max,
+    min: sizeDraggable.min,
+  };
+}
+
 export const useDrag = (props: TdDrawerProps) => {
   // 以下为拖拽改变抽屉大小相关 可以抽成hooks
   const isSizeDragging = ref(false);
@@ -20,32 +36,46 @@ export const useDrag = (props: TdDrawerProps) => {
     isSizeDragging.value = false;
   };
 
-  const handleMousemove = (e: MouseEvent) => {
+  const handleMousemove = (e: DragEvent) => {
     // 鼠标移动时计算draggedSizeValue的值
     const { x, y } = e;
     const maxHeight = document.documentElement.clientHeight;
     const maxWidth = document.documentElement.clientWidth;
     const offsetHeight = 8;
     const offsetWidth = 8;
+    // x 轴方向使用最大宽度，y轴方向使用最大高度
+    const max = props.placement === 'left' || props.placement === 'right' ? maxWidth : maxHeight;
+    // x 轴方向使用默认最小宽度，y轴方向使用默认最小高度
+    const min = props.placement === 'left' || props.placement === 'right' ? offsetWidth : offsetHeight;
+    const { allowSizeDraggable, max: limitMax, min: limitMin } = getSizeDraggable(props.sizeDraggable, { max, min });
 
-    if (isSizeDragging.value && props.sizeDraggable) {
-      if (props.placement === 'right') {
-        const moveLeft = Math.min(Math.max(maxWidth - x + offsetWidth, offsetWidth), maxWidth);
-        draggedSizeValue.value = `${moveLeft}px`;
-      }
-      if (props.placement === 'left') {
-        const moveRight = Math.min(Math.max(x + offsetWidth, offsetWidth), maxWidth);
-        draggedSizeValue.value = `${moveRight}px`;
-      }
-      if (props.placement === 'top') {
-        const moveBottom = Math.min(Math.max(y + offsetHeight, offsetHeight), maxHeight);
-        draggedSizeValue.value = `${moveBottom}px`;
-      }
-      if (props.placement === 'bottom') {
-        const moveTop = Math.min(Math.max(maxHeight - y + offsetHeight, offsetHeight), maxHeight);
-        draggedSizeValue.value = `${moveTop}px`;
-      }
+    // 不支持拖拽就直接返回
+    if (!allowSizeDraggable || !isSizeDragging.value) return;
+
+    let moveSize: number | undefined;
+    switch (props.placement) {
+      case 'right':
+        moveSize = Math.min(Math.max(maxWidth - x, limitMin), limitMax);
+        break;
+      case 'left':
+        moveSize = Math.min(Math.max(x, limitMin), limitMax);
+        break;
+      case 'top':
+        moveSize = Math.min(Math.max(y, limitMin), limitMax);
+        break;
+      case 'bottom':
+        moveSize = Math.min(Math.max(maxHeight - y, limitMin), limitMax);
+        break;
+      default:
+        // 参数缺失直接返回
+        return;
     }
+
+    draggedSizeValue.value = `${moveSize}px`;
+    props.onSizeDragEnd?.({
+      e,
+      size: moveSize,
+    });
   };
 
   const draggableLineStyles = computed(() => {
