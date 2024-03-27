85a4a060a28dec56c47dc8a3780a67d72f1fa95b
sinbadmaster <bless_lee@126.com>
2024-03-27 11:07:22 +0800

    fix(menu): 去除demo中的争议属性
    closed #3846

--- src/menu/_example/side-menu-width.vue
+++ src/menu/_example/side-menu-width.vue
@@ -1,5 +1,5 @@
 <template>
-  <t-menu v-model="value" theme="light" width="200px" height="550px">
+  <t-menu v-model="value" theme="light" width="200px">
     <template #logo>
       <img height="28" src="https://tdesign.gtimg.com/site/baseLogo-light.png" alt="logo" />
     </template>
