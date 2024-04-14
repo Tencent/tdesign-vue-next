294dd9fa14a0c4d9c423368d19e7621df4ec4000
yojim <chenyujian@oigcn.cn>
2024-04-14 20:01:28 +0800

    fix(image): `src = File` error triggered `handleError

--- src/hooks/useImagePreviewUrl.ts
+++ src/hooks/useImagePreviewUrl.ts
@@ -3,21 +3,24 @@ import { getFileUrlByFileRaw } from '../_common/js/upload/utils';
 
 export function useImagePreviewUrl(imgUrl: Ref<string | File> | ComputedRef<string | File>) {
   const previewUrl = ref('');
+  const loading = ref(true);
 
   watch(
     [imgUrl],
     ([imgUrl], [preImgUrl]) => {
       if (preImgUrl === imgUrl) return;
       if (typeof imgUrl === 'string') {
+        loading.value = false;
         previewUrl.value = imgUrl;
         return;
       }
       getFileUrlByFileRaw(imgUrl).then((url) => {
+        loading.value = false;
         previewUrl.value = url;
       });
     },
     { immediate: true },
   );
 
-  return { previewUrl };
+  return { loading, previewUrl };
 }
