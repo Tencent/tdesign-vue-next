47c00f3c2afbb5d7289c91de64ed7184c6df0a9a
boogie-ben <Boogie.Ben@outlook.com>
2024-04-18 12:04:09 +0800

    fix(BreadcrumbItem): 读取content内容的问题; 文本溢出时调用slot?.default()非函数的问题

--- src/breadcrumb/_example/base.vue
+++ src/breadcrumb/_example/base.vue
@@ -1,7 +1,7 @@
 <template>
   <t-breadcrumb :max-item-width="'150'">
-    <t-breadcrumbItem to="/vue-next/components/menu"> 页面1 </t-breadcrumbItem>
-    <t-breadcrumbItem>页面2面包屑文案超长时悬浮显示文案全部信息</t-breadcrumbItem>
-    <t-breadcrumbItem :max-width="'160'"> 面包屑中文案过长时可缩略显示，鼠标hover时显示全部 </t-breadcrumbItem>
+    <t-breadcrumb-item to="/vue-next/components/menu"> 页面1 </t-breadcrumb-item>
+    <t-breadcrumb-item>页面2面包屑文案超长时悬浮显示文案全部信息</t-breadcrumb-item>
+    <t-breadcrumb-item :max-width="'160'"> 面包屑中文案过长时可缩略显示，鼠标hover时显示全部 </t-breadcrumb-item>
   </t-breadcrumb>
 </template>
