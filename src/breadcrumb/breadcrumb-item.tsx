68fc2245b2a7da9643fc8a8616160c4a103153ac
BoogieBen <Boogie.Ben@outlook.com>
2024-04-19 01:00:50 +0800

    fix(BreadcrumbItem): 读取content内容的问题; 文本溢出时调用slot?.default()非函数的问题 (#4120)

--- src/breadcrumb/breadcrumb-item.tsx
+++ src/breadcrumb/breadcrumb-item.tsx
@@ -6,7 +6,7 @@ import Tooltip from '../tooltip/index';
 import { isNodeOverflow } from '../utils/dom';
 import { usePrefixClass } from '../hooks/useConfig';
 import { useGlobalIcon } from '../hooks/useGlobalIcon';
-import { useTNodeJSX } from '../hooks/tnode';
+import { useTNodeJSX, useContent } from '../hooks/tnode';
 import isFunction from 'lodash/isFunction';
 
 interface LocalTBreadcrumb {
@@ -31,7 +31,10 @@ export default defineComponent({
   props: {
     ...props,
   },
-  setup(props, { slots, attrs }) {
+  setup(props, { attrs }) {
+    const renderContent = useContent();
+    const renderTNodeJSX = useTNodeJSX();
+
     const breadcrumbText = ref<HTMLElement>();
     const localTBreadcrumb = inject('tBreadcrumb', localTBreadcrumbOrigin);
     const themeClassName = ref(localTBreadcrumb?.theme);
@@ -84,8 +87,6 @@ export default defineComponent({
     };
 
     return () => {
-      const renderTNodeJSX = useTNodeJSX();
-
       const itemClass = [COMPONENT_NAME.value, themeClassName.value];
       const textClass = [textFlowClass.value];
 
@@ -101,11 +102,14 @@ export default defineComponent({
           }
         },
       };
+
+      const content = renderContent('default', 'content');
+
       const textContent = (
         <span {...{ class: maxLengthClass.value, style: maxWithStyle.value }}>
           {renderTNodeJSX('icon')}
           <span ref={breadcrumbText} class={`${maxLengthClass.value}-text`}>
-            {renderTNodeJSX('default')}
+            {content}
           </span>
         </span>
       );
@@ -121,7 +125,7 @@ export default defineComponent({
       }
       return (
         <div class={itemClass} {...attrs} onClick={!props.disabled && props.onClick}>
-          {isCutOff.value ? <Tooltip content={() => slots?.default()}>{itemContent}</Tooltip> : itemContent}
+          {isCutOff.value ? <Tooltip content={() => content}>{itemContent}</Tooltip> : itemContent}
           <span class={separatorClass.value}>
             {isFunction(separatorContent) ? separatorContent() : separatorContent}
           </span>
