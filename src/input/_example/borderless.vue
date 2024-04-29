84ac0eebc78cd905b91a2676102c1fc0abbeb470
Uyarn <uyarnchen@gmail.com>
2024-04-29 11:53:59 +0800

    feat(Input): add borderless mode

---
+++ src/input/_example/borderless.vue
@@ -0,0 +1,18 @@
+<template>
+  <t-space direction="vertical" style="max-width: 500px">
+    <t-input clearable>
+      <template #suffixIcon>
+        <search-icon />
+      </template>
+    </t-input>
+
+    <t-input placeholder="borderless Input" borderless clearable>
+      <template #suffixIcon>
+        <search-icon />
+      </template>
+    </t-input>
+  </t-space>
+</template>
+<script setup>
+import { SearchIcon } from 'tdesign-icons-vue-next';
+</script>
