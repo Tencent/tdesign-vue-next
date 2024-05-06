09b48811b15686f5b6f70980025155fb6e7d69d8
Uyarn <uyarnchen@gmail.com>
2024-05-06 17:18:55 +0800

    chore: update snapshot

--- src/input/_example/borderless.vue
+++ src/input/_example/borderless.vue
@@ -1,18 +1,8 @@
 <template>
   <t-space direction="vertical" style="max-width: 500px">
-    <t-input clearable>
-      <template #suffixIcon>
-        <search-icon />
-      </template>
-    </t-input>
+    <t-input placeholder="please input" clearable> </t-input>
 
-    <t-input placeholder="borderless Input" borderless clearable>
-      <template #suffixIcon>
-        <search-icon />
-      </template>
-    </t-input>
+    <t-input placeholder="borderless Input" borderless clearable> </t-input>
   </t-space>
 </template>
-<script setup>
-import { SearchIcon } from 'tdesign-icons-vue-next';
-</script>
+<script setup></script>
