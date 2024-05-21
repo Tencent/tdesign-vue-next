6aef4e8c417b3945a9013e2e9f372c93b62f4a79
zhangpaopao <zhangpaopao0609@gmail.com>
2024-05-21 19:58:22 +0800

    chore: site vite config

--- sites/vue3/vite.config.ts
+++ sites/vue3/vite.config.ts
@@ -1,8 +1,25 @@
-import { defineConfig } from 'vite';
+import * as path from 'node:path';
+import vue from '@vitejs/plugin-vue';
+import vueJsx from '@vitejs/plugin-vue-jsx';
 import { VitePWA } from 'vite-plugin-pwa';
-import tDocPlugin from './plugin-doc';
-import pwaConfig from './pwaConfig';
-import { resolveConfig, basePlugin } from '../script/vite.base.config';
+import { defineConfig, searchForWorkspaceRoot } from 'vite';
+
+import TDocPlugin from './plugin-doc';
+import PWA from './configs/pwaConfig';
+
+const workspaceRoot = searchForWorkspaceRoot(process.cwd());
+const getRootPath = (...args: string[]) => path.posix.resolve(workspaceRoot, ...args);
+
+function resolveAlias(vueVersion: number) {
+  return {
+    '@adapter/vue': getRootPath(`packages/adapter/vue/vue${vueVersion}`),
+    '@adapter/hooks': getRootPath(`packages/adapter/hooks/vue${vueVersion}`),
+    '@adapter/utils': getRootPath(`packages/adapter/utils/vue${vueVersion}`),
+    '@td/intel': getRootPath(`packages/intel/vue${vueVersion}/src`),
+    '@td/components': getRootPath(`packages/components/vue${vueVersion}`),
+    'tdesign-vue-next/es/locale': getRootPath(`packages/components/locale/src`),
+  };
+}
 
 const publicPathMap = {
   preview: '/',
@@ -10,25 +27,39 @@ const publicPathMap = {
   production: 'https://static.tdesign.tencent.com/vue-next/',
 };
 
-export default ({ mode }) => {
-  return defineConfig({
+// ! vue-next 中是从 ../script/vite.base.config 引入
+const isCustomElement = tag => tag.startsWith('td-') || tag.startsWith('tdesign-theme');
+
+export default defineConfig(({ mode }) => {
+  return {
     base: publicPathMap[mode],
-    resolve: resolveConfig,
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname),
+        ...resolveAlias(3),
+      },
+    },
     server: {
       host: '0.0.0.0',
       port: 17000,
       open: '/',
-      https: false,
       fs: {
         allow: ['..'],
       },
     },
-    build: {
-      outDir: '../_site',
-    },
-    plugins: [...basePlugin, tDocPlugin(), VitePWA(pwaConfig)],
-    optimizeDeps: {
-      include: ['prismjs', 'prismjs/components/prism-bash.js'],
-    },
-  });
-};
+    plugins: [
+      VitePWA(PWA),
+      TDocPlugin(),
+      vue({
+        template: {
+          compilerOptions: {
+            isCustomElement,
+          },
+        },
+      }),
+      vueJsx({
+        isCustomElement,
+      }),
+    ],
+  };
+});
