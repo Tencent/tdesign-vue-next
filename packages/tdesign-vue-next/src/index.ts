38483a741b1014d7b144c5d8084aa624726019c4
zhangpaopao <zhangpaopao0609@gmail.com>
2024-05-30 13:35:49 +0800

    refactor: fix index

--- packages/tdesign-vue-next/src/index.ts
+++ packages/tdesign-vue-next/src/index.ts
@@ -1,80 +1,80 @@
 // 基础
-export * from './src/button';
-export * from './src/icon';
-export * from './src/link';
+export * from './button';
+export * from './icon';
+export * from './link';
 
 // 布局
-export * from './src/grid';
-export * from './src/layout';
-export * from './src/divider';
-export * from './src/space';
-export * from './src/descriptions';
+export * from './grid';
+export * from './layout';
+export * from './divider';
+export * from './space';
+export * from './descriptions';
 
 // 导航
-export * from './src/affix';
-export * from './src/anchor';
-export * from './src/breadcrumb';
-export * from './src/dropdown';
-export * from './src/menu';
-export * from './src/pagination';
-export * from './src/steps';
-export * from './src/tabs';
-export * from './src/sticky-tool';
-export * from './src/back-top';
+export * from './affix';
+export * from './anchor';
+export * from './breadcrumb';
+export * from './dropdown';
+export * from './menu';
+export * from './pagination';
+export * from './steps';
+export * from './tabs';
+export * from './sticky-tool';
+export * from './back-top';
 
 // 输入
-export * from './src/auto-complete';
-export * from './src/cascader';
-export * from './src/date-picker';
-export * from './src/form';
-export * from './src/input-adornment';
-export * from './src/input';
-export * from './src/input-number';
-export * from './src/radio';
-export * from './src/select';
-export * from './src/slider';
-export * from './src/switch';
-export * from './src/tag-input';
-export * from './src/select-input';
-export * from './src/textarea';
-export * from './src/transfer';
-export * from './src/time-picker';
-// export * from './src/tree-select';
-export * from './src/color-picker';
-export * from './src/range-input';
+export * from './auto-complete';
+export * from './cascader';
+export * from './date-picker';
+export * from './form';
+export * from './input-adornment';
+export * from './input';
+export * from './input-number';
+export * from './radio';
+export * from './select';
+export * from './slider';
+export * from './switch';
+export * from './tag-input';
+export * from './select-input';
+export * from './textarea';
+export * from './transfer';
+export * from './time-picker';
+// export * from './tree-select';
+export * from './color-picker';
+export * from './range-input';
 
 // 数据展示
-export * from './src/avatar';
-export * from './src/badge';
-export * from './src/calendar';
-export * from './src/card';
-export * from './src/comment';
-export * from './src/image';
-export * from './src/image-viewer';
-export * from './src/list';
-export * from './src/progress';
-export * from './src/skeleton';
-export * from './src/statistic';
-export * from './src/swiper';
-export * from './src/tag';
-export * from './src/timeline';
-export * from './src/tooltip';
-// export * from './src/tree';
-export * from './src/collapse';
-export * from './src/watermark';
-export * from './src/rate';
+export * from './avatar';
+export * from './badge';
+export * from './calendar';
+export * from './card';
+export * from './comment';
+export * from './image';
+export * from './image-viewer';
+export * from './list';
+export * from './progress';
+export * from './skeleton';
+export * from './statistic';
+export * from './swiper';
+export * from './tag';
+export * from './timeline';
+export * from './tooltip';
+// export * from './tree';
+export * from './collapse';
+export * from './watermark';
+export * from './rate';
 
 // 消息提醒
 
-export * from './src/alert';
-export * from './src/dialog';
-export * from './src/drawer';
-export * from './src/guide';
-export * from './src/loading';
-export * from './src/message';
-export * from './src/notification';
-export * from './src/popconfirm';
-export * from './src/upload';
+export * from './alert';
+export * from './dialog';
+export * from './drawer';
+export * from './guide';
+export * from './loading';
+export * from './message';
+export * from './notification';
+export * from './popconfirm';
+export * from './upload';
 
 // 全局配置
-export * from './src/config-provider';
+export * from './config-provider';
