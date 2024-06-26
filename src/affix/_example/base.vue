46bb299671b2f15410a5502e47840018f576bfa2
黎伟杰 <674416404@qq.com>
2024-06-26 12:10:57 +0800

    test: ci

--- src/affix/_example/base.vue
+++ src/affix/_example/base.vue
@@ -1,7 +1,7 @@
 <template>
   <t-space>
     <t-affix ref="affix" :offset-top="140" :offset-bottom="20">
-      <t-button>test ci</t-button>
+      <t-button>test ci 111</t-button>
     </t-affix>
   </t-space>
 </template>
