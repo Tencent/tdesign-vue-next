4277b4bf9a98c58b7dd07862dff32d5c0c219f2e
黎伟杰 <674416404@qq.com>
2024-08-30 15:21:23 +0800

    chore: fix lint

--- src/tag/tag.tsx
+++ src/tag/tag.tsx
@@ -7,7 +7,6 @@ import { useConfig, usePrefixClass, useCommonClassName } from '../hooks/useConfi
 import { useGlobalIcon } from '../hooks/useGlobalIcon';
 import { useTNodeJSX, useContent } from '../hooks/tnode';
 import { Styles } from '../common';
-import isString from 'lodash/isString';
 
 export default defineComponent({
   name: 'TTag',
