17e9dba57385fd5657283327e1d1d781ee571340
Haixing <65376724+HaixingOoO@users.noreply.github.com>
2024-09-01 23:07:45 +0800

    feat(empty): new Empty component (#4519)
    * feat(empty): new Empty component
    
    * chore(overage file): rm overage file
    
    * chore: rm unused var
    
    * test: update test snapshot
    
    * chore: update coverage-badge
    
    * chore(empty): update empty demo
    
    * chore(empty): update empty demo
    
    * test: update snapshot

---
+++ src/empty/_example-ts/self-defined.vue
@@ -0,0 +1,28 @@
+<template>
+  <t-row :gutter="40">
+    <t-col>
+      <t-empty description="暂无数据">
+        <template #image>
+          <error-circle-icon size="64" color="var(--td-text-color-placeholder)" />
+        </template>
+      </t-empty>
+    </t-col>
+    <t-col>
+      <t-empty description="暂无数据">
+        <template #image>
+          <div
+            style="
+              width: 64px;
+              height: 64px;
+              background-image: url(https://tdesign.gtimg.com/demo/demo-image-1.png);
+              background-size: 100% 100%;
+            "
+          ></div>
+        </template>
+      </t-empty>
+    </t-col>
+  </t-row>
+</template>
+<script lang="ts" setup>
+import { ErrorCircleIcon } from 'tdesign-icons-vue-next';
+</script>
