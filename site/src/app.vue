e0962fc1bf9ebd68b9382eae7170e8a9744a1fe1
阿菜 Cai <jimmyrss1102@gmail.com>
2024-09-12 20:08:22 +0800

    fix: use `tdesign-site-components`’s `getLang` set component internat… (#4561)
    * fix: use `tdesign-site-components`’s `getLang` set component internationalization
    
    * chore: remove listener
    
    ---------
    
    Co-authored-by: wū yāng <uyarnchen@gmail.com>

--- site/src/app.vue
+++ site/src/app.vue
@@ -8,21 +8,15 @@
 import { onMounted, ref } from 'vue';
 import cnConfig from 'tdesign-vue-next/es/locale/zh_CN';
 import enConfig from 'tdesign-vue-next/es/locale/en_US';
+import { getLang } from 'tdesign-site-components';
 
 const globalConfig = ref(cnConfig);
 
 onMounted(() => {
-  const lang = localStorage.getItem('tdesign_site_lang');
-  if (lang) {
-    globalConfig.value = lang === 'en' ? enConfig : cnConfig;
-  }
+  const lang = getLang();
+  globalConfig.value = lang === 'en' ? enConfig : cnConfig;
 });
 
-//  nextLang 'en' | 'zh'
-document.addEventListener('tdesign_site_lang', ({ detail: nextLang }) => {
-  localStorage.setItem('tdesign_site_lang', nextLang);
-  globalConfig.value = nextLang === 'en' ? enConfig : cnConfig;
-});
 </script>
 
 <style lang="less">
