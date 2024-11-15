8c1a6eba2236f47f6bbe7f0146a13444c34375b2
黎伟杰 <674416404@qq.com>
2024-11-15 14:16:07 +0800

    feat(range-input): udpate readonly api

--- src/range-input/type.ts
+++ src/range-input/type.ts
@@ -51,7 +51,6 @@ export interface TdRangeInputProps {
   prefixIcon?: TNode;
   /**
    * 只读状态
-   * @default false
    */
   readonly?: boolean;
   /**
@@ -198,7 +197,6 @@ export interface TdRangeInputPopupProps {
   rangeInputProps?: RangeInputProps;
   /**
    * 只读状态，值为真会隐藏输入框，且无法打开下拉框
-   * @default false
    */
   readonly?: boolean;
   /**
