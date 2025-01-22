dbb0351eb95e002edeaf031ba8c08f789a6b06ef
sukuki <2021152011@email.szu.edu.cn>
2025-01-22 16:47:32 +0800

    revert(cascader): undo changes to example
    Closes #4874

--- src/cascader/_example/show-all-levels.vue
+++ src/cascader/_example/show-all-levels.vue
@@ -16,26 +16,14 @@ const options = [
       {
         label: '子选项一',
         value: '1.1',
-        children: [
-          {
-            label: '三级选项一',
-            value: '1.1.1',
-          },
-          {
-            label: '三级选项二',
-            value: '1.1.2',
-          },
-        ],
       },
       {
         label: '子选项二',
         value: '1.2',
-        children: [
-          {
-            label: '三级选项一',
-            value: '1.2.1',
-          },
-        ],
+      },
+      {
+        label: '子选项三',
+        value: '1.3',
       },
     ],
   },
@@ -46,26 +34,10 @@ const options = [
       {
         label: '子选项一',
         value: '2.1',
-        children: [
-          {
-            label: '三级选项一',
-            value: '2.1.1',
-          },
-          {
-            label: '三级选项二',
-            value: '2.1.2',
-          },
-        ],
       },
       {
         label: '子选项二',
         value: '2.2',
-        children: [
-          {
-            label: '三级选项一',
-            value: '2.2.1',
-          },
-        ],
       },
     ],
   },
