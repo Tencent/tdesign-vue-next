2e58544601038e1b6ae8eecacb148ad4ac3141ee
Uyarn <uyarnchen@gmail.com>
2025-02-02 21:10:36 +0800

    chore: fix implicitAny true

--- packages/components/dropdown/dropdown.tsx
+++ packages/components/dropdown/dropdown.tsx
@@ -31,6 +31,7 @@ export default defineComponent({
         setTimeout(() => (isPopupVisible.value = false), manualCloseTimeout.value);
 
         props.popupProps?.onVisibleChange?.(false, context);
+        // TODO
         // @ts-ignore types only declare onVisibleChange，but not declare on-visible-change
         props.popupProps?.['on-visible-change']?.(false, context);
       }
@@ -42,6 +43,7 @@ export default defineComponent({
       isPopupVisible.value = visible;
 
       props.popupProps?.onVisibleChange?.(visible, context);
+      // TODO
       // @ts-ignore types only declare onVisibleChange，but not declare on-visible-change
       props.popupProps?.['on-visible-change']?.(visible, context);
     };
