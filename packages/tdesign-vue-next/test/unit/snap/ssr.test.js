1e19dfa9410907f41d43ada8d02e6bb2890e692c
zhangpaopao <zhangpaopao0609@gmail.com>
2025-02-14 22:04:05 +0800

    style: for lint

--- packages/tdesign-vue-next/test/unit/snap/ssr.test.js
+++ packages/tdesign-vue-next/test/unit/snap/ssr.test.js
@@ -2,7 +2,6 @@ import { globSync } from 'glob';
 import MockDate from 'mockdate';
 import { vi } from 'vitest';
 import { config } from '@vue/test-utils';
-import path from 'path';
 
 // 固定时间，当使用 new Date() 时，返回固定时间，防止“当前时间”的副作用影响，导致 snapshot 变更，mockdate 插件见 https://github.com/boblauer/MockDate
 MockDate.set('2020-12-28 00:00:00');
