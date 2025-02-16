ff445c634def3582b92ceff18e0f2374c5ea15b6
zhangpaopao <zhangpaopao0609@gmail.com>
2025-02-16 12:53:04 +0800

    test: vitest use allowExternal: true

--- packages/tdesign-vue-next/test/vitest.config.js
+++ packages/tdesign-vue-next/test/vitest.config.js
@@ -1,11 +1,21 @@
 import { resolveConfig, basePlugin } from '../../../script/vite.base.config';
+import path from 'path';
+
+const resolve = (...args) => path.resolve(...args);
+// TODO 后续使用 @pnpm/find-workspace-dir 替换
+const workspaceDir = resolve(__dirname, '../../../');
+const relativeWorkspacePath = (...args) => resolve(workspaceDir, ...args);
 
 // 单元测试相关配置
 const testConfig = {
   include:
     process.env.NODE_ENV === 'test-snap'
-      ? ['./packages/tdesign-vue-next/test/unit/snap/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}']
-      : ['./packages/components/**/__tests__/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+      ? [
+          relativeWorkspacePath(
+            'packages/tdesign-vue-next/test/unit/snap/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}',
+          ),
+        ]
+      : [relativeWorkspacePath('packages/components/**/__tests__/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}')],
   globals: true,
   environment: 'jsdom',
   testTimeout: 5000,
@@ -15,13 +25,12 @@ const testConfig = {
   },
   coverage: {
     reporter: ['text', 'json', 'html'],
-    reportsDirectory: 'packages/tdesign-vue-next/test/coverage',
+    allowExternal: true,
+    include: [relativeWorkspacePath('packages/components')],
   },
 };
 
 export default {
-  // TODO 后续使用 @pnpm/find-workspace-dir 替换
-  root: '../../../',
   resolve: resolveConfig,
   plugins: basePlugin,
   test: testConfig,
