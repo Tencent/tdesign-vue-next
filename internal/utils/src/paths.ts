6b4f8e68c33bd0e5ab3388b75b5fb38408252061
zhangpaopao <zhangpaopao0609@gmail.com>
2025-02-17 12:46:55 +0800

    refactor: tdesign-vue-next test path

--- internal/utils/src/paths.ts
+++ internal/utils/src/paths.ts
@@ -11,5 +11,22 @@ export const componentsRoot = resolve(packagesRoot, 'components');
 // packages/tdesign-vue-next
 export const tdesignVueNextRoot = resolve(packagesRoot, 'tdesign-vue-next');
 
+// resolve packagesRoot
+export const resolvePackagesRoot = (...args: string[]) => resolve(packagesRoot, ...args);
+// resolve componentsRoot
+export const resolveComponentsRoot = (...args: string[]) => resolve(componentsRoot, ...args);
 // resolve tdesignVueNextRoot
 export const resolveTDesignVueNextRoot = (...args: string[]) => resolve(tdesignVueNextRoot, ...args);
+
+/**
+ * getRelativeWorkspaceRootPath
+ * @description get the relative path from the workspaceRoot directory
+ * @param absolutePath
+ * @returns
+ */
+export const getRelativeWorkspaceRootPath = (absolutePath: string) => {
+  if (!absolutePath.startsWith(workspaceRoot)) {
+    throw new Error('path is not a workspaceRoot path');
+  }
+  return absolutePath.replace(workspaceRoot, '.');
+};
