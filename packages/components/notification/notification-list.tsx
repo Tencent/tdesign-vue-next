b70dd653757e08d6b21bcb1b6895ece12592c7cc
zhangpaopao <zhangpaopao0609@gmail.com>
2025-02-23 10:14:56 +0800

    refactor(message): component optimization

--- packages/components/notification/notification-list.tsx
+++ packages/components/notification/notification-list.tsx
@@ -1,7 +1,7 @@
 import { defineComponent, ref, computed, Ref, CSSProperties } from 'vue';
 import Notification from './notification';
 import { TdNotificationProps, NotificationOptions } from './type';
-import { DEFAULT_Z_INDEX, PLACEMENT_OFFSET, DISTANCE } from './const';
+import { DEFAULT_Z_INDEX, PLACEMENT_OFFSET, DISTANCE } from './consts';
 import { usePrefixClass } from '../hooks/useConfig';
 
 export default defineComponent({
