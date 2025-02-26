76a733d0b289154c6015ddf8682ccfb8494d1b8a
黎伟杰 <674416404@qq.com>
2025-02-26 21:39:45 +0800

    fix: content props

--- packages/components/layout/content.tsx
+++ packages/components/layout/content.tsx
@@ -1,9 +1,11 @@
 import { defineComponent } from 'vue';
 import { usePrefixClass } from '../hooks/useConfig';
 import { useContent } from '../hooks/tnode';
+import props from './content-props';
 
 export default defineComponent({
   name: 'TContent',
+  props,
   setup() {
     const COMPONENT_NAME = usePrefixClass('layout__content');
     const renderContent = useContent();
