b8fadaeda51d7faaff334ef95c839eee6036a384
zhangpaopao <zhangpaopao0609@gmail.com>
2025-02-26 21:30:27 +0800

    refactor(menu): component optimization (#5097)
    * refactor(menu): component optimization
    
    * refactor(menu): fix
    
    * refactor: up snap

--- packages/components/menu/menu-group.tsx
+++ packages/components/menu/menu-group.tsx
@@ -1,23 +1,19 @@
 import { defineComponent } from 'vue';
 import props from './menu-group-props';
-import { renderTNodeJSX } from '../utils/render-tnode';
 import { usePrefixClass } from '../hooks/useConfig';
+import { useTNodeJSX } from '../hooks/tnode';
 
 export default defineComponent({
   name: 'TMenuGroup',
   props,
   setup() {
     const classPrefix = usePrefixClass();
-    return {
-      classPrefix,
-    };
-  },
-  render() {
-    const { classPrefix } = this;
-    return (
-      <div class={`${classPrefix}-menu-group`}>
-        <div class={`${classPrefix}-menu-group__title`}>{renderTNodeJSX(this, 'title', { silent: false })}</div>
-        {renderTNodeJSX(this, 'default')}
+    const renderTNodeJSX = useTNodeJSX();
+
+    return () => (
+      <div class={`${classPrefix.value}-menu-group`}>
+        <div class={`${classPrefix.value}-menu-group__title`}>{renderTNodeJSX('title', { silent: false })}</div>
+        {renderTNodeJSX('default')}
       </div>
     );
   },
