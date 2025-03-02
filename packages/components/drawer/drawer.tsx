75326cd678fb7909e9cb757bb230183a198f1303
Wesley <985189328@qq.com>
2025-03-02 22:02:13 +0800

    fix: type

--- packages/components/drawer/drawer.tsx
+++ packages/components/drawer/drawer.tsx
@@ -110,6 +110,8 @@ export default defineComponent({
       ) {
         props.onEscKeydown?.({ e });
         closeDrawer({ trigger: 'esc', e });
+        // 阻止事件冒泡
+        e.stopImmediatePropagation();
       }
     };
 
