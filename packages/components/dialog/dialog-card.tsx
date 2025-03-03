ccb206a48bde8b5ab5023527aa1c19d541975458
Wesley <985189328@qq.com>
2025-03-03 20:30:00 +0800

    chore: improve dialog-card

--- packages/components/dialog/dialog-card.tsx
+++ packages/components/dialog/dialog-card.tsx
@@ -1,4 +1,4 @@
-import { computed, defineComponent, ref, toRefs, watch } from 'vue';
+import { computed, defineComponent, ref, toRefs } from 'vue';
 import {
   CloseIcon as TdCloseIcon,
   InfoCircleFilledIcon as TdInfoCircleFilledIcon,
@@ -12,14 +12,66 @@ import { useAction } from './hooks';
 import { useContent, useTNodeJSX } from '../hooks/tnode';
 import type { TdDialogProps } from './type';
 import dialogProps from './props';
+import { GetCSSValue } from './hooks';
+
+function InitDragEvent(dragBox: HTMLElement) {
+  const target = dragBox;
+  const windowInnerWidth = window.innerWidth || document.documentElement.clientWidth;
+  const windowInnerHeight = window.innerHeight || document.documentElement.clientHeight;
+  target.addEventListener('mousedown', (targetEvent: MouseEvent) => {
+    // 算出鼠标相对元素的位置
+    const disX = targetEvent.clientX - target.offsetLeft;
+    const disY = targetEvent.clientY - target.offsetTop;
+    const dialogW = target.offsetWidth;
+    const dialogH = target.offsetHeight;
+    // 如果弹出框超出屏幕范围 不能进行拖拽
+    if (dialogW > windowInnerWidth || dialogH > windowInnerHeight) return;
+    function mouseMoverHandler(documentEvent: MouseEvent) {
+      // 用鼠标的位置减去鼠标相对元素的位置，得到元素的位置
+      let left = documentEvent.clientX - disX;
+      let top = documentEvent.clientY - disY;
+      // 临界判断
+      // 拖拽上左边界限制
+      if (left < 0) left = 0;
+      if (top < 0) top = 0;
+      if (windowInnerWidth - target.offsetWidth - left < 0) left = windowInnerWidth - target.offsetWidth;
+      if (windowInnerHeight - target.offsetHeight - top < 0) top = windowInnerHeight - target.offsetHeight;
+      target.style.position = 'absolute';
+      target.style.left = `${left}px`;
+      target.style.top = `${top}px`;
+    }
+    function mouseUpHandler() {
+      // 鼠标弹起来的时候不再移动
+      document.removeEventListener('mousemove', mouseMoverHandler);
+      // 预防鼠标弹起来后还会循环（即预防鼠标放上去的时候还会移动）
+      document.removeEventListener('mouseup', mouseUpHandler);
+    }
+    // 元素按下时注册document鼠标监听事件
+    document.addEventListener('mousemove', mouseMoverHandler);
+    // 鼠标弹起来移除document鼠标监听事件
+    document.addEventListener('mouseup', mouseUpHandler);
+    // 拖拽结束移除鼠标监听事件，解决文字拖拽结束事件未解绑问题
+    document.addEventListener('dragend', mouseUpHandler);
+  });
+}
 
 export default defineComponent({
   name: 'TDialogCard',
+  // 注册v-draggable指令,传入true时候初始化拖拽事件
+  directives: {
+    draggable(el, binding) {
+      // el 指令绑定的元素
+      if (el && binding && binding.value) {
+        InitDragEvent(el);
+      }
+    },
+  },
   props: {
     ...dialogProps,
     ...dialogCardProps,
   },
-  setup(props) {
+  setup(props, { expose }) {
+    const rootRef = ref<HTMLElement | null>(null);
     const COMPONENT_NAME = usePrefixClass('dialog');
     const classPrefix = usePrefixClass();
     const renderTNodeJSX = useTNodeJSX();
@@ -32,7 +84,6 @@ export default defineComponent({
       ErrorCircleFilledIcon: TdErrorCircleFilledIcon,
     });
     const { cancelBtn, confirmBtn, confirmLoading } = toRefs(props);
-
     const confirmBtnAction = (e: MouseEvent) => props.onConfirm?.({ e });
     const cancelBtnAction = (e: MouseEvent) => props.onCancel?.({ e });
     const { getConfirmBtn, getCancelBtn } = useAction({ confirmBtnAction, cancelBtnAction });
@@ -40,14 +91,48 @@ export default defineComponent({
     const isModeLess = computed(() => props.mode === 'modeless');
     // 是否全屏对话框
     const isFullScreen = computed(() => props.mode === 'full-screen');
-
     const closeBtnAction = (e: MouseEvent) => props?.onCloseBtnClick?.({ e });
-
     const onStopDown = (e: MouseEvent) => {
       if (isModeLess.value && props?.draggable) e.stopPropagation();
     };
 
+    const resetPosition = () => {
+      if (!rootRef.value && isModeLess.value && props.draggable) return;
+      rootRef.value.style.position = 'relative';
+      rootRef.value.style.left = 'unset';
+      rootRef.value.style.top = 'unset';
+    };
+
+    // 暴露给父组件的接口
+    expose({
+      $el: rootRef,
+      resetPosition,
+    });
+
+    const dialogClass = computed(() => {
+      const dialogClass = [
+        `${COMPONENT_NAME.value}`,
+        `${COMPONENT_NAME.value}__modal-${props.theme}`,
+        isModeLess.value && props.draggable && `${COMPONENT_NAME.value}--draggable`,
+        props.dialogClassName,
+      ];
+
+      if (isFullScreen.value) {
+        dialogClass.push(`${COMPONENT_NAME.value}__fullscreen`);
+      } else {
+        dialogClass.push(...[`${COMPONENT_NAME.value}--default`, `${COMPONENT_NAME.value}--${props.placement}`]);
+      }
+      return dialogClass;
+    });
+    const dialogStyle = computed(() => {
+      return !isFullScreen.value ? { width: GetCSSValue(props.width), ...props.dialogStyle } : { ...props.dialogStyle }; // width全屏模式不生效
+    });
+
     const renderCard = () => {
+      const confirmBtnLoading = computed(() => {
+        // @ts-ignore
+        return confirmBtn.value?.loading || confirmLoading.value;
+      });
       const defaultFooter = (
         <div>
           {getCancelBtn({
@@ -61,7 +146,7 @@ export default defineComponent({
             globalConfirm: globalConfig.value.confirm,
             globalConfirmBtnTheme: globalConfig.value.confirmBtnTheme,
             className: `${COMPONENT_NAME.value}__confirm`,
-            confirmLoading: (confirmBtn.value as TdDialogProps['confirmBtn'])?.loading || confirmLoading.value,
+            confirmLoading: confirmBtnLoading.value,
           })}
         </div>
       );
@@ -144,6 +229,16 @@ export default defineComponent({
       );
     };
 
-    return () => <>{renderCard()}</>;
+    return () => (
+      <div
+        key="dialog"
+        ref={rootRef}
+        class={dialogClass.value}
+        style={dialogStyle.value}
+        v-draggable={isModeLess.value && props.draggable}
+      >
+        {renderCard()}
+      </div>
+    );
   },
 });
--- packages/components/dialog/hooks/index.ts
+++ packages/components/dialog/hooks/index.ts
@@ -1,2 +1,6 @@
 export * from './useAction';
 export * from './useSameTarget';
+
+export function GetCSSValue(v: string | number) {
+  return Number.isNaN(Number(v)) ? v : `${Number(v)}px`;
+}
