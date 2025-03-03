1040989d68686fd8c9bd2b66aa48c822d8510cec
zhangpaopao <zhangpaopao0609@gmail.com>
2025-03-03 13:11:03 +0800

    refactor(color-picker): alpha switch render to setup return

--- packages/components/color-picker/components/panel/alpha.tsx
+++ packages/components/color-picker/components/panel/alpha.tsx
@@ -31,25 +31,20 @@ export default defineComponent({
         background: `linear-gradient(to right, rgba(0, 0, 0, 0), ${props.color.rgb})`,
       };
     });
-    return {
-      baseClassName,
-      railStyle,
-      handleChange,
+
+    return () => {
+      return (
+        <ColorSlider
+          class={[`${baseClassName.value}__alpha`, `${baseClassName.value}--bg-alpha`]}
+          color={props.color}
+          value={props.color.alpha * 100}
+          onChange={handleChange}
+          rail-style={railStyle.value}
+          max-value={100}
+          disabled={props.disabled}
+          type="alpha"
+        />
+      );
     };
-  },
-  render() {
-    const { baseClassName } = this;
-    return (
-      <ColorSlider
-        class={[`${baseClassName}__alpha`, `${baseClassName}--bg-alpha`]}
-        color={this.color}
-        value={this.color.alpha * 100}
-        onChange={this.handleChange}
-        rail-style={this.railStyle}
-        max-value={100}
-        disabled={this.disabled}
-        type="alpha"
-      />
-    );
   },
 });
