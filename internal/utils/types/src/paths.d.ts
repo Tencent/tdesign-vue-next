aee3060e2a7a1df51c523168a931186e436c8392
zhangpaopao <zhangpaopao0609@gmail.com>
2025-03-20 11:45:53 +0800

    refactor: remove path-v2

--- internal/utils/types/src/paths.d.ts
+++ internal/utils/types/src/paths.d.ts
@@ -1,7 +1,7 @@
 declare const resolve: (...paths: string[]) => string;
 export { resolve };
 /**
- * root 原本是使用 @pnpm/find-workspace-dir 获取的，但它的获取方式是同步的，导致部分地方使用时出现问题，比如 vitest 的 snapshot 测试，
+ * root 原本是使用 @pnpm/find-workspace-dir 获取的，但它的获取方式是异步的，导致部分地方使用时出现问题，比如 vitest 的 snapshot 测试，
  * 因此这里改成同步写，在本项目中不会出现问题，因为整个项目只有一个 pnpm-workspace.yaml 文件
  * @returns
  */
