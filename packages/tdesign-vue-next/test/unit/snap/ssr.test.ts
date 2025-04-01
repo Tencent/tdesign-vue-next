9e77b2b9221ffb86281b3193399f6a828ffdf64f
zhangpaopao <zhangpaopao0609@gmail.com>
2025-04-01 10:28:33 +0800

    refactor(test): refactor test js to ts

--- packages/tdesign-vue-next/test/unit/snap/ssr.test.ts
+++ packages/tdesign-vue-next/test/unit/snap/ssr.test.ts
@@ -1,15 +1,31 @@
 import { globSync } from 'glob';
 import MockDate from 'mockdate';
-import { vi } from 'vitest';
-import { config } from '@vue/test-utils';
+import { vi, describe, it, expect } from 'vitest';
 import { resolveComponentsRoot, getRelativeWorkspaceRootPath } from '@tdesign/internal-utils';
+import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
+import { renderToString } from 'vue/server-renderer';
+import TDesign from '@tdesign/components';
 
 // 固定时间，当使用 new Date() 时，返回固定时间，防止“当前时间”的副作用影响，导致 snapshot 变更，mockdate 插件见 https://github.com/boblauer/MockDate
 MockDate.set('2020-12-28 00:00:00');
 
-function runTest() {
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [],
+});
+
+const createSSRApp = (comp: any) => {
+  const app = createApp(comp);
+  // TODO: paopao 从代码上看，这句应该是没有用的
+  app.config.globalProperties.$route = undefined;
+  app.use(TDesign);
+  app.use(router);
+  return renderToString(app);
+};
+
+const runTest = () => {
   const files = globSync(resolveComponentsRoot('**/_example/*.vue'));
-  const { createSSRApp } = config.global;
 
   describe('ssr snapshot test', () => {
     HTMLCanvasElement.prototype.getContext = vi.fn();
@@ -24,6 +40,6 @@ function runTest() {
       });
     });
   });
-}
+};
 
 runTest();
