e58ae5b34ece8e0446779aac7656434531a25035
yeonzhou <yeonzhou@tencent.com>
2025-04-01 20:29:20 +0800

    feat(chat): chat官网示例

---
+++ packages/components/chat/_example/text-message.vue
@@ -0,0 +1,9 @@
+<template>
+  <t-chat-content :content="ask" variant="base" role="user"></t-chat-content>
+</template>
+<script setup lang="ts">
+const ask = `import TDesign from 'tdesign-vue-next'; // 引入tdesign组件库
+import TDesignChat from 'tdesign-vue-chat'; // 引入组件库chat组件库
+app.use(TDesign).use(TDesignChat).use(router).mount('#app');`;
+</script>
+<style scoped></style>
