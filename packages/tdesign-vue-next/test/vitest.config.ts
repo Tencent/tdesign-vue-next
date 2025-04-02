2fb2b25d9131bc0978463d9c94a3a4c290546925
zhangpaopao <zhangpaopao0609@gmail.com>
2025-04-02 10:43:32 +0800

    test(unit): spec to test

--- packages/tdesign-vue-next/test/vitest.config.ts
+++ packages/tdesign-vue-next/test/vitest.config.ts
@@ -15,12 +15,12 @@ export default defineConfig({
   test: {
     include:
       process.env.NODE_ENV === 'test-snap'
-        ? [await resolveTdesignVueNextRoot('test/unit/snap/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}')]
-        : [await resolveComponentsRoot('**/__tests__/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}')],
+        ? [await resolveTdesignVueNextRoot('test/unit/snap/*.test.ts')]
+        : [await resolveComponentsRoot('**/__tests__/*.test.{js,ts,jsx,tsx}')],
     globals: true,
     environment: 'jsdom',
     testTimeout: 5000,
-    setupFiles: process.env.NODE_ENV === 'test-snap' ? './unit/test-setup.js' : '',
+    setupFiles: process.env.NODE_ENV === 'test-snap' ? './unit/test-setup.ts' : '',
     coverage: {
       provider: 'v8',
       reporter: ['text', 'json', 'html'],
