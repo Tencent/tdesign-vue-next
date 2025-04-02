9ed5f2a155551dcb733cfa859609f8af6bb8d4e3
yeonzhou <yeonzhou@tencent.com>
2025-04-02 09:44:36 +0800

    feat(chat): usage生成

--- packages/components/chat/style/index.js
+++ packages/components/chat/style/index.js
@@ -1,1 +1,2 @@
-import '@tdesign/common-style/web/components/checkbox/_index.less';
+import '@tdesign/common-style/web/components/chat/_index.less';
+import '@tdesign/common-style/web/components/chat/atom-one-dark.less';
