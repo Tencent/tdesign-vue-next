603af891e584a54613c169376ecf210dab7d25a4
zhangpaopao <zhangpaopao0609@gmail.com>
2025-04-03 14:08:48 +0800

    chore(site): optimize demo-page (#5267)

--- packages/components/guide/_example/popup-dialog.vue
+++ packages/components/guide/_example/popup-dialog.vue
@@ -39,7 +39,7 @@
 
 <script setup>
 import { ref } from 'vue';
-import DialogBody from './dialog-body.vue';
+import DialogBody from './components/dialog-body.vue';
 
 const visible = ref(false);
 const current = ref(-1);
