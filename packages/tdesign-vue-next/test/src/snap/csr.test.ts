8599c48346f62a79d1b107e7b734aea6508e1e3b
zhangpaopao <zhangpaopao0609@gmail.com>
2025-04-09 21:55:11 +0800

    TD Mono:  refactoring the vue-next test (#5263)
    * refactor(test): refactor test js to ts
    
    * test(unit): spec to test
    
    * refactor(test): tEST_TARGET
    
    * fix(test): coverage
    
    * chore: up common
    
    * test: remove getSSRAttach
    
    * chore: update pr-comment-ci
    
    ---------
    
    Co-authored-by: 黎伟杰 <674416404@qq.com>

--- packages/tdesign-vue-next/test/src/snap/csr.test.ts
+++ packages/tdesign-vue-next/test/src/snap/csr.test.ts
@@ -1,28 +1,34 @@
 import { globSync } from 'glob';
 import MockDate from 'mockdate';
 import { vi, describe, it, expect } from 'vitest';
-import { config } from '@vue/test-utils';
+import { mount } from '@vue/test-utils';
 import { resolveComponentsRoot, getRelativeWorkspaceRootPath } from '@tdesign/internal-utils';
 
 // 固定时间，当使用 new Date() 时，返回固定时间，防止“当前时间”的副作用影响，导致 snapshot 变更，mockdate 插件见 https://github.com/boblauer/MockDate
 MockDate.set('2020-12-28 00:00:00');
 
 function runTest() {
-  const files = globSync(resolveComponentsRoot('**/_example/*.vue'), {
+  const files = globSync(resolveComponentsRoot('**!(chat)/_example/*.vue'), {
     ignore: [resolveComponentsRoot('chat/_example/*.vue')],
   });
-  const { createSSRApp } = config.global;
+  const $routerMock = { push: vi.fn() };
 
-  describe('ssr snapshot test', () => {
+  describe('csr snapshot test', () => {
     HTMLCanvasElement.prototype.getContext = vi.fn();
 
     files.forEach((file) => {
-      it(`ssr test ${getRelativeWorkspaceRootPath(file)}`, async () => {
+      it(`csr test ${getRelativeWorkspaceRootPath(file)}`, async () => {
         const demo = await import(file);
         const realDemoComp = demo.default ? demo.default : demo;
-        realDemoComp.name = `test-ssr-${realDemoComp.name}`;
-        const html = await createSSRApp(realDemoComp);
-        expect(html).toMatchSnapshot();
+        realDemoComp.name = `test-csr-${realDemoComp.name}`;
+        const wrapper = mount(realDemoComp, {
+          global: {
+            mocks: {
+              $router: $routerMock,
+            },
+          },
+        });
+        expect(wrapper.element).toMatchSnapshot();
       });
     });
   });
