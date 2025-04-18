ceb4749cc89880dde4690cbb72135fe0661837f1
RSS1102 <jimmyrss1102@gmail.com>
2025-04-18 19:04:48 +0800

    chore: add import `readFileSync`

--- script/workflows/prepare-package.mjs
+++ script/workflows/prepare-package.mjs
@@ -1,6 +1,6 @@
 import { execSync } from 'child_process';
 import { resolve } from 'path';
-import { rmSync, writeFileSync } from 'fs';
+import { rmSync, writeFileSync, readFileSync } from 'fs';
 
 const projectName = 'tdesign-vue-next-demo';
 
