25f59e59809584898ddf32b7b2043ebe1d32efde
阿菜 Cai <jimmyrss1102@gmail.com>
2025-04-24 17:05:34 +0800

    fix(table): select the correct return of the current page data (#5248)
    * fix(table): select the correct return of the currentPageData
    
    * feat: 监听 data 变化避免在远程分页情况下当前页全选按钮失效
    
    * chore: 添加注释说明
    
    * chore: 修改注释说明

--- packages/components/table/hooks/useRowSelect.tsx
+++ packages/components/table/hooks/useRowSelect.tsx
@@ -24,8 +24,15 @@ export default function useRowSelect(
   props: TdPrimaryTableProps,
   tableSelectedClasses: TableClassName['tableSelectedClasses'],
 ) {
-  const { selectedRowKeys, columns, rowKey, data, reserveSelectedRowOnPaginate } = toRefs(props);
-  const currentPaginateData = ref<TableRowData[]>(data.value);
+  const { selectedRowKeys, columns, rowKey, data, reserveSelectedRowOnPaginate, pagination } = toRefs(props);
+  const currentPaginateData = ref<TableRowData[]>(
+    pagination.value
+      ? data.value.slice(
+          (pagination.value.current - 1) * pagination.value.pageSize,
+          pagination.value.current * pagination.value.pageSize,
+        )
+      : data.value,
+  );
   const selectedRowClassNames = ref();
   const [tSelectedRowKeys, setTSelectedRowKeys] = useDefaultValue(
     selectedRowKeys,
@@ -68,10 +75,14 @@ export default function useRowSelect(
       };
       const selectedRowClass = selected.size ? selectedRowClassFunc : undefined;
       selectedRowClassNames.value = [disabledRowClass, selectedRowClass];
-      currentPaginateData.value = data.value;
     },
     { immediate: true },
   );
+
+  // 在远程分页场景下，当前页全选功能的状态判定需基于当前页数据是否存在进行动态重新计算
+  watch(data, () => {
+    currentPaginateData.value = data.value;
+  });
 
   function isDisabled(row: Record<string, any>, rowIndex: number): boolean {
     return isRowSelectedDisabled(selectColumn.value, row, rowIndex);
