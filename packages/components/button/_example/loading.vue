6252d107514ca99c3d120f9266f91f8756262238
黎伟杰 <674416404@qq.com>
2025-05-06 18:09:15 +0800

    ci: auto-changelog

--- packages/components/button/_example/loading.vue
+++ packages/components/button/_example/loading.vue
@@ -1,7 +1,68 @@
 <template>
-  <div class="button-base">
-    <t-button loading> 主按钮 </t-button>
-    <t-button loading variant="outline"> 次按钮 </t-button>
-  </div>
+  <t-space direction="vertical">
+    <t-space direction="vertical" style="padding: 20px">
+      <t-space>
+        <t-button loading theme="default">加载</t-button>
+        <t-button loading variant="outline" theme="default">加载</t-button>
+        <t-button loading variant="dashed" theme="default">加载</t-button>
+        <t-button loading variant="text" theme="default">加载</t-button>
+      </t-space>
+      <t-space>
+        <t-button loading theme="primary">加载</t-button>
+        <t-button loading variant="outline" theme="primary">加载</t-button>
+        <t-button loading variant="dashed" theme="primary">加载</t-button>
+        <t-button loading variant="text" theme="primary">加载</t-button>
+      </t-space>
+      <t-space>
+        <t-button loading theme="danger">加载</t-button>
+        <t-button loading variant="outline" theme="danger">加载</t-button>
+        <t-button loading variant="dashed" theme="danger">加载</t-button>
+        <t-button loading variant="text" theme="danger">加载</t-button>
+      </t-space>
+      <t-space>
+        <t-button loading theme="warning">加载</t-button>
+        <t-button loading variant="outline" theme="warning">加载</t-button>
+        <t-button loading variant="dashed" theme="warning">加载</t-button>
+        <t-button loading variant="text" theme="warning">加载</t-button>
+      </t-space>
+      <t-space>
+        <t-button loading theme="success">加载</t-button>
+        <t-button loading variant="outline" theme="success">加载</t-button>
+        <t-button loading variant="dashed" theme="success">加载</t-button>
+        <t-button loading variant="text" theme="success">加载</t-button>
+      </t-space>
+    </t-space>
+    <t-space direction="vertical" style="background-color: black; padding: 20px">
+      <t-space>
+        <t-button loading ghost theme="default">加载</t-button>
+        <t-button loading ghost variant="outline" theme="default">加载</t-button>
+        <t-button loading ghost variant="dashed" theme="default">加载</t-button>
+        <t-button loading ghost variant="text" theme="default">加载</t-button>
+      </t-space>
+      <t-space>
+        <t-button loading ghost theme="primary">加载</t-button>
+        <t-button loading ghost variant="outline" theme="primary">加载</t-button>
+        <t-button loading ghost variant="dashed" theme="primary">加载</t-button>
+        <t-button loading ghost variant="text" theme="primary">加载</t-button>
+      </t-space>
+      <t-space>
+        <t-button loading ghost theme="danger">加载</t-button>
+        <t-button loading ghost variant="outline" theme="danger">加载</t-button>
+        <t-button loading ghost variant="dashed" theme="danger">加载</t-button>
+        <t-button loading ghost variant="text" theme="danger">加载</t-button>
+      </t-space>
+      <t-space>
+        <t-button loading ghost theme="warning">加载</t-button>
+        <t-button loading ghost variant="outline" theme="warning">加载</t-button>
+        <t-button loading ghost variant="dashed" theme="warning">加载</t-button>
+        <t-button loading ghost variant="text" theme="warning">加载</t-button>
+      </t-space>
+      <t-space>
+        <t-button loading ghost theme="success">加载</t-button>
+        <t-button loading ghost variant="outline" theme="success">加载</t-button>
+        <t-button loading ghost variant="dashed" theme="success">加载</t-button>
+        <t-button loading ghost variant="text" theme="success">加载</t-button>
+      </t-space>
+    </t-space>
+  </t-space>
 </template>
-;
