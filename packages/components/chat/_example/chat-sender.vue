b739ce19bc611bf329b6e34a7da7d60c8b12df48
yeonzhou <yeonzhou@tencent.com>
2025-05-16 14:30:07 +0800

    fix(sender): 示例处理自动清空输入框内容

--- packages/components/chat/_example/chat-sender.vue
+++ packages/components/chat/_example/chat-sender.vue
@@ -9,17 +9,17 @@
     @file-select="fileSelect"
   >
     <!-- 自定义操作区域的内容，默认支持图片上传、附件上传和发送按钮 -->
-    <!-- <template #suffix="{ renderPresets }"> -->
-    <!-- 在这里可以进行自由的组合使用，或者新增预设 -->
-    <!-- 不需要附件操作的使用方式 -->
-    <!-- <component :is="renderPresets([])" /> -->
-    <!-- 只需要附件上传的使用方式-->
-    <!-- <component :is="renderPresets([{ name: 'uploadAttachment' }])" /> -->
-    <!-- 只需要图片上传的使用方式-->
-    <!-- <component :is="renderPresets([{ name: 'uploadImage' }])" /> -->
-    <!-- 任意配置顺序-->
-    <!-- <component :is="renderPresets([{ name: 'uploadAttachment' }, { name: 'uploadImage' }])" /> -->
-    <!-- </template> -->
+    <template #suffix="{ renderPresets }">
+      <!-- 在这里可以进行自由的组合使用，或者新增预设 -->
+      <!-- 不需要附件操作的使用方式 -->
+      <component :is="renderPresets([])" />
+      <!-- 只需要附件上传的使用方式-->
+      <!-- <component :is="renderPresets([{ name: 'uploadAttachment' }])" /> -->
+      <!-- 只需要图片上传的使用方式-->
+      <!-- <component :is="renderPresets([{ name: 'uploadImage' }])" /> -->
+      <!-- 任意配置顺序-->
+      <!-- <component :is="renderPresets([{ name: 'uploadAttachment' }, { name: 'uploadImage' }])" /> -->
+    </template>
   </t-chat-sender>
 </template>
 <script setup lang="ts">
@@ -27,12 +27,12 @@ import { ref } from 'vue';
 const query = ref('');
 const loading = ref(false);
 // 模拟消息发送
-const inputEnter = function (inputValue: string) {
-  console.log('监听发送事件', inputValue);
+const inputEnter = function () {
   if (loading.value) {
     return;
   }
-  if (!inputValue) return;
+  if (!query.value) return;
+  query.value = '';
   loading.value = true;
   setTimeout(() => {
     loading.value = false;
