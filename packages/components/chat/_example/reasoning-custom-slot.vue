9370318a6f5dab43a8c3ecd1c1d84ec516399e88
yeonzhou <yeonzhou@tencent.com>
2025-05-26 19:49:37 +0800

    feat(chat): 封装webc的思考过程组件

--- packages/components/chat/_example/reasoning-custom-slot.vue
+++ packages/components/chat/_example/reasoning-custom-slot.vue
@@ -1,12 +1,18 @@
 <template>
-  <t-chat ref="chatRef" layout="both" style="height: 150px" :clear-history="false" :reverse="false">
-    <t-chat-reasoning v-model:collapsed="collapsed" expand-icon-placement="right" @expand-change="handleChange">
+  <!-- <t-chat ref="chatRef" layout="both" style="height: 150px" :clear-history="false" :reverse="false"> -->
+  <!-- <t-chat-reasoning v-model:collapsed="collapsed" expand-icon-placement="right" @expand-change="handleChange">
       <template #header>
         <t-chat-loading text="思考中..." />
       </template>
       <t-chat-content :content="reasoning" />
-    </t-chat-reasoning>
-  </t-chat>
+    </t-chat-reasoning> -->
+  <t-chat-thinking-content :content="content" status="pending" layout="border">
+    <!-- <template #header>
+      <t-chat-loading text="思考中..." />
+    </template>
+    <t-chat-content :content="reasoning" /> -->
+  </t-chat-thinking-content>
+  <!-- </t-chat> -->
 </template>
 <script setup lang="jsx">
 import { ref } from 'vue';
@@ -23,6 +29,10 @@ const reasoning = `嗯，用户问牛顿第一定律是不是适用于所有参�
 `;
 const handleChange = (value) => {
   console.log(value);
+};
+const content = {
+  title: '思考中',
+  text: '嗯，用户问牛顿第一定律是不是适用于所有参考系。首先，我得先回忆一下牛顿第一定律的内容。牛顿第一定律，也就是惯性定律，说物体在没有外力作用时会保持静止或匀速直线运动。也就是说，保持原来的运动状态。',
 };
 </script>
 <style lang="less" scoped></style>
--- packages/components/chat/index.ts
+++ packages/components/chat/index.ts
@@ -22,12 +22,14 @@ import {
 } from './type';
 
 import './style';
+import '@tencent/tdesign-chatbot/lib/style/index.css';
 import '@tencent/tdesign-chatbot/lib/chat-sender';
 import '@tencent/tdesign-chatbot/lib/attachments';
+import '@tencent/tdesign-chatbot/lib/chat-message/content/thinking-content';
 import { omiVueify } from 'omi-vueify';
 import type { DefineComponent } from 'vue';
-import type { TdAttachmentsProps } from '@tencent/tdesign-chatbot/lib/attachments';
-
+import type { TdAttachmentsProps } from '@tencent/tdesign-chatbot';
+import type { TdChatThinkContentProps } from '@tencent/tdesign-chatbot/lib/chat-message/content/thinking-content';
 export * from './type';
 
 export type ChatProps = TdChatProps;
@@ -47,8 +49,15 @@ export const ChatContent = withInstall(_ChatContent);
 export const ChatReasoning = withInstall(_ChatReasoning);
 export const ChatAction = withInstall(_ChatAction);
 export const ChatLoading = withInstall(_ChatLoading);
-export const Attachments = omiVueify('t-attachments', {
+// 附件
+export const ChatAttachments = omiVueify('t-attachments', {
   methodNames: [],
 }) as DefineComponent<TdAttachmentsProps>;
-export const ChatAttachments = withInstall(Attachments);
+// export const ChatAttachments = withInstall(Attachments);
+// 思考过程
+export const ChatThinking = omiVueify('t-chat-thinking-content', {
+  methodNames: [],
+}) as DefineComponent<TdChatThinkContentProps>;
+// export const ChatThinking = withInstall(ChatThinking);
+
 export default Chat;
