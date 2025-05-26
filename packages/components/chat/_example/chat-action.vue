aa69b4eb99acd0bbbbec948ea00e858b0dabb092
CodeCarfter <43510057+zhangjiaoalice@users.noreply.github.com>
2025-05-26 18:32:16 +0800

    chat-action 组件 isGood/isBad 参数合并为 comment 参数 (#5503)
    * chat-action 组件 isGood/isBad 参数合并为 comment 参数
    
    * feat: 将action-chat组件中 的 operationBtn api 重命名为 actionBar， 将 onOperation api 重命名为 onActions
    
    将action-chat组件中 的 operationBtn api 重命名为 actionBar， 将 onOperation api
    
    BREAKING CHANGE: List any BREAKING CHANGES (optional). Use "
    " to break new line:List any BREAKING CHANGES (optional). Use "
    " to break new line:  将action-chat组件中 的 operationBtn api 重命名为 actionBar， 将 onOperation api
    
    ---------
    
    Co-authored-by: aalicezhang <aalicezhang@tencent.com>

--- packages/components/chat/_example/chat-action.vue
+++ packages/components/chat/_example/chat-action.vue
@@ -1,26 +1,18 @@
 <template>
   <div class="chat-action-content">
     <t-chat-action
-      :is-good="isGood"
-      :is-bad="isBad"
+      :comment="comment"
       content="它叫 McMurdo Station ATM，是美国富国银行安装在南极洲最大科学中心麦克默多站的一台自动提款机。"
-      :operation-btn="['good', 'bad', 'replay', 'copy']"
-      @operation="handleOperation"
+      :action-bar="['good', 'bad', 'replay', 'copy']"
+      @actions="handleActions"
     />
   </div>
 </template>
 <script setup>
 import { ref } from 'vue';
-const isGood = ref(false);
-const isBad = ref(false);
-const handleOperation = (type, options) => {
+const comment = ref('');
+const handleActions = (type, options) => {
   console.log(type, options);
-  if (type === 'good') {
-    isGood.value = !isGood.value;
-    isBad.value = false;
-  } else if (type === 'bad') {
-    isBad.value = !isBad.value;
-    isGood.value = false;
-  }
+  comment.value = type;
 };
 </script>
