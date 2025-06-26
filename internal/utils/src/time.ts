3835a102b61b9b882f2d81fc15bb7e3335ad47fc
Cat1007 <54425790+Cat1007@users.noreply.github.com>
2025-06-26 20:10:41 +0800

    feat(Plugin): Plugin usage supports appContext content (#5559)
    * feat(DialogPlugin): ✨ DialogPlugin 支持 AppContext 的传入
    
    * feat(Drawer): ✨ 插件支持透传context
    
    * feat(Notification): ✨ plugin增加appContext的传入
    
    * feat(Message): ✨ plugin增加appContext的传入
    
    * feat(Loading): ✨ plugin用法支持appContext传入
    
    * chore: remove deprecated content
    
    * chore: remove deprecated content
    
    * chore: fix lint
    
    * chore: fix typo
    
    * chore: fix docs
    
    * chore: stash changelog [ci skip]
    
    ---------
    
    Co-authored-by: Uyarn <uyarnchen@gmail.com>
    Co-authored-by: tdesign-bot <tdesign@tencent.com>

--- internal/utils/src/time.ts
+++ internal/utils/src/time.ts
@@ -3,6 +3,6 @@
  * @param ms 等待的毫秒数
  * @returns 一个在指定时间后解析的 Promise
  */
-export const sleep = (ms: number = 16): Promise<void> => {
+export const sleep = (ms = 16): Promise<void> => {
   return new Promise((resolve) => setTimeout(resolve, ms));
 };
