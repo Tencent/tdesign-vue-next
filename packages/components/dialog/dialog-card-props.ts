af016b57f147ea4e632f2973ee38938fee276476
Uyarn <uyarnchen@gmail.com>
2025-06-26 15:52:22 +0800

    chore: remove deprecated content

--- packages/components/dialog/dialog-card-props.ts
+++ packages/components/dialog/dialog-card-props.ts
@@ -24,14 +24,15 @@ export default {
   /** 确认按钮。值为 null 则不显示确认按钮。值类型为字符串，则表示自定义按钮文本，值类型为 Object 则表示透传 Button 组件属性。使用 TNode 自定义按钮时，需自行控制确认事件 */
   confirmBtn: {
     type: [String, Object, Function] as PropType<TdDialogCardProps['confirmBtn']>,
-    default: true as TdDialogCardProps['confirmBtn'],
   },
   /** 确认按钮加载状态 */
-  confirmLoading: Boolean,
+  confirmLoading: {
+    type: Boolean,
+    default: undefined,
+  },
   /** 底部操作栏，默认会有“确认”和“取消”两个按钮。值为 true 显示默认操作按钮，值为 false 不显示任何内容，值类型为 Function 表示自定义底部内容 */
   footer: {
     type: [Boolean, Function] as PropType<TdDialogCardProps['footer']>,
-    default: true as TdDialogCardProps['footer'],
   },
   /** 头部内容。值为 true 显示空白头部，值为 false 不显示任何内容，值类型为 string 则直接显示值，值类型为 Function 表示自定义头部内容 */
   header: {
