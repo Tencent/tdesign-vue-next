f300b2a713a0338a8635c376fd57cd00758d9eac
Wesley <985189328@qq.com>
2025-07-05 10:16:37 +0800

    test: paragraph

---
+++ packages/components/typography/__tests__/typography-paragraph.test.tsx
@@ -0,0 +1,43 @@
+import { mount } from '@vue/test-utils';
+import { Paragraph } from '@tdesign/components/typography';
+import { nextTick } from 'vue';
+
+describe('Typography Paragraph', () => {
+  const longTextString = `TDesign was founded with the principles of open-source collaboration from the beginning. The collaboration scheme discussion, component design, and API design, including source code, are fully open within the company, garnering widespread attention from internal developers and designers. TDesign follows an equal, open, and strict policy, regardless of the participants' roles.`;
+  const shortText = 'TDesign was founded with the principles of open-source collaboration from the beginning.';
+  const ellipsisText = new RegExp('...');
+
+  describe('props', () => {
+    it(':', () => {
+      const defaultWrapper = mount(() => <Paragraph>{shortText}</Paragraph>);
+      const propWrapper = mount(() => <Paragraph content={shortText}></Paragraph>);
+
+      expect(defaultWrapper.find('.t-typography').element.innerHTML).toMatch(new RegExp(shortText));
+      expect(propWrapper.find('.t-typography').element.innerHTML).toMatch(new RegExp(shortText));
+    });
+
+    it(':ellipsis', () => {
+      const wrapper = mount(() => <Paragraph ellipsis>{longTextString}</Paragraph>);
+
+      expect(wrapper.find('.t-typography').element.innerHTML).toMatch(ellipsisText);
+    });
+
+    it(':ellipsis-expand', async () => {
+      const wrapper = await mount(() => (
+        <Paragraph
+          ellipsis={{
+            expandable: true,
+            collapsible: true,
+          }}
+        >
+          {longTextString}
+        </Paragraph>
+      ));
+
+      // 模拟操作
+      wrapper.find('.t-typography-ellipsis-symbol').trigger('click');
+      await nextTick();
+      expect(wrapper.find('.t-typography-ellipsis-symbol').element.innerHTML).toBe('收起');
+    });
+  });
+});
