a6aefdfa0619125fbbc97ec567cd36e1131bd7e0
zhangpaopao <zhangpaopao0609@gmail.com>
2025-07-26 10:22:53 +0800

    feat: deps path

--- internal/utils/scripts/postinstall-tasks.ts
+++ internal/utils/scripts/postinstall-tasks.ts
@@ -4,12 +4,13 @@ import { joinWorkspaceRoot } from '../src/paths';
 import { run } from '../src/exec';
 
 // 1. generate catalog.ts
+const DEPS_PATH = './internal/utils/src/deps.ts';
 const sourcePath = joinWorkspaceRoot('pnpm-workspace.yaml');
-const destPath = joinWorkspaceRoot('internal/utils/src/deps.ts');
+const destPath = joinWorkspaceRoot(DEPS_PATH);
 const yamlContent = readFileSync(sourcePath, 'utf-8');
 const documents = parse(yamlContent);
 writeFileSync(destPath, `export const deps = ${JSON.stringify(documents.catalogs, null, 2)}`);
-run('npx prettier --write ./internal/utils/src/deps.ts', { cwd: joinWorkspaceRoot() });
+run(`npx prettier --write ${DEPS_PATH}`, { cwd: joinWorkspaceRoot() });
 
 // 2. rollup.config.ts
 run('npx rollup -c rollup.config.ts', { cwd: joinWorkspaceRoot('internal/utils') });
