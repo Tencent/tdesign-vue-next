a7859f0c33cfbbe3d70cfb760e9350c1c80a7979
hozenli <hozen@live.com>
2025-07-26 13:41:21 +0800

    test(unit): improve the test coverage of Avatar
    improve the test coverage of Avatar
    
    closed #5631

--- packages/components/avatar/__tests__/mount.tsx
+++ packages/components/avatar/__tests__/mount.tsx
@@ -3,9 +3,9 @@ import { mount } from '@vue/test-utils';
 import { Avatar, AvatarGroup } from '@tdesign/components';
 
 // Avatar 数量变化会影响测试用例结果
-export function getAvatarGroupDefaultMount(props, events) {
+export function getAvatarGroupDefaultMount(props, events, slots = null) {
   return mount(
-    <AvatarGroup {...props} {...events}>
+    <AvatarGroup {...props} {...events} v-slots={slots}>
       <Avatar image="https://tdesign.gtimg.com/site/avatar.jpg" />
       <Avatar>W</Avatar>
       <Avatar icon={() => <i class="custom-node"></i>} />
