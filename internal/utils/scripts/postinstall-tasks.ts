b4e58e381da525a6518fad241278e24d5a6cad6d
zhangpaopao <zhangpaopao0609@gmail.com>
2025-07-26 09:22:01 +0800

    fix: get deps from pnpm-workspace

---
+++ internal/utils/scripts/postinstall-tasks.ts
@@ -0,0 +1,14 @@
+import { parse } from 'yaml';
+import { readFileSync, writeFileSync } from 'fs';
+import { joinWorkspaceRoot } from '../src/paths';
+import { run } from '../src/exec';
+
+// 1. generate catalog.ts
+const sourcePath = joinWorkspaceRoot('pnpm-workspace.yaml');
+const destPath = joinWorkspaceRoot('internal/utils/src/deps.ts');
+const yamlContent = readFileSync(sourcePath, 'utf-8');
+const documents = parse(yamlContent);
+writeFileSync(destPath, `export const deps = ${JSON.stringify(documents.catalogs, null, 2)}`);
+
+// 2. rollup.config.ts
+run('npx rollup -c rollup.config.ts', { cwd: joinWorkspaceRoot('internal/utils') });
