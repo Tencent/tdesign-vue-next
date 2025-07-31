57330fe869066e085e7527c7a19b27b3211d4b89
黎伟杰 <674416404@qq.com>
2025-07-31 15:52:55 +0800

    docs: update content

--- packages/tdesign-vue-next/site/src/components/newWindow/index.vue
+++ packages/tdesign-vue-next/site/src/components/newWindow/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <t-tooltip content="新窗口打开">
+  <t-tooltip content="在新窗口打开">
     <div class="action-online" @click="onNewWindow">
       <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
         <g id="bpwjump">
