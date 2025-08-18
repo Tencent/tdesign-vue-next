d831cbbbe979291ae11f3229af8ae97b4d4c97a8
RSS1102 <jimmyrss1102@gmail.com>
2025-08-18 18:38:22 +0800

    fix(table): 优化滚动到顶部的逻辑以提高兼容性

--- packages/components/table/hooks/usePagination.tsx
+++ packages/components/table/hooks/usePagination.tsx
@@ -75,12 +75,13 @@ export default function usePagination(
             props.onPageChange?.(pageInfo, dataSource.value);
 
             // 当切换分页时，内容区域滚动到顶部
-            if (tableContentRef.value.scrollTo) {
-              tableContentRef.value.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
+            const ref = tableContentRef.value;
+            if (ref.scrollTo) {
+              ref.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
             } else {
               // 兼容测试环境或旧浏览器
-              tableContentRef.value.scrollTop = 0;
-              tableContentRef.value.scrollLeft = 0;
+              ref.scrollTop = 0;
+              ref.scrollLeft = 0;
             }
           }}
           v-slots={{ totalContent: context.slots.totalContent }}
