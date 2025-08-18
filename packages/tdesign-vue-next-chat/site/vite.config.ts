e19a36488de6099e2afb91ac460eec9123362f95
Uyarn <uyarnchen@gmail.com>
2025-08-18 20:58:26 +0800

    chore: fix build

--- packages/tdesign-vue-next-chat/site/vite.config.ts
+++ packages/tdesign-vue-next-chat/site/vite.config.ts
@@ -2,7 +2,12 @@ import { defineConfig, searchForWorkspaceRoot } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 import tDocPlugin from './plugin-doc';
-import { joinPosix, joinComponentsRoot, joinTdesignVueNextRoot } from '@tdesign/internal-utils';
+import {
+  joinPosix,
+  joinComponentsRoot,
+  joinTdesignVueNextRoot,
+  getProComponentsChatRoot,
+} from '@tdesign/internal-utils';
 
 const publicPathMap: Record<string, string> = {
   preview: '/',
@@ -18,6 +23,7 @@ export default defineConfig(({ mode }) => {
         '@': joinPosix(__dirname, './'),
         '@tdesign/vue-next': joinTdesignVueNextRoot(),
         '@tdesign/components': joinComponentsRoot(),
+        '@tdesign-vue-next/chat': getProComponentsChatRoot(),
         // TODO: paopao 为什么还需要 alias，因为在 example 中的写法只能是 tdesign-vue-next，虽然有这个子应用，但没有 build 是没用的，同时即便是 prebuild 了，hmr 也是问题
         'tdesign-vue-next/es': joinComponentsRoot(),
         'tdesign-vue-next': joinComponentsRoot(),
