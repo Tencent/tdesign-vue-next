2a751eb3b2e43ea314a03966c35cdaba6eab28e7
Rylan <rylanbot@qq.com>
2025-08-20 21:05:13 +0800

    fix(Tree): add disabled state check for draggable nodes

--- packages/components/tree/hooks/useTreeItem.tsx
+++ packages/components/tree/hooks/useTreeItem.tsx
@@ -143,7 +143,7 @@ export default function useTreeItem(state: TypeTreeItemState) {
         data-level={level}
         style={styles}
         onClick={(evt: MouseEvent) => handleClick(evt)}
-        draggable={node.isDraggable()}
+        draggable={!node.isDisabled() && node.isDraggable()}
         onDragstart={(evt: DragEvent) => handleDragStart(evt)}
         onDragend={(evt: DragEvent) => handleDragEnd(evt)}
         onDragover={(evt: DragEvent) => handleDragOver(evt)}
