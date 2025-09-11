f9ab3bbb0d652f396398c133bbc34024ba315944
Wesley <985189328@qq.com>
2025-09-11 12:55:14 +0800

    chore: test

--- packages/components/watermark/_example/layout.vue
+++ packages/components/watermark/_example/layout.vue
@@ -66,24 +66,6 @@
     <div style="height: 300px" />
   </t-watermark>
 
-  多行水印-奇怪用法
-  <t-watermark
-    :watermark-content="[
-      { text: '水印文本', fontColor: '#0052D9' },
-      { url: 'https://tdesign.gtimg.com/starter/brand-logo-light.png' },
-      { text: '水印文本', fontColor: '#0052D9' },
-      { url: 'https://tdesign.gtimg.com/starter/brand-logo-light.png' },
-    ]"
-    :line-space="24"
-    :x="100"
-    :y="120"
-    :width="158"
-    :height="22"
-    layout="hexagonal"
-  >
-    <div style="height: 300px" />
-  </t-watermark>
-
   多行灰度水印
   <t-watermark
     :watermark-content="[
