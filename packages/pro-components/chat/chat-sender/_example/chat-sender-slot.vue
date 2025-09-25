f2711f0c63134a75f52ab76bfeedd3ff61719713
zhangjiaoalice <2640847812@qq.com>
2025-09-25 17:04:27 +0800

    style: 输入框自定义样式调整

--- packages/pro-components/chat/chat-sender/_example/chat-sender-slot.vue
+++ packages/pro-components/chat/chat-sender/_example/chat-sender-slot.vue
@@ -30,7 +30,13 @@
             @focus="allowToolTip = false"
           ></t-select>
         </t-tooltip>
-        <t-button class="check-box" :class="{ 'is-active': isChecked }" variant="text" @click="checkClick">
+        <t-button
+          class="check-box"
+          theme="default"
+          :class="{ 'is-active': isChecked }"
+          variant="outline"
+          @click="checkClick"
+        >
           <SystemSumIcon />
           <span>深度思考</span>
         </t-button>
@@ -155,8 +161,8 @@ const switchScene = (data: any) => {
       width: 112px;
       height: var(--td-comp-size-m);
       border-radius: 32px;
-      border: 0;
-      background: var(--td-bg-color-component);
+      // border: 0;
+      // background: var(--td-bg-color-component);
       color: var(--td-text-color-primary);
       box-sizing: border-box;
       flex: 0 0 auto;
