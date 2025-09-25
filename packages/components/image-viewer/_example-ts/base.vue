a8ce5571289693356d87769faadfd410d64418d8
Uyarn <uyarnchen@gmail.com>
2025-09-25 15:50:00 +0800

    chore: update demo

--- packages/components/image-viewer/_example-ts/base.vue
+++ packages/components/image-viewer/_example-ts/base.vue
@@ -1,21 +1,11 @@
 <template>
   <div>
     <div class="tdesign-demo-image-viewer__base">
-      <t-image-viewer :images="[img]">
-        <template #trigger="{ open }">
-          <div class="tdesign-demo-image-viewer__ui-image">
-            <img alt="test" :src="img" class="tdesign-demo-image-viewer__ui-image--img" />
-            <div class="tdesign-demo-image-viewer__ui-image--hover" @click="open">
-              <span><BrowseIcon size="1.4em" /> 预览</span>
-            </div>
-          </div>
-        </template>
-      </t-image-viewer>
+      <t-image-viewer :images="[img]"></t-image-viewer>
     </div>
   </div>
 </template>
 <script lang="ts" setup>
-import { BrowseIcon } from 'tdesign-icons-vue-next';
 const img = 'https://tdesign.gtimg.com/demo/demo-image-1.png';
 </script>
 <style scoped>
