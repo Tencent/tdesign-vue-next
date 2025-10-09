fd8022575d768ff44bf163eec1c37605b111809d
zydemail <1054055589@qq.com>
2025-10-09 10:41:37 +0800

    feat: 为聊天消息项新增status属性显示加载状态

--- packages/pro-components/chat/chat/chat.tsx
+++ packages/pro-components/chat/chat/chat.tsx
@@ -100,6 +100,7 @@ export default defineComponent({
             avatar={item.avatar}
             name={item.name}
             role={item.role}
+            status={props.textLoading && (props.reverse ? index === 0 : index === data.length - 1) ? 'pending' : ''}
             content={item.content}
             datetime={item.datetime}
             animation={props.animation}
