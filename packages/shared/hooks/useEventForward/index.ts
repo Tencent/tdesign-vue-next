2eb383bf9f9ab33e8cd479ab3703a0a455fa88de
Wesley <985189328@qq.com>
2025-10-13 13:10:03 +0800

    chore: hooks

--- packages/shared/hooks/useEventForward/index.ts
+++ packages/shared/hooks/useEventForward/index.ts
@@ -4,7 +4,7 @@ import { computed, ComputedRef } from 'vue';
  * 合并组件 props 中的透传事件与组件的回调
  * 先执行封装组件回调，再执行用户回调
  *
- * @param props 用户传入的 props
+ * @param componentsProps 用户传入的 componentsProps
  * @param internalHandlers 组件使用的回调
  * @returns 合并后的 props
  *
@@ -13,21 +13,21 @@ import { computed, ComputedRef } from 'vue';
  *   onChange: () => innerChange(),
  *   onClick: () => innerClick(),
  * };
- * const events = useEventForward(props, internalHandlers);
+ * const events = useEventForward(componentsProps, internalHandlers);
  *
  * <Component {...events.value} />
  */
 export function useEventForward<T extends Record<string, any>>(
-  props: T,
+  componentsProps: T,
   internalHandlers?: Partial<{
     [K in keyof T]: T[K] extends (...args: any[]) => void ? T[K] : never;
   }>,
-): ComputedRef {
+): ComputedRef<T> {
   return computed(() => {
-    const merged = { ...props };
+    const merged = { ...componentsProps };
 
-    (Object.keys(internalHandlers) as Array<keyof T>).forEach((key) => {
-      const userHandler = props?.[key];
+    (Object.keys(internalHandlers ?? {}) as Array<keyof T>).forEach((key) => {
+      const userHandler = componentsProps?.[key];
       const internalHandler = internalHandlers?.[key];
 
       if (typeof userHandler === 'function' && typeof internalHandler === 'function') {
@@ -47,7 +47,7 @@ export function useEventForward<T extends Record<string, any>>(
         // 用户无回调，只用组件回调
         merged[key] = internalHandler;
       }
-      // 封装组件无回调，保留用户的回调方法 或 保留不是方法的参数
+      // 封装组件无回调(或保留不是方法的参数)，保留用户传入内容
     });
 
     return merged;
