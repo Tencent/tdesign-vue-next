cb2775f677a5dd64e714d0542bc17f187ac861c0
阿菜 Cai <jimmyrss1102@gmail.com>
2025-10-30 20:56:02 +0800

    fix(table): ensure `defaultPageSize` is used when `pageSize` not provided (#6101)
    * fix(pagination): ensure default page size is used when pageSize is not provided
    
    * fix(pagination): reset current page to 1 when total page count changes
    
    * fix(pagination): remove redundant reset of current page when total page count changes
    
    * chore: stash changelog [ci skip]
    
    ---------
    
    Co-authored-by: tdesign-bot <tdesign@tencent.com>

--- packages/components/pagination/pagination.tsx
+++ packages/components/pagination/pagination.tsx
@@ -140,13 +140,6 @@ export default defineComponent({
       return array;
     });
 
-    // 如果页面总数发生变化并当前页数大于总页数则重置为1
-    watch(
-      () => pageCount.value,
-      () => {
-        if (innerCurrent.value > pageCount.value) innerCurrent.value = 1;
-      },
-    );
     watch(
       () => innerCurrent.value,
       (val) => (jumpIndex.value = val),
