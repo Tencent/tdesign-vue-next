5a069bd814c1b9ade27100d81b5894e43dfa0486
HaixingOoO <974758671@qq.com>
2025-11-02 20:39:24 +0800

    chore(cascader): sync example

--- packages/components/cascader/_example-ts/virtual-scroll.vue
+++ packages/components/cascader/_example-ts/virtual-scroll.vue
@@ -13,33 +13,42 @@
 import { ref } from 'vue';
 
 const options = [];
-for (let i = 1; i < 100; i++) {
-  const children = [];
-  for (let j = 1; j < 100; j++) {
-    const child = [];
-    for (let k = 1; k < 100; k++) {
-      child.push({
-        label: `子选项${i}.${j}.${k}`,
-        value: `${i}.${j}.${k}`,
-      });
-    }
-    children.push({
-      label: `子选项${i}.${j}`,
-      value: `${i}.${j}`,
-      children: child,
-    });
-  }
-
-  options.push({
-    label: `选项${i}`,
-    value: `${i}`,
-    children,
-  });
-}
-
-const value = ref('1.1');
+const value = ref(['20.1.20']);
 
 const onChange = (val, context) => {
   console.log(val, context);
 };
+
+function initOptions() {
+  const list = [];
+  for (let i = 1; i < 100; i++) {
+    const children = [];
+    for (let j = 1; j < 100; j++) {
+      const child = [];
+      for (let k = 1; k < 100; k++) {
+        child.push({
+          label: `子选项${i}.${j}.${k}`,
+          value: `${i}.${j}.${k}`,
+        });
+      }
+      children.push({
+        label: `子选项${i}.${j}`,
+        value: `${i}.${j}`,
+        children: child,
+      });
+    }
+
+    list.push({
+      label: `选项${i}`,
+      value: `${i}`,
+      children,
+    });
+  }
+  return list;
+}
+
+onMounted(() => {
+  const data = initOptions();
+  options.value = data;
+});
 </script>
