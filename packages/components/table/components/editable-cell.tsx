831b4d15571b17f3c90dc56ab01c1691d4b2d518
Uyarn <uyarnchen@gmail.com>
2025-11-24 12:15:44 +0800

    fix: fix edit validator abort in keep edit mode

--- packages/components/table/components/editable-cell.tsx
+++ packages/components/table/components/editable-cell.tsx
@@ -186,8 +186,10 @@ export default defineComponent({
           trigger,
         };
         const rules = isFunction(col.value.edit.rules) ? col.value.edit.rules(cellParams.value) : col.value.edit.rules;
+
         if (!col.value.edit || !rules || !rules.length) {
           props.onValidate?.(params);
+
           resolve(true);
           return;
         }
@@ -200,6 +202,7 @@ export default defineComponent({
             resolve(true);
           } else {
             errorList.value = list;
+
             resolve(list);
           }
         });
@@ -222,19 +225,17 @@ export default defineComponent({
           editValue.value = oldValue;
           outsideAbortEvent?.(...args);
         }
+        if (isKeepEditMode.value) return;
         editOnListeners.value[eventName]?.(args[2]);
         // 此处必须在事件执行完成后异步销毁编辑组件，否则会导致事件清除不及时引起的其他问题
         const timer = setTimeout(() => {
-          if (!isKeepEditMode.value) {
-            isEdit.value = false;
-          }
           errorList.value = [];
           props.onEditableChange?.({
             ...cellParams.value,
             value: editValue.value,
             editedRow: { ...props.row, [props.col.colKey]: editValue.value },
             validateEdit,
-            isEdit: isEdit.value,
+            isEdit: false,
           });
           clearTimeout(timer);
         }, 0);
@@ -307,6 +308,7 @@ export default defineComponent({
       const node = path.find((node: HTMLElement) => node.classList?.contains(`${classPrefix.value}-popup__content`));
       if (node) return;
       const outsideAbortEvent = col.value.edit.onEdited;
+
       updateAndSaveAbort(outsideAbortEvent, '', {
         ...cellParams.value,
         trigger: 'document',
