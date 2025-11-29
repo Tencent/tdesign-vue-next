fb02c3b5c021b47b5fd4a851dbb0cecf5edb46da
树林 <3335405397@qq.com>
2025-11-29 11:41:15 +0800

    docs(calendar): 新增单独设置日历范围上限或下限的示例

--- packages/components/calendar/_example-ts/range.vue
+++ packages/components/calendar/_example-ts/range.vue
@@ -1,17 +1,39 @@
 <template>
   <t-space direction="vertical">
+    <h3>仅指定范围上限</h3>
     <t-form layout="inline">
-      <t-form-item label="日历可选范围：">
-        <t-date-range-picker v-model="range" mode="month" :disable-date="{ before: '1970-01' }" allow-input />
+      <t-form-item label="日历最大可选：">
+        <t-date-picker v-model="maxDate" mode="month" :disable-date="{ before: '1970-01' }" allow-input />
       </t-form-item>
     </t-form>
-    <t-calendar :range="range" />
+    <t-calendar :range="range1" />
+    <br />
+    <h3>仅指定范围下限</h3>
+    <t-form layout="inline">
+      <t-form-item label="日历最小可选：">
+        <t-date-picker v-model="minDate" mode="month" :disable-date="{ before: '1970-01' }" allow-input />
+      </t-form-item>
+    </t-form>
+    <t-calendar :range="range2" />
+    <br />
+    <h3>指定范围上限和下限</h3>
+    <t-form layout="inline">
+      <t-form-item label="日历可选范围：">
+        <t-date-range-picker v-model="range3" mode="month" :disable-date="{ before: '1970-01' }" allow-input />
+      </t-form-item>
+    </t-form>
+    <t-calendar :range="range3" />
   </t-space>
 </template>
 
 <script lang="tsx" setup>
 import { ref } from 'vue';
-import { CalendarProps } from 'tdesign-vue-next';
+import { CalendarProps, CalendarValue } from 'tdesign-vue-next';
 
-const range = ref<CalendarProps['range']>(['2018-08', '2028-04']);
+const maxDate = ref<CalendarValue>('2028-04');
+const minDate = ref<CalendarValue>('2018-08');
+
+const range1 = ref<CalendarProps['range']>(['', maxDate.value]);
+const range2 = ref<CalendarProps['range']>([minDate.value, '']);
+const range3 = ref<CalendarProps['range']>(['2018-08', '2028-04']);
 </script>
--- packages/components/calendar/_example/range.vue
+++ packages/components/calendar/_example/range.vue
@@ -1,16 +1,38 @@
 <template>
   <t-space direction="vertical">
+    <h3>仅指定范围上限</h3>
     <t-form layout="inline">
-      <t-form-item label="日历可选范围：">
-        <t-date-range-picker v-model="range" mode="month" :disable-date="{ before: '1970-01' }" allow-input />
+      <t-form-item label="日历最大可选：">
+        <t-date-picker v-model="maxDate" mode="month" :disable-date="{ before: '1970-01' }" allow-input />
       </t-form-item>
     </t-form>
-    <t-calendar :range="range" />
+    <t-calendar :range="range1" />
+    <br />
+    <h3>仅指定范围下限</h3>
+    <t-form layout="inline">
+      <t-form-item label="日历最小可选：">
+        <t-date-picker v-model="minDate" mode="month" :disable-date="{ before: '1970-01' }" allow-input />
+      </t-form-item>
+    </t-form>
+    <t-calendar :range="range2" />
+    <br />
+    <h3>指定范围上限和下限</h3>
+    <t-form layout="inline">
+      <t-form-item label="日历可选范围：">
+        <t-date-range-picker v-model="range3" mode="month" :disable-date="{ before: '1970-01' }" allow-input />
+      </t-form-item>
+    </t-form>
+    <t-calendar :range="range3" />
   </t-space>
 </template>
 
 <script setup>
 import { ref } from 'vue';
 
-const range = ref(['2018-08', '2028-04']);
+const maxDate = ref('2028-04');
+const minDate = ref('2018-08');
+
+const range1 = ref(['', maxDate.value]);
+const range2 = ref([minDate.value, '']);
+const range3 = ref(['2018-08', '2028-04']);
 </script>
