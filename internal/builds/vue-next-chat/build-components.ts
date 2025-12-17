edc1c853c930e0bcc981840f744e00a7d918a365
lwj <674416404@qq.com>
2025-12-17 20:23:39 +0800

    chore: optimize ignore path

--- internal/builds/vue-next-chat/build-components.ts
+++ internal/builds/vue-next-chat/build-components.ts
@@ -115,10 +115,7 @@ const getPlugins = ({
       copy({
         targets: [
           {
-            src: [
-              joinProComponentsChatRoot('**/style/css.js'),
-              `!${joinProComponentsChatRoot('**/node_modules/**/style/css.js')}`,
-            ],
+            src: [joinProComponentsChatRoot('**/style/css.js'), `!${joinProComponentsChatRoot('**/node_modules')}`],
             dest: joinTdesignVueNextChatRoot('es'),
             rename: (name, extension, fullPath) =>
               `${fullPath.replace(joinProComponentsChatRoot(), '').slice(0, -6)}${name}.mjs`,
