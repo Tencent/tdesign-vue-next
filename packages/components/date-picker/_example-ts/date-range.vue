b9631a09894459436bd8508e15239cdec29eb4e1
Uyarn <uyarnchen@gmail.com>
2026-01-05 16:24:29 +0800

    chore: revert demo ui change

--- packages/components/date-picker/_example-ts/date-range.vue
+++ packages/components/date-picker/_example-ts/date-range.vue
@@ -1,16 +1,13 @@
 <template>
   <t-space direction="vertical">
-    <div>
-      <p v-if="eventLog" style="color: #0052d9; margin-bottom: 8px">{{ eventLog }}</p>
-      <t-date-range-picker
-        allow-input
-        clearable
-        @pick="onPick"
-        @change="onChange"
-        @month-change="onMonthChange"
-        @year-change="onYearChange"
-      />
-    </div>
+    <t-date-range-picker
+      allow-input
+      clearable
+      @pick="onPick"
+      @change="onChange"
+      @month-change="onMonthChange"
+      @year-change="onYearChange"
+    />
     <t-date-range-picker
       enable-time-picker
       allow-input
@@ -24,10 +21,7 @@
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue';
 import { DateRangePickerProps } from 'tdesign-vue-next';
-
-const eventLog = ref('');
 
 const onPick: DateRangePickerProps['onPick'] = (value, context) => console.log('onPick:', value, context);
 
@@ -41,16 +35,13 @@ const onChange: DateRangePickerProps['onChange'] = (value, context) => {
     'YYYYMMDD:',
     context.dayjsValue.map((d) => d.format('YYYYMMDD')),
   );
-  eventLog.value = `onChange: ${value}`;
 };
 
 const onMonthChange: DateRangePickerProps['onMonthChange'] = (context) => {
   console.log('onMonthChange:', context);
-  eventLog.value = `月份切换: ${context.month}月 (触发方式: ${context.trigger}, 面板: ${context.partial})`;
 };
 
 const onYearChange: DateRangePickerProps['onYearChange'] = (context) => {
   console.log('onYearChange:', context);
-  eventLog.value = `年份切换: ${context.year}年 (触发方式: ${context.trigger}, 面板: ${context.partial})`;
 };
 </script>
