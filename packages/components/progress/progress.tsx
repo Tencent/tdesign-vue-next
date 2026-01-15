6daa16dfb7a2d6138e717ada31d4b50a84d746a3
黎伟杰 <674416404@qq.com>
2026-01-15 14:52:14 +0800

    chore: 迁移使用common仓库的定义

--- packages/components/progress/progress.tsx
+++ packages/components/progress/progress.tsx
@@ -9,7 +9,13 @@ import {
 } from 'tdesign-icons-vue-next';
 
 import { getBackgroundColor } from '@tdesign/common-js/utils/helper';
-import { PRO_THEME, CIRCLE_SIZE, CIRCLE_SIZE_PX, STATUS_ICON, CIRCLE_FONT_SIZE_RATIO } from './consts';
+import {
+  PRO_THEME,
+  CIRCLE_SIZE,
+  CIRCLE_SIZE_PX,
+  STATUS_ICON,
+  CIRCLE_FONT_SIZE_RATIO,
+} from '@tdesign/common-js/progress/const';
 import props from './props';
 
 import { useTNodeJSX, useGlobalIcon, usePrefixClass, useResizeObserver } from '@tdesign/shared-hooks';
