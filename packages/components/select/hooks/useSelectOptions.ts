26479ab99910db9ba92ed4dea0ee38462cb2c594
yuyuanfei <3334355151@qq.com>
2026-03-25 16:54:54 +0800

    fix(component): [t-select] t-select update the option data to display text echo issues

--- packages/components/select/hooks/useSelectOptions.ts
+++ packages/components/select/hooks/useSelectOptions.ts
@@ -130,7 +130,7 @@ export const useSelectOptions = (
    */
   const searchDisplayOptions = computed(() => {
     const currentSelectedOptions = getSelectedOptions(optionsList.value, innerValue.value);
-    searchOptions.value = uniqBy([...searchOptions.value, ...currentSelectedOptions], 'value');
+    searchOptions.value = uniqBy([...currentSelectedOptions, ...searchOptions.value], 'value');
     const searchSelectedOptions = getSelectedOptions(searchOptions.value, innerValue.value);
 
     return uniqBy([...searchSelectedOptions, ...optionsList.value], 'value');
