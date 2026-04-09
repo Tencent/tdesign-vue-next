a8d67b81969b3e8e21174daa776346552ced36fb
Uyarn <uyarnchen@gmail.com>
2026-04-09 19:56:19 +0800

    chore: optmize

--- packages/components/menu/_example/head-menu-ellipsis.vue
+++ packages/components/menu/_example/head-menu-ellipsis.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="box">
+  <div class="box" style="max-width: 800px">
     <t-head-menu default-value="2-1" theme="dark" expand-type="popup" style="margin-top: 24px">
       <template #logo>
         <img height="28" src="https://tdesign.gtimg.com/site/baseLogo-dark.png" alt="logo" />
@@ -8,53 +8,12 @@
       <t-menu-item value="2-1"> 子菜单2-1 </t-menu-item>
       <t-menu-item value="3-1"> 子菜单3-1 </t-menu-item>
       <t-menu-item value="4-1"> 子菜单4-1 </t-menu-item>
-      <t-menu-item value="5-1"> 子菜单5-1这个菜单有点长 </t-menu-item>
+      <t-menu-item value="5-1"> 子菜单5-1 </t-menu-item>
       <t-menu-item value="6-1"> 子菜单6-1 </t-menu-item>
       <t-menu-item value="7-1"> 子菜单7-1 </t-menu-item>
       <t-menu-item value="8-1"> 子菜单8-1 </t-menu-item>
       <t-menu-item value="9-1"> 子菜单9-1 </t-menu-item>
       <t-menu-item value="10-1"> 子菜单10-1 </t-menu-item>
-      <t-menu-item value="11-1"> 子菜单11-1 </t-menu-item>
-      <t-menu-item value="12-1"> 子菜单12-1 </t-menu-item>
-      <t-menu-item value="13-1"> 子菜单13-1 </t-menu-item>
-      <t-menu-item value="14-1"> 子菜单14-1 </t-menu-item>
-      <t-menu-item value="15-1"> 子菜单15-1 </t-menu-item>
-      <t-menu-item value="16-1"> 子菜单16-1 </t-menu-item>
-      <t-menu-item value="17-1"> 子菜单17-1 </t-menu-item>
-      <t-menu-item value="18-1"> 子菜单18-1 </t-menu-item>
-      <t-menu-item value="19-1"> 子菜单19-1 </t-menu-item>
-      <t-menu-item value="20-1"> 子菜单20-1 </t-menu-item>
-      <t-menu-item value="21-1"> 子菜单21-1 </t-menu-item>
-      <t-submenu value="22">
-        <template #title>
-          <span>菜单1</span>
-        </template>
-        <t-submenu value="22-1" title="子菜单22-1">
-          <t-menu-item value="22-1-1"> 子菜单22-1-1 </t-menu-item>
-          <t-menu-item value="22-1-3"> 子菜单22-1-3 </t-menu-item>
-          <t-menu-item value="22-1-2"> 子菜单22-1-2 </t-menu-item>
-          <t-menu-item value="22-1-4"> 子菜单22-1-4 </t-menu-item>
-          <t-menu-item value="22-1-5"> 子菜单22-1-5 </t-menu-item>
-          <t-menu-item value="22-1-6"> 子菜单22-1-6 </t-menu-item>
-          <t-menu-item value="22-1-7"> 子菜单22-1-7 </t-menu-item>
-          <t-menu-item value="22-1-8"> 子菜单22-1-8 </t-menu-item>
-          <t-menu-item value="22-1-9"> 子菜单22-1-9 </t-menu-item>
-          <t-submenu value="22-1-10" title="子菜单22-1-10">
-            <t-menu-item value="22-1-10-1"> 子菜单22-1-10-1 </t-menu-item>
-            <t-menu-item value="22-1-10-3"> 子菜单22-1-10-3 </t-menu-item>
-            <t-menu-item value="22-1-10-2"> 子菜单22-1-10-2 </t-menu-item>
-            <t-menu-item value="22-1-10-4"> 子菜单22-1-10-4 </t-menu-item>
-            <t-menu-item value="22-1-10-5"> 子菜单22-1-10-5 </t-menu-item>
-            <t-menu-item value="22-1-10-6"> 子菜单22-1-10-6 </t-menu-item>
-            <t-menu-item value="22-1-10-7"> 子菜单22-1-10-7 </t-menu-item>
-            <t-menu-item value="22-1-10-8"> 子菜单22-1-10-8 </t-menu-item>
-            <t-menu-item value="22-1-10-9"> 子菜单22-1-10-9 </t-menu-item>
-          </t-submenu>
-        </t-submenu>
-        <t-menu-item value="23-1"> 子菜单23-1 </t-menu-item>
-        <t-menu-item value="24-2"> 子菜单24-2 </t-menu-item>
-        <t-menu-item value="25-3"> 子菜单25-3 </t-menu-item>
-      </t-submenu>
       <t-submenu
         value="26"
         :popup-props="{
@@ -62,7 +21,7 @@
         }"
       >
         <template #title>
-          <span>菜单2</span>
+          <span>菜单11-1</span>
         </template>
         <t-menu-item value="26-1"> 子菜单2-1 </t-menu-item>
         <t-menu-item value="26-2"> 子菜单2-2 </t-menu-item>
