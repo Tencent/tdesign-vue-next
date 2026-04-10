c1c4a7470f1ab6dc1b741db9098f888d1db749b4
Wesley <985189328@qq.com>
2026-04-10 18:47:40 +0800

    feat(head-menu): support auto ellipsis when menu items are overflow (#6530)
    * feat: menu ellipsis
    
    * feat: common & demo
    
    * chore: optimize
    
    * chore: lint
    
    * chore: update common
    
    * chore: optmize
    
    * chore: optimize
    
    * chore: optimize
    
    * chore: stash changelog [ci skip]
    
    * chore: optimize
    
    * Revert "chore: optimize"
    
    This reverts commit 474ca2d8800e5425d16a619d1c231f1150b8c90f.
    
    * chore: optimize
    
    * chore: update snapshot
    
    * chore: fix active status
    
    ---------
    
    Co-authored-by: Uyarn <uyarnchen@gmail.com>
    Co-authored-by: tdesign-bot <tdesign@tencent.com>

---
+++ packages/components/menu/_example/head-menu-ellipsis.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="box" style="max-width: 500px">
+    <t-head-menu default-value="2" theme="dark" expand-type="popup" style="margin-top: 24px">
+      <template #logo>
+        <img height="28" src="https://tdesign.gtimg.com/site/baseLogo-dark.png" alt="logo" />
+      </template>
+      <t-menu-item value="1"> 菜单1 </t-menu-item>
+      <t-menu-item value="2"> 菜单2 </t-menu-item>
+      <t-menu-item value="3"> 菜单3 </t-menu-item>
+      <t-menu-item value="4"> 菜单4 </t-menu-item>
+      <t-menu-item value="5"> 菜单5 </t-menu-item>
+      <t-menu-item value="6"> 菜单6 </t-menu-item>
+      <t-menu-item value="7"> 菜单7 </t-menu-item>
+      <t-menu-item value="8"> 菜单8 </t-menu-item>
+      <t-menu-item value="9"> 菜单9 </t-menu-item>
+      <t-menu-item value="10"> 菜单10 </t-menu-item>
+      <t-submenu
+        value="26"
+        :popup-props="{
+          overlayClassName: 'custom-popup-class',
+        }"
+      >
+        <template #title>
+          <span>菜单11</span>
+        </template>
+        <t-menu-item value="26"> 子菜单11-1 </t-menu-item>
+        <t-menu-item value="26-2"> 子菜单11-2 </t-menu-item>
+        <t-menu-item value="26-3"> 子菜单11-3 </t-menu-item>
+      </t-submenu>
+      <template #operations>
+        <div class="t-demo-menu--dark">
+          <t-button variant="text" shape="square">
+            <template #icon><t-icon name="search" /></template>
+          </t-button>
+          <t-button variant="text" shape="square">
+            <template #icon><t-icon name="mail" /></template>
+          </t-button>
+          <t-button variant="text" shape="square">
+            <template #icon><t-icon name="user" /></template>
+          </t-button>
+          <t-button variant="text" shape="square">
+            <template #icon><t-icon name="ellipsis" /></template>
+          </t-button>
+        </div>
+      </template>
+    </t-head-menu>
+  </div>
+</template>
+<style lang="less" scoped>
+.t-menu__operations {
+  .t-button {
+    margin-left: 8px;
+  }
+}
+.t-demo-menu--dark {
+  .t-button {
+    color: #fff;
+    &:hover {
+      background-color: #4b4b4b;
+      border-color: transparent;
+      --ripple-color: #383838;
+    }
+  }
+}
+</style>
