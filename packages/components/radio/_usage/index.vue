23d913262a87e97e7e91ee9274b967655dd2b2de
lwj <674416404@qq.com>
2026-04-15 22:57:58 +0800

    chore: update live demo

--- packages/components/radio/_usage/index.vue
+++ packages/components/radio/_usage/index.vue
@@ -2,21 +2,33 @@
 <template>
   <base-usage :code="usageCode" :config-list="configList" :panel-list="panelList" @panel-change="onPanelChange">
     <template #radio="{ configProps }"><t-radio v-bind="configProps">单选框</t-radio></template>
+    <template #radioGroup="{ configProps }">
+      <t-radio-group v-bind="configProps" :options="radioOptions" />
+    </template>
   </base-usage>
 </template>
 
 <script setup lang="jsx">
 /* eslint-disable */
 import { ref, onMounted } from 'vue';
-import configJson from './props.json';
+import radioConfigJson from './radio-props.json';
+import radioGroupConfigJson from './radio-group-props.json';
 
-const configList = ref(configJson);
-const panelList = [{ label: 'radio', value: 'radio' }];
+const configList = ref(radioConfigJson);
+const panelList = [
+  { label: 'radio', value: 'radio', config: radioConfigJson },
+  { label: 'radioGroup', value: 'radioGroup', config: radioGroupConfigJson },
+];
+const radioOptions = ['选项一', '选项二', '选项三', '选项四'];
 
-const usageCodeMap = { radio: '<t-radio v-bind="configProps">单选框</t-radio>' };
+const usageCodeMap = {
+  radio: '<t-radio v-bind="configProps">单选框</t-radio>',
+  radioGroup: '<t-radio-group v-bind="configProps" :options="radioOptions" />',
+};
 const usageCode = ref(`<template>${usageCodeMap[panelList[0].value].trim()}</template>`);
 
 function onPanelChange(panel) {
+  configList.value = panelList.find((item) => item.value === panel).config;
   usageCode.value = `<template>${usageCodeMap[panel].trim()}</template>`;
 }
 </script>
