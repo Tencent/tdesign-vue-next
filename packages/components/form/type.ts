867724a9a2ddd04b9c627eb4f849b7bec8a5c4ee
ruguoba <xx732395@gmail.com>
2026-05-18 18:05:44 +0800

    fix(form): correct clearValidate return type to Promise

--- packages/components/form/type.ts
+++ packages/components/form/type.ts
@@ -109,7 +109,7 @@ export interface FormInstanceFunctions<FormData extends Data = Data> {
   /**
    * 清空校验结果。可使用 fields 指定清除部分字段的校验结果，fields 值为空则表示清除所有字段校验结果。清除邮箱校验结果示例：`clearValidate(['email'])`
    */
-  clearValidate: (fields?: Array<keyof FormData>) => void;
+  clearValidate: (fields?: Array<keyof FormData>) => Promise<void>;
   /**
    * 重置表单，表单里面没有重置按钮`<button type=\"reset\" />`时可以使用该方法，默认重置全部字段为空，该方法会触发 `reset` 事件。<br />如果表单属性 `resetType='empty'` 或者 `reset.type='empty'` 会重置为空；<br />如果表单属性 `resetType='initial'` 或者 `reset.type='initial'` 会重置为表单初始值。<br />`reset.fields` 用于设置具体重置哪些字段，示例：`reset({ type: 'initial', fields: ['name', 'age'] })`
    */
