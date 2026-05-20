1717c7a29e9842b4a56ed7a67303358186836848
黎伟杰 <674416404@qq.com>
2026-05-20 14:34:08 +0800

    refactor(nuxt): 将模块配置移至模块声明内

--- packages/tdesign-vue-next-nuxt/site/nuxt.config.ts
+++ packages/tdesign-vue-next-nuxt/site/nuxt.config.ts
@@ -1,19 +1,22 @@
 import { defineNuxtConfig } from 'nuxt/config';
 
 export default defineNuxtConfig({
-  modules: ['../src/module'],
+  modules: [
+    [
+      '../src/module',
+      {
+        resolveIcons: true,
+        // include:['Button']
+        // exclude: ['Button']
+        // iconInclude: ['Edit1']
+        // prefix: 'tdesign'
+        // plugins:['NotifyPlugin'],
+        // importVariables:'~/assets/theme.css'
+      },
+    ],
+  ],
 
   devtools: { enabled: true },
   devServer: { port: 17002 },
   compatibilityDate: 'latest',
-
-  tdesign: {
-    resolveIcons: true,
-    // include:['Button']
-    // exclude: ['Button']
-    // iconInclude: ['Edit1']
-    // prefix: 'tdesign'
-    // plugins:['NotifyPlugin'],
-    // importVariables:'~/assets/theme.css'
-  },
 });
