879b9fe642e9c19e66f918d550af111c2b351c63
Wesley <985189328@qq.com>
2026-07-02 14:42:22 +0800

    feat: demo

---
+++ packages/components/switch/_example/theme.vue
@@ -0,0 +1,19 @@
+<template>
+  <t-space direction="vertical" size="large">
+    <t-space>
+      <t-switch theme="round" size="small" />
+      <t-switch theme="round" />
+      <t-switch theme="round" size="large" />
+    </t-space>
+    <t-space>
+      <t-switch theme="rectangle" size="small" />
+      <t-switch theme="rectangle" />
+      <t-switch theme="rectangle" size="large" />
+    </t-space>
+    <t-space>
+      <t-switch theme="line" size="small" />
+      <t-switch theme="line" />
+      <t-switch theme="line" size="large" />
+    </t-space>
+  </t-space>
+</template>
