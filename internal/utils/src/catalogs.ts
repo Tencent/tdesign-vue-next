a25d63077346d6365d05a445fbc4c941dd36b685
Uyarn <uyarnchen@gmail.com>
2026-08-06 17:31:11 +0800

    fix(Icon): fix multiple alpha color overlap

--- internal/utils/src/catalogs.ts
+++ internal/utils/src/catalogs.ts
@@ -90,7 +90,7 @@ export const catalogs = {
   tdesign: {
     '@tdesign/site-components': '^0.19.2',
     '@tdesign/theme-generator': '^1.2.4',
-    'tdesign-icons-vue-next': '~0.4.7',
+    'tdesign-icons-vue-next': '^0.4.8',
     'tdesign-publish-cli': '^0.0.12',
   },
   test: {
