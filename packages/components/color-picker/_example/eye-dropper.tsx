3b3b77477c7e477b5ea3c2f550d6f58d8904e5b2
Leo Cheng <chengkelfan@qq.com>
2026-08-06 20:22:28 +0800

    fix(color-picker): convert eyeDropper example to SFC to unblock build:vue.
    Signed-off-by: Leo Cheng <chengkelfan@qq.com>

--- packages/components/color-picker/_example/eye-dropper.tsx
+++
@@ -1,8 +0,0 @@
-import { defineComponent } from 'vue';
-import { ColorPicker } from 'tdesign-vue-next';
-
-export default defineComponent({
-  setup() {
-    return () => <ColorPicker eye-dropper defaultValue="#0052d9" />;
-  },
-});
---
+++ packages/components/color-picker/_example/eye-dropper.vue
@@ -0,0 +1,8 @@
+<template>
+  <t-color-picker v-model="color" :eye-dropper="true" default-value="#0052d9" />
+</template>
+<script lang="ts" setup>
+import { ref } from 'vue';
+
+const color = ref('#0052d9');
+</script>
