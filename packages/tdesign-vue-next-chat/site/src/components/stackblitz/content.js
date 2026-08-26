017430f6632b700e46674c347c22c91ad707d362
阿菜 Cai <jimmyrss1102@gmail.com>
2026-08-26 16:15:55 +0800

    chore(chat): pin web components preview commit

--- packages/tdesign-vue-next-chat/site/src/components/stackblitz/content.js
+++ packages/tdesign-vue-next-chat/site/src/components/stackblitz/content.js
@@ -86,9 +86,10 @@ export const packageJSONContent = JSON.stringify(
       vite: catalogs.bundle.vite,
       '@vitejs/plugin-vue': catalogs.bundle['@vitejs/plugin-vue'],
       '@vitejs/plugin-vue-jsx': catalogs.bundle['@vitejs/plugin-vue-jsx'],
-      '@tdesign/web-components': 'https://pkg.pr.new/TDesignOteam/tdesign-web-components/@tdesign/web-components@418',
+      '@tdesign/web-components':
+        'https://pkg.pr.new/TDesignOteam/tdesign-web-components/@tdesign/web-components@a68991563d5e8ac7f3eb4825fa70409851fafc0c',
       '@tdesign/web-components-chat':
-        'https://pkg.pr.new/TDesignOteam/tdesign-web-components/@tdesign/web-components-chat@418',
+        'https://pkg.pr.new/TDesignOteam/tdesign-web-components/@tdesign/web-components-chat@a68991563d5e8ac7f3eb4825fa70409851fafc0c',
       'omi-vueify': '^0.0.12',
       'tvision-charts-vue-next': '^3.3.13',
       'highlight.js': '^11.11.1',
