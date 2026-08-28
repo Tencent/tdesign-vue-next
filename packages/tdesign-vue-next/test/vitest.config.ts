e5679b8b6299b8651ff427860227a3707f8af4de
Uyarn <uyarnchen@gmail.com>
2026-08-28 15:27:06 +0800

    chore: fix lint

--- packages/tdesign-vue-next/test/vitest.config.ts
+++ packages/tdesign-vue-next/test/vitest.config.ts
@@ -6,9 +6,6 @@ import { joinComponentsRoot, joinPackagesRoot, joinTdesignVueNextRoot } from '@t
 export default defineConfig({
   resolve: {
     alias: {
-      // TODO: paopao 为什么还需要 alias，因为在 example 中的写法只能是 tdesign-vue-next，虽然有这个子应用，但没有 build 是没用的，同时即便是 prebuild 了，hmr 也是问题
-      '@tdesign/common-js': await joinCommonRoot('js'),
-      '@tdesign/common-style': await joinCommonRoot('style'),
       'tdesign-vue-next/es': joinComponentsRoot(),
       'tdesign-vue-next': joinComponentsRoot(),
     },
