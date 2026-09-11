eda8d0f6fe67690392e5b4161584a731504e23e0
黎伟杰 <674416404@qq.com>
2026-09-11 09:36:23 +0800

    docs(site): 更新 LLM 文档站点描述

--- packages/tdesign-vue-next/site/plugins/generate-llms/index.ts
+++ packages/tdesign-vue-next/site/plugins/generate-llms/index.ts
@@ -87,7 +87,7 @@ export default function generateLlmsPlugin() {
         platform: 'web',
         parseComponentDoc,
         siteTitle: 'TDesign Vue Next',
-        siteDescription: 'TDesign Vue Next 组件库的 LLM 友好文档索引。',
+        siteDescription: 'TDesign 适配桌面端的组件库，适合在 Vue3.x 技术栈项目中使用。',
         siteBaseUrl: config.base,
       });
     },
